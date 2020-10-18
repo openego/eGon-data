d5c224fe066f5341678958d191eb09ebc9b6e57b
Stephan Günther <gnn.code@gmail.com>
2020-10-18 17:12:29 +0200

    Test "--version" output in the main CLI test
    In normal TDD behaviour this test doesn't pass yet but instead documents
    the intended behaviour.

--- tests/test_egon-data.py
+++ tests/test_egon-data.py
@@ -1,11 +1,14 @@
 from click.testing import CliRunner
 
+from egon.data import __version__
 from egon.data.cli import main
 
 
 def test_main():
     runner = CliRunner()
-    result = runner.invoke(main, [])
+    result = runner.invoke(main, ["--version"])
 
-    assert result.output == '()\n'
+    assert result.output == "{name}, version {version}\n".format(
+        name=main.name, version=__version__
+    )
     assert result.exit_code == 0
