43b494649e4fc71d0006909a56fc56fe662b6586
Guido Pleßmann <guido.plessmann@rl-institut.de>
2020-10-22 09:39:13 +0200

    Read OSM data set configuration from data_sets.yml

--- src/egon/data/data_import/openstreetmap/import_osm.py
+++ src/egon/data/data_import/openstreetmap/import_osm.py
@@ -3,22 +3,18 @@ import os
 from urllib.request import urlretrieve
 import yaml
 import egon.data
+from egon.data import utils
 
 
-STYLEFILE = "oedb.style"
-PBFFILEURL = "https://download.geofabrik.de/europe/germany/bremen-200101.osm.pbf"
-PBFFILE = "bremen-200101.osm.pbf"
-TABLE_PREFIX = "osm"
+def download_osm_file():
+    data_config = utils.data_set_configuration()
+    osm_config = data_config["openstreetmap"]["original_data"]["osm"]
+
+    if not os.path.isfile(osm_config["file"]):
+        urlretrieve(osm_config["url"] + osm_config["file"], osm_config["file"])
 
 
-def download_osm_file(url=PBFFILEURL, file=PBFFILE):
-    # file = os.path.join(DOWNLOADDIR, filename)
-
-    if not os.path.isfile(file):
-        urlretrieve(url, file)
-
-
-def osm2postgres(num_processes=4, cache_size=4096):
+def osm2postgres(osm_file=None, num_processes=4, cache_size=4096):
 
     # Read database configuration from docker-compose.yml
     package_path = egon.data.__path__[0]
@@ -29,6 +25,10 @@ def osm2postgres(num_processes=4, cache_size=4096):
     docker_db_config["HOST"] = docker_db_config_additional[0]
     docker_db_config["PORT"] = docker_db_config_additional[1]
 
+    # Get data set config
+    data_config = utils.data_set_configuration()
+    osm_config = data_config["openstreetmap"]["original_data"]["osm"]
+
     # Prepare osm2pgsql command
     cmd = ["osm2pgsql",
            "--create",
@@ -37,9 +37,9 @@ def osm2postgres(num_processes=4, cache_size=4096):
            f"--number-processes {num_processes}",
            f"--cache {cache_size}",
            f"-H {docker_db_config['HOST']} -P {docker_db_config['PORT']} -d {docker_db_config['POSTGRES_DB']} -U {docker_db_config['POSTGRES_USER']}",
-           f"-p {TABLE_PREFIX}",
-           f"-S {STYLEFILE}",
-           f"{PBFFILE}"
+           f"-p {osm_config['table_prefix']}",
+           f"-S {osm_config['stylefile']}",
+           f"{osm_config['file']}"
            ]
 
     # Execute osm2pgsql for import OSM data
@@ -48,10 +48,13 @@ def osm2postgres(num_processes=4, cache_size=4096):
                    env={"PGPASSWORD": docker_db_config['POSTGRES_PASSWORD']},
                    cwd=os.path.dirname(__file__))
 
-# TODO: Specify OSM related params (version, etc.) in yaml file
+
+# TODO: Insert table names in SQL script read from data_sets.yml
 # TODO: call osm download from airflow
 # TODO: call osm import (.py) from airflow
 # TODO: add SQL import script and call it with airflow
+# TODO: write docstrings for added functions
+# TODO: add module docstring
 # TODO: maybe downgrade Postgres version to the same as in data_processing
 
 
