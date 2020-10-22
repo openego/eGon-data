caf780be83327a2c2ba795e54b5b62e6145a3462
Stephan Günther <gnn.code@gmail.com>
2020-10-22 11:06:21 +0200

    Add a comment explaining how to order tasks

--- src/egon/data/airflow/dags/pipeline.py
+++ src/egon/data/airflow/dags/pipeline.py
@@ -10,3 +10,11 @@ with airflow.DAG(
     default_args={"start_date": days_ago(1)},
 ) as pipeline:
     setup = PythonOperator(task_id="initdb", python_callable=initdb)
+    # If you a second task, e.g. `teardown`, which should come after `setup`
+    # you would specify this in the following way:
+    #
+    # setup.set_downstream(teardown)
+    #
+    # or
+    #
+    # teardown.set_upstream(setup)
