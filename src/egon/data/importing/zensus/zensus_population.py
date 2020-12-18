50c986f7e68b4aca6d4abdd0148b1cea738c204d
IlkaCu <ilka.cussmann@hs-flensburg.de>
2020-12-18 13:45:37 +0100

    first draft zensus pop import

---
+++ src/egon/data/importing/zensus/zensus_population.py
@@ -0,0 +1,86 @@
+"""The central module containing all code dealing with importing Zensus data."""
+
+from urllib.request import urlretrieve
+import os
+import zipfile
+
+from egon.data import db
+import egon.data.config 
+
+
+def download_zensus_pop(): 
+    """Download Zensus csv file on population per hectar grid cell."""
+    data_config = egon.data.config.datasets()
+    zensus_population_config = data_config["zensus_population"]["original_data"]
+
+    target_file = os.path.join(
+        os.path.dirname(__file__), zensus_population_config["target"]["path"]
+    )
+
+    if not os.path.isfile(target_file):
+        urlretrieve(zensus_population_config["source"]["url"], target_file)
+        
+download_zensus_pop()
+
+
+def zspop_to_postgres(): 
+    
+        # Get information from data configuration file
+    data_config = egon.data.config.datasets()
+    zensus_population_orig = data_config["zensus_population"]["original_data"]
+    zensus_population_processed = data_config["zensus_population"]["processed"]
+    input_file = os.path.join(
+        os.path.dirname(__file__), zensus_population_orig["target"]["path"]
+        )
+        
+
+    # Create target schema  
+    db.execute_sql(f"CREATE SCHEMA IF NOT EXISTS {zensus_population_processed['schema']};")
+    
+    # Drop and create target table
+    db.execute_sql(f"DROP TABLE IF EXISTS {zensus_population_processed['schema']}.{zensus_population_processed['table']} CASCADE;")
+
+    db.execute_sql(f"""CREATE TABLE {zensus_population_processed['schema']}.{zensus_population_processed['table']}
+                       (
+                        gid        SERIAL NOT NULL,
+                        grid_id    character varying(254) NOT NULL,
+                        x_mp       int,
+                        y_mp       int,
+                        population smallint,
+                        geom_point geometry(Point,3035),
+                        geom geometry (Polygon, 3035),
+                        CONSTRAINT zensus_population_per_ha_pkey PRIMARY KEY (gid));""")
+    
+    with zipfile.ZipFile(input_file) as zf:
+        for filename in zf.namelist():
+            zf.extract(filename)
+    
+            db.execute_sql(f"""COPY society.destatis_zensus_population_per_ha (grid_id, x_mp, y_mp, population)
+                               FROM '{os.path.join(
+                               os.path.dirname(__file__),filename)}' 
+                               DELIMITER ';'
+                               CSV HEADER; """)
+        os.remove(filename)
+        
+    db.execute_sql("""UPDATE society.destatis_zensus_population_per_ha zs
+                        geom_point= ST_SetSRID(ST_MakePoint(zs.x_mp, zs.y_mp),3035);""")
+    
+    db.execute_sql("""UPDATE society.destatis_zensus_population_per_ha zs
+                        geom = ST_SetSRID((ST_MakeEnvelope(zs.x_mp-50,zs.y_mp-50,zs.x_mp+50,zs.y_mp+50)),3035);""")
+    
+    db.execute_sql("""CREATE INDEX destatis_zensus_population_per_ha_geom_idx
+                        ON society.destatis_zensus_population_per_ha
+                        USING gist
+                        (geom);""")
+    
+    db.execute_sql("""CREATE INDEX destatis_zensus_population_per_ha_geom_point_idx
+                        ON society.destatis_zensus_population_per_ha
+                        USING gist
+                         (geom_point);""")
+
+zspop_to_postgres()
+
+
+
+
+
