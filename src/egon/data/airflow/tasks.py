12b2a988aeb78aa273f43870212bd7a8b766d974
Stephan Günther <gnn.code@gmail.com>
2021-01-11 03:12:27 +0100

    Wrap a context manager around `socket.connect_ex`
    Saves one line and helps to never forget it.

--- src/egon/data/airflow/tasks.py
+++ src/egon/data/airflow/tasks.py
@@ -8,9 +8,8 @@ from egon.data.db import credentials
 def initdb():
     """ Initialize the local database used for data processing. """
     db = credentials()
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    code = s.connect_ex((db["HOST"], int(db["PORT"])))
-    s.close()
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        code = s.connect_ex((db["HOST"], int(db["PORT"])))
     if code != 0:
         subprocess.run(
             ["docker-compose", "up", "-d", "--build"],
