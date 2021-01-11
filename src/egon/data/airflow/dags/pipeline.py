99e3a81bd71d3d398293165350d854fa1ad7e7c9
IlkaCu <ilka.cussmann@hs-flensburg.de>
2021-01-11 15:50:09 +0100

    Include zensus import into pipeline.py

--- src/egon/data/airflow/dags/pipeline.py
+++ src/egon/data/airflow/dags/pipeline.py
@@ -5,6 +5,7 @@ import airflow
 from egon.data.airflow.tasks import initdb
 import egon.data.importing.openstreetmap as import_osm
 import egon.data.processing.openstreetmap as process_osm
+import egon.data.importing.zensus as import_zs
 
 with airflow.DAG(
     "egon-data-processing-pipeline",
@@ -28,3 +29,15 @@ with airflow.DAG(
         task_id="add-osm-metadata", python_callable=import_osm.add_metadata
     )
     setup >> osm_download >> osm_import >> osm_migrate >> osm_add_metadata
+    
+    # Zensus population import 
+    zs_pop_download = PythonOperator(
+        task_id="download-zensus-population", 
+        python_callable=import_zs.download_zs_pop
+    )
+    
+    zs_pop_import = PythonOperator(
+        task_id="import-zensus-population",
+        python_callable=import_zs.zspop_to_postgres
+    )
+    setup >> zs_pop_download >> zs_pop_import
