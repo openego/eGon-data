082d40a0ea9db71a62abfb9e13ec62da0b18be22
Stephan Günther <gnn.code@gmail.com>
2021-01-14 05:50:05 +0100

    Manually break lines to be less than 80 characters
    The first line was pretty interesting because it contained exactly 80
    characters. Other than that, breaking things up was pretty simple and
    there are several degrees of freedom in how can distribute the strings.
    The most important thing to know is that consecutive lines consisting
    just of strings are automatically concatenated by the Python
    interpreter.

--- src/egon/data/importing/zensus/__init__.py
+++ src/egon/data/importing/zensus/__init__.py
@@ -1,4 +1,5 @@
-"""The central module containing all code dealing with importing Zensus data."""
+"""The central module containing all code dealing with importing Zensus data.
+"""
 
 from urllib.request import urlretrieve
 import os
@@ -44,68 +45,78 @@ def population_to_postgres():
 
     # Drop and create target table
     db.execute_sql(
-        f"DROP TABLE IF EXISTS {zensus_population_processed['schema']}.{zensus_population_processed['table']} CASCADE;"
+        "DROP TABLE IF EXISTS"
+        f" {zensus_population_processed['schema']}"
+        f".{zensus_population_processed['table']}"
+        " CASCADE;"
     )
 
     db.execute_sql(
-        f"""CREATE TABLE {zensus_population_processed['schema']}.{zensus_population_processed['table']}
-                       (
-                        gid        SERIAL NOT NULL,
-                        grid_id    character varying(254) NOT NULL,
-                        x_mp       int,
-                        y_mp       int,
-                        population smallint,
-                        geom_point geometry(Point,3035),
-                        geom geometry (Polygon, 3035),
-                        CONSTRAINT zensus_population_per_ha_pkey PRIMARY KEY (gid));"""
+        "CREATE TABLE"
+        f" {zensus_population_processed['schema']}"
+        f".{zensus_population_processed['table']}"
+        """( gid        SERIAL NOT NULL,
+             grid_id    character varying(254) NOT NULL,
+             x_mp       int,
+             y_mp       int,
+             population smallint,
+             geom_point geometry(Point,3035),
+             geom geometry (Polygon, 3035),
+             CONSTRAINT zensus_population_per_ha_pkey PRIMARY KEY (gid));
+        """
     )
 
     with zipfile.ZipFile(input_file) as zf:
         for filename in zf.namelist():
             zf.extract(filename)
-
+            host = ["-h", f"{docker_db_config['HOST']}"]
+            port = ["-p", f"{docker_db_config['PORT']}"]
+            pgdb = ["-d", f"{docker_db_config['POSTGRES_DB']}"]
+            user = ["-U", f"{docker_db_config['POSTGRES_USER']}"]
+            command = [
+                "-c",
+                "\copy"
+                f" {zensus_population_processed['schema']}"
+                f".{zensus_population_processed['table']}"
+                " (grid_id, x_mp, y_mp, population)"
+                " FROM '{filename}'"
+                " DELIMITER ';' CSV HEADER;",
+            ]
             subprocess.run(
-                [
-                    "psql",
-                    "-h",
-                    f"{docker_db_config['HOST']}",
-                    "-p",
-                    f"{docker_db_config['PORT']}",
-                    "-d",
-                    f"{docker_db_config['POSTGRES_DB']}",
-                    "-U",
-                    f"{docker_db_config['POSTGRES_USER']}",
-                    "-c",
-                    f"""\copy {zensus_population_processed['schema']}.{zensus_population_processed['table']} (grid_id, x_mp, y_mp, population)
-                              FROM '{filename}'
-                              DELIMITER ';'
-                                CSV HEADER; """,
-                ],
+                ["psql"] + host + port + pgdb + user + command,
                 env={"PGPASSWORD": docker_db_config["POSTGRES_PASSWORD"]},
             )
 
         os.remove(filename)
 
     db.execute_sql(
-        f"""UPDATE {zensus_population_processed['schema']}.{zensus_population_processed['table']} zs
-                   SET geom_point= ST_SetSRID(ST_MakePoint(zs.x_mp, zs.y_mp),3035);"""
+        "UPDATE"
+        f" {zensus_population_processed['schema']}"
+        f".{zensus_population_processed['table']} zs"
+        " SET geom_point=ST_SetSRID(ST_MakePoint(zs.x_mp, zs.y_mp), 3035);"
     )
 
     db.execute_sql(
-        f"""UPDATE {zensus_population_processed['schema']}.{zensus_population_processed['table']} zs
-                   SET geom = ST_SetSRID((ST_MakeEnvelope(zs.x_mp-50,zs.y_mp-50,zs.x_mp+50,zs.y_mp+50)),3035);"""
+        "UPDATE"
+        f" {zensus_population_processed['schema']}"
+        f".{zensus_population_processed['table']} zs"
+        """ SET geom=ST_SetSRID(
+                (ST_MakeEnvelope(zs.x_mp-50,zs.y_mp-50,zs.x_mp+50,zs.y_mp+50)),
+                3035
+            );
+        """
     )
 
     db.execute_sql(
-        f"""CREATE INDEX destatis_zensus_population_per_ha_geom_idx
-                   ON {zensus_population_processed['schema']}.{zensus_population_processed['table']}
-                   USING gist
-                   (geom);"""
+        "CREATE INDEX destatis_zensus_population_per_ha_geom_idx ON"
+        f" {zensus_population_processed['schema']}"
+        f".{zensus_population_processed['table']}"
+        " USING gist (geom);"
     )
 
     db.execute_sql(
-        f"""CREATE INDEX destatis_zensus_population_per_ha_geom_point_idx
-                   ON {zensus_population_processed['schema']}.{zensus_population_processed['table']}
-                   USING gist
-                   (geom_point);"""
+        "CREATE INDEX destatis_zensus_population_per_ha_geom_point_idx ON"
+        f" {zensus_population_processed['schema']}"
+        f".{zensus_population_processed['table']}"
+        " USING gist (geom_point);"
     )
