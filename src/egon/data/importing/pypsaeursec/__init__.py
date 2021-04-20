a996d2c2044d9141ae42831e41782b3cf31c5131
Stephan Günther <gnn.code@gmail.com>
2021-04-21 00:46:02 +0200

    Reformat "src/egon/data/importing/pypsaeursec/__init__.py"
    Done automatically by running `black` on the file.

--- src/egon/data/importing/pypsaeursec/__init__.py
+++ src/egon/data/importing/pypsaeursec/__init__.py
@@ -15,12 +15,12 @@ Base = declarative_base()
 
 
 def run_pypsa_eur_sec():
-    
+
     # execute pypsa-eur-sec
     import os
     import egon.data.subprocess as subproc
     from pathlib import Path
-    
+
     filepath = Path(".")
     pypsa_eur_repos = filepath / "pypsa-eur"
     technology_data_repos = filepath / "technology-data"
@@ -29,35 +29,22 @@ def run_pypsa_eur_sec():
 
     if not os.path.exists(pypsa_eur_repos):
         subproc.run(
-            [
-                "git",
-                "clone",
-                "https://github.com/PyPSA/pypsa-eur.git",
-            ],
+            ["git", "clone", "https://github.com/PyPSA/pypsa-eur.git"],
             cwd=filepath,
         )
 
     if not os.path.exists(technology_data_repos):
         subproc.run(
-            [
-                "git",
-                "clone",
-                "https://github.com/PyPSA/technology-data.git",
-            ],
+            ["git", "clone", "https://github.com/PyPSA/technology-data.git"],
             cwd=filepath,
         )
-        
 
     if not os.path.exists(pypsa_eur_sec_repos):
         subproc.run(
-            [
-                "git",
-                "clone",
-                "https://github.com/openego/pypsa-eur-sec.git",
-            ],
+            ["git", "clone", "https://github.com/openego/pypsa-eur-sec.git"],
             cwd=filepath,
         )
-    
+
     subproc.run(
         [
             "wget",
@@ -65,25 +52,17 @@ def run_pypsa_eur_sec():
         ],
         cwd=pypsa_eur_sec_repos_data,
     )
- 
-    subproc.run(
-        [
-             "tar",
-             "xvzf",
-             "pypsa-eur-sec-data-bundle-201012.tar.gz",
-        ],
-        cwd=pypsa_eur_sec_repos_data,
-    )
- 
 
     subproc.run(
-        [
-            "snakemake",
-            "-j1",
-            "prepare_sector_networks",
-        ],
+        ["tar", "xvzf", "pypsa-eur-sec-data-bundle-201012.tar.gz"],
+        cwd=pypsa_eur_sec_repos_data,
+    )
+
+    subproc.run(
+        ["snakemake", "-j1", "prepare_sector_networks"],
         cwd=pypsa_eur_sec_repos,
     )
+
 
 def pypsa_eur_sec_eGon100_capacities():
     """Inserts installed capacities for the eGon100 scenario
@@ -98,22 +77,26 @@ def pypsa_eur_sec_eGon100_capacities():
     engine = db.engine()
 
     # Delete rows if already exist
-    db.execute_sql("DELETE FROM supply.egon_scenario_capacities "
-                   "WHERE scenario_name = 'eGon100'")
+    db.execute_sql(
+        "DELETE FROM supply.egon_scenario_capacities "
+        "WHERE scenario_name = 'eGon100'"
+    )
 
     # read-in installed capacities
     target_file = os.path.join(
         os.path.dirname(__file__),
-        scenario_config('eGon100')['paths']['capacities'])
+        scenario_config("eGon100")["paths"]["capacities"],
+    )
 
     df = pd.read_csv(target_file, skiprows=5)
-    df.columns = ['component', 'country', 'carrier', 'capacity']
-    df['scenario_name'] = 'eGon100'
+    df.columns = ["component", "country", "carrier", "capacity"]
+    df["scenario_name"] = "eGon100"
 
     # Insert data to db
-    df.to_sql('egon_scenario_capacities',
-                       engine,
-                       schema='supply',
-                       if_exists='append',
-                       index=df.index)
-
+    df.to_sql(
+        "egon_scenario_capacities",
+        engine,
+        schema="supply",
+        if_exists="append",
+        index=df.index,
+    )
