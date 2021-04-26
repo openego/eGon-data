86aa865a5099401176a999d6d45542d60255dbf0
KathiEsterl <47177587+KathiEsterl@users.noreply.github.com>
2021-04-26 11:44:34 +0200

    Add files via upload

--- src/egon/data/processing/regio_Wind&PV/PV.py
+++ src/egon/data/processing/regio_Wind&PV/PV.py
@@ -356,7 +356,7 @@ def regio_of_pv_ground_mounted(path,con, pow_per_area, join_buffer, max_dist_hv,
     pv_rora = build_pv(pv_rora)
     pv_agri = build_pv(pv_agri)
     
-    '''# files for depiction in QGis
+    # files for depiction in QGis
     pv_rora['centroid'].to_file("PVs_rora_new.geojson", driver='GeoJSON')
     pv_agri['centroid'].to_file("PVs_agri_new.geojson", driver='GeoJSON')
     
@@ -368,7 +368,7 @@ def regio_of_pv_ground_mounted(path,con, pow_per_area, join_buffer, max_dist_hv,
     pv_rora_mv, pv_rora_hv, pv_agri_mv, pv_agri_hv = check_target(pv_rora_mv, pv_rora_hv, pv_agri_mv, pv_agri_hv, potentials_rora, pv_rora, pv_agri)
       
     
-    return pv_rora_mv, pv_rora_hv, pv_agri_mv, pv_agri_hv'''
+    return pv_rora_mv, pv_rora_hv, pv_agri_mv, pv_agri_hv
 
     return pv_rora, pv_agri
 
@@ -401,11 +401,8 @@ max_dist_hv = 20000 # m
 target_power = 1337984 # kW 
 # assumption for target value of installed capacity in Germany per scenario
 
-pv_rora, pv_agri  = regio_of_pv_ground_mounted(path,con,
+pv_rora_mv, pv_rora_hv, pv_agri_mv, pv_agri_hv = regio_of_pv_ground_mounted(path,con,
                                                 pow_per_area, join_buffer, max_dist_hv, target_power)
-
-#pv_rora_mv, pv_rora_hv, pv_agri_mv, pv_agri_hv = regio_of_pv_ground_mounted(path,con,
-                                                #pow_per_area, join_buffer, max_dist_hv, target_power)
 
 
 
