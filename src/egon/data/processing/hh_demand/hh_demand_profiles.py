fcaf960bfb0c550b4b12119fa0f916eb538a8bab
Julian.Endres <julian.endres@rl-institut.de>
2021-05-21 00:51:36 +0200

    added hh-demand-profile-generator

---
+++ src/egon/data/processing/hh_demand/hh_demand_profiles.py
@@ -0,0 +1,207 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import os
+import pandas as pd
+from egon.data import db
+
+import hh_demand_profiles_tools as hh_tools
+
+
+
+# Loadprofilesdump
+# ###################
+# in Wh
+# TODO: > to/from SQL?
+file = 'h0_profiles.h5'
+file = os.path.join(os.path.realpath(file))
+df_profiles = pd.read_hdf(file)
+
+# set multiindex to HH_types
+df_profiles.columns = pd.MultiIndex.from_arrays([df_profiles.columns.str[:2], df_profiles.columns.str[3:]])
+
+
+# Load Zensus data at nuts-level
+# ###################
+# TODO: > to/from SQL?
+file = 'Zensus2011_Personen.csv'
+file = os.path.join(os.path.realpath(file))
+df_zensus = pd.read_csv(file, sep=';', decimal='.', skiprows=5, skipfooter=7,
+                        index_col=[0, 1], header=[0, 1], encoding='latin1', engine='python')
+
+# clean data
+df_zensus = df_zensus.applymap(hh_tools.clean).applymap(int)
+# preprocess nuts1 zensus data
+df_zensus = hh_tools.process_nuts1_zensus_data(df_zensus)
+
+# ## Household distribution
+# - Adults living in househould type
+# - number of kids not included even if in housholdtype name
+# **! The Eurostat data only gives the amount of adults/seniors, excluding the amount of kids <15**
+# eurostat is used for demand-profile-generator @fraunhofer
+
+# hh shares mapping zensus to eurostat
+hh_types = {'SR': [('Einpersonenhaushalte (Singlehaushalte)', 'Insgesamt', 'Seniors'),
+                   ('Alleinerziehende Elternteile', 'Insgesamt', 'Seniors')],  # Single Seniors Single Parents Seniors
+            'SO': [('Einpersonenhaushalte (Singlehaushalte)', 'Insgesamt', 'Adults')],  # Single Adults
+            'SK': [('Alleinerziehende Elternteile', 'Insgesamt', 'Adults')],  # Single Parents Adult
+            'PR': [('Paare ohne Kind(er)', '2 Personen', 'Seniors'),
+                   ('Mehrpersonenhaushalte ohne Kernfamilie', '2 Personen', 'Seniors')],
+            # Couples without Kids Senior & same sex couples & shared flat seniors
+            'PO': [('Paare ohne Kind(er)', '2 Personen', 'Adults'),
+                   ('Mehrpersonenhaushalte ohne Kernfamilie', '2 Personen', 'Adults')],
+            # Couples without Kids adults & same sex couples & shared flat adults
+            'P1': [('Paare mit Kind(ern)', '3 Personen', 'Adults')],
+            'P2': [('Paare mit Kind(ern)', '4 Personen', 'Adults')],
+            'P3': [('Paare mit Kind(ern)', '5 Personen', 'Adults'),
+                   ('Paare mit Kind(ern)', '6 und mehr Personen', 'Adults')],
+            'OR': [('Mehrpersonenhaushalte ohne Kernfamilie', '3 Personen', 'Seniors'),
+                   ('Mehrpersonenhaushalte ohne Kernfamilie', '4 Personen', 'Seniors'),
+                   ('Mehrpersonenhaushalte ohne Kernfamilie', '5 Personen', 'Seniors'),
+                   ('Mehrpersonenhaushalte ohne Kernfamilie', '6 und mehr Personen', 'Seniors'),
+                   ('Paare mit Kind(ern)', '3 Personen', 'Seniors'),
+                   ('Paare ohne Kind(er)', '3 Personen', 'Seniors'),
+                   ('Paare mit Kind(ern)', '4 Personen', 'Seniors'),
+                   ('Paare ohne Kind(er)', '4 Personen', 'Seniors'),
+                   ('Paare mit Kind(ern)', '5 Personen', 'Seniors'),
+                   ('Paare ohne Kind(er)', '5 Personen', 'Seniors'),
+                   ('Paare mit Kind(ern)', '6 und mehr Personen', 'Seniors'),
+                   ('Paare ohne Kind(er)', '6 und mehr Personen', 'Seniors')],  # no info about share of kids
+
+            # OO, O1, O2 have the same amount, as no information about the share of kids within zensus data set.
+            # if needed the total amount can be corrected in the hh_tools.get_hh_dist function
+            # using multi_adjust=True option
+            'OO': [('Mehrpersonenhaushalte ohne Kernfamilie', '3 Personen', 'Adults'),
+                   ('Mehrpersonenhaushalte ohne Kernfamilie', '4 Personen', 'Adults'),
+                   ('Mehrpersonenhaushalte ohne Kernfamilie', '5 Personen', 'Adults'),
+                   ('Mehrpersonenhaushalte ohne Kernfamilie', '6 und mehr Personen', 'Adults'),
+                   ('Paare ohne Kind(er)', '3 Personen', 'Adults'),
+                   ('Paare ohne Kind(er)', '4 Personen', 'Adults'),
+                   ('Paare ohne Kind(er)', '5 Personen', 'Adults'),
+                   ('Paare ohne Kind(er)', '6 und mehr Personen', 'Adults')],  # no info about share of kids
+            # TODO: maybe remove following lines if not needed
+            #             'O1': [('Mehrpersonenhaushalte ohne Kernfamilie', '3 Personen', 'Adults'),
+            #                    ('Mehrpersonenhaushalte ohne Kernfamilie', '4 Personen', 'Adults'),
+            #                    ('Mehrpersonenhaushalte ohne Kernfamilie', '5 Personen', 'Adults'),
+            #                    ('Mehrpersonenhaushalte ohne Kernfamilie', '6 und mehr Personen', 'Adults'),
+            #                    ('Paare ohne Kind(er)', '3 Personen', 'Adults'),
+            #                    ('Paare ohne Kind(er)', '4 Personen', 'Adults'),
+            #                    ('Paare ohne Kind(er)', '5 Personen', 'Adults'),
+            #                    ('Paare ohne Kind(er)', '6 und mehr Personen', 'Adults')],  # no info about share of kids
+            #             'O2': [('Mehrpersonenhaushalte ohne Kernfamilie', '3 Personen', 'Adults'),
+            #                    ('Mehrpersonenhaushalte ohne Kernfamilie', '4 Personen', 'Adults'),
+            #                    ('Mehrpersonenhaushalte ohne Kernfamilie', '5 Personen', 'Adults'),
+            #                    ('Mehrpersonenhaushalte ohne Kernfamilie', '6 und mehr Personen', 'Adults'),
+            #                    ('Paare ohne Kind(er)', '3 Personen', 'Adults'),
+            #                    ('Paare ohne Kind(er)', '4 Personen', 'Adults'),
+            #                    ('Paare ohne Kind(er)', '5 Personen', 'Adults'),
+            #                    ('Paare ohne Kind(er)', '6 und mehr Personen', 'Adults')]
+            }
+
+# distribution of people by household @eurostats
+# df_hh_types_D = pd.Series({'SR': 0.083, 'SO': 0.158, 'SK': 0.022,
+#                            'PR': 0.145, 'PO': 0.203, 'P1': 0.081, 'P2': 0.077, 'P3': 0.024,
+#                            'OR': 0.023, 'OO': 0.13, 'O1': 0.04, 'O2': 0.015})
+
+# hh_tools.get_hh_dist without eurostat adjustment for O1-03 Groups in absolute values
+df_hh_types_nad_abs = hh_tools.get_hh_dist(df_zensus, hh_types, multi_adjust=False, relative=False)
+
+# #########################
+# FIXME:
+# mapping needs to be adjusted for OR, OO, O1, O2
+#
+# ###########################
+
+mapping_people_in_households = {'SR': 1,
+                                'SO': 1,
+                                'SK': 1,  # kids are excluded
+                                'PR': 2,
+                                'PO': 2,
+                                'P1': 2,  # kids are excluded
+                                'P2': 2,  # ""
+                                'P3': 2,  # ""
+                                'OR': 4,  # parameter needs to be re/defined
+                                'OO': 4,  # ""
+                                #                                 'O1': 4,  # ""
+                                #                                 'O2': 4,  # ""
+                                }
+# derivate households data from inhabitants data by compound number of people per household type
+df_dist_households = hh_tools.inhabitants_to_households(df_hh_types_nad_abs, mapping_people_in_households)
+
+
+# SQL - Access Zensus household data cell-level
+df_households_typ = db.select_dataframe(sql="""
+            SELECT grid_id, attribute, characteristics_code, characteristics_text, quantity
+            FROM society.destatis_zensus_household_per_ha
+            WHERE attribute = 'HHTYP_FAM' """)
+df_households_typ = df_households_typ.drop(columns=['attribute', 'characteristics_text'])
+df_households_typ = df_households_typ.rename(columns={'quantity': 'hh_5types'})
+
+mapping_zensus_hh_subgroups = {1: ['SR', 'SO'],
+                               2: ['PR', 'PO'],
+                               3: ['SK'],
+                               4: ['P1', 'P2', 'P3'],
+                               5: ['OR', 'OO'],
+                               }
+
+for value in mapping_zensus_hh_subgroups.values():
+    df_dist_households.loc[value] = df_dist_households.loc[value].div(df_dist_households.loc[value].sum())
+
+# SQL- create table to map cells to nuts3 and nuts1
+df_grid_id = db.select_dataframe(sql="""
+                    SELECT pop.grid_id, pop.gid, vg250.vg250_nuts3 as nuts3, lan.nuts as nuts1, lan.gen
+                    FROM society.destatis_zensus_population_per_ha_inside_germany as pop
+                    LEFT JOIN boundaries.egon_map_zensus_vg250 as vg250
+                    ON (pop.gid=vg250.zensus_population_id)
+                    LEFT JOIN boundaries.vg250_lan as lan
+                    ON (LEFT(vg250.vg250_nuts3, 3)=lan.nuts) """)
+df_grid_id = df_grid_id.drop_duplicates()
+df_grid_id = df_grid_id.reset_index(drop=True)
+
+# merge nuts info to zensus cell level data
+# how='inner' is used as ids of unpopulated areas are removed df_grid_id or earliers tables. see here:
+# https://github.com/openego/eGon-data/blob/59195926e41c8bd6d1ca8426957b97f33ef27bcc/src/egon/data/importing/zensus/__init__.py#L418-L449
+df_households_typ = pd.merge(df_households_typ, df_grid_id[['grid_id', 'gen', 'nuts1', 'nuts3']],
+                             left_on='grid_id', right_on='grid_id', how='inner')
+
+# Merge Zensus nuts level household data with zensus cell level by dividing hh-groups with mapping_zensus_hh_subgroups
+df_zensus_cells = pd.DataFrame()
+for (country, code), df_country_type in df_households_typ.groupby(['gen', 'characteristics_code']):
+
+    # iterate over zenus_country subgroups
+    for typ in mapping_zensus_hh_subgroups[code]:
+        df_country_type['hh_type'] = typ
+        df_country_type['factor'] = df_dist_households.loc[typ, country]
+        df_country_type['hh_10types'] = df_country_type['hh_5types'] * df_dist_households.loc[typ, country]
+        df_zensus_cells = df_zensus_cells.append(df_country_type, ignore_index=True)
+
+df_zensus_cells = df_zensus_cells.sort_values(by=['grid_id', 'characteristics_code']).reset_index(drop=True)
+
+# change profile numbers to int
+df_profiles.columns = pd.MultiIndex.from_tuples([(a, int(b)) for a, b in df_profiles.columns])
+
+pool_size = df_profiles.groupby(level=0, axis=1).size()
+
+df_demand_regio = db.select_dataframe(sql="""
+                        SELECT year, nuts3, SUM (demand) as demand_mWha
+                        FROM demand.egon_demandregio_hh as egon_d
+                        GROUP BY nuts3, year
+                        ORDER BY year""", index_col=['year', 'nuts3'])
+
+# testcase
+# test_data = df_zensus_cells.groupby('nuts3').get_group('DEF03')
+# test_data = pd.concat([df_zensus_cells.groupby('nuts3').get_group('DEF03'),
+#                        df_zensus_cells.groupby('nuts3').get_group('DEF06')])
+# 
+# df_cell_demand_metadata = hh_tools.get_cell_demand_metadata(test_data, df_profiles)
+# df_cell_demand_metadata = hh_tools.adjust_to_demand_regio_nuts3_annual(df_cell_demand_metadata, df_profiles, df_demand_regio)
+# 
+# 
+# 
+# import random
+# load_area_ids = random.sample(list(df_cell_demand_metadata.index), 100)
+# max_value_load_area = hh_tools.get_load_area_max_load(df_profiles, df_cell_demand_metadata, load_area_ids, 2034)
+# # print(df_cell_demand_metadata.shape)
+# print(max_value_load_area)
+
+
---
+++ src/egon/data/processing/hh_demand/hh_demand_profiles_tools.py
@@ -0,0 +1,202 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pandas as pd
+import numpy as np
+
+from itertools import cycle
+import random
+
+
+def clean(x):
+    x = str(x).replace('-', str(0))
+    x = str(x).replace('.', str(0))
+    x = x.strip('()')
+    return x
+
+
+def get_hh_dist(df_zensus, hh_types, multi_adjust=True, relative=True):
+    # adjust multi with/without kids via eurostat share as not clearly derivable without infos about share of kids
+    if multi_adjust:
+        adjust = {'SR': 1, 'SO': 1, 'SK': 1, 'PR': 1, 'PO': 1, 'P1': 1, 'P2': 1, 'P3': 1, 'OR': 1,
+                  'OO': 0.703, 'O1': 0.216, 'O2': 0.081, }
+    else:
+        adjust = {'SR': 1, 'SO': 1, 'SK': 1, 'PR': 1, 'PO': 1, 'P1': 1, 'P2': 1, 'P3': 1, 'OR': 1,
+                  'OO': 1, 'O1': 0, 'O2': 0, }
+
+    df_hh_types = pd.DataFrame(
+        ({hhtype: adjust[hhtype] * df_zensus.loc[countries, codes].sum() for hhtype, codes in hh_types.items()}
+         for countries in df_zensus.index), index=df_zensus.index)
+    # drop zero columns
+    df_hh_types = df_hh_types.loc[:, (df_hh_types != 0).any(axis=0)]
+    if relative:
+        # normalize
+        df_hh_types = df_hh_types.div(df_hh_types.sum(axis=1), axis=0)
+    return df_hh_types.T
+
+
+def get_loadprofiles(df_profiles, df_hh_types, hh_total, state_dist=False):
+    # equal share of hh_total for every state
+    if not state_dist:
+        state_dist = pd.Series(hh_total / df_hh_types.shape[1], index=df_hh_types.columns)
+    # specific share of hh_total by state_dist
+    else:
+        state_dist = state_dist * hh_total
+
+    header = pd.MultiIndex.from_tuples(
+        [(state, hh_type) for state in df_hh_types.columns for hh_type in df_hh_types.index],
+        names=['State', 'Type'])
+
+    df_loadprofiles = pd.DataFrame(columns=header)
+
+    for state, state_share in state_dist.items():
+
+        for hh_type, type_share in df_hh_types[state].items():
+            samples = state_share * type_share
+            #             if samples<1:
+            #                 raise ValueError(f"Sample size needs to be >=1. hh-share of {state}-{hh_type} is {samples:.2f} , increase hh_total")
+            samples = int(samples)
+            hh_type_ts = df_profiles[hh_type].T.sample(samples, axis=0, replace=True).sum()
+
+            df_loadprofiles.loc[:, (state, hh_type)] = hh_type_ts
+
+    timestamp = pd.date_range(start='01-01-2012', periods=df_loadprofiles.shape[0], freq='h')
+    df_loadprofiles.index = timestamp
+    return df_loadprofiles
+
+
+def normalize_loadprofiles(df_lp, to_value):
+    # normed to 'to_value' kWh annual
+    df_kwh = df_lp.groupby(level='State', axis=1).sum() * to_value / df_lp.groupby(level='State', axis=1).sum().sum()
+
+    return df_kwh
+
+
+def aggregate_loadprofiles(df_lp, to_value):
+    # normed to 'to_value' kWh annual
+    df_kwh = df_lp.sum(axis=1) * to_value / df_lp.sum(axis=1).sum()
+
+    return df_kwh
+
+
+def inhabitants_to_households(df_people_by_householdtypes_abs, mapping_people_in_households):
+    diff = set(df_people_by_householdtypes_abs.index) ^ set(mapping_people_in_households.keys())
+
+    if bool(diff):
+        for key in diff:
+            mapping_people_in_households = dict(mapping_people_in_households)
+            del mapping_people_in_households[key]
+        print(f'Removed {diff} from mapping!')
+
+    df_households_by_type = df_people_by_householdtypes_abs.div(mapping_people_in_households, axis=0)
+    df_households_by_type = df_households_by_type.apply(np.ceil)  # round up households
+
+    return df_households_by_type
+
+
+def process_nuts1_zensus_data(df_zensus):
+    # Group data to fit Load Profile Generator categories
+    # define kids/adults/seniors
+    kids = ['Unter 3', '3 - 5', '6 - 14']  # < 15
+    adults = ['15 - 17', '18 - 24', '25 - 29', '30 - 39', '40 - 49', '50 - 64']  # 15 < x <65
+    seniors = ['65 - 74', '75 und älter']  # >65
+
+    # sum groups of kids, adults and seniors and concat
+    df_kids = df_zensus.loc[:, (slice(None), kids)].groupby(level=0, axis=1).sum()
+    df_adults = df_zensus.loc[:, (slice(None), adults)].groupby(level=0, axis=1).sum()
+    df_seniors = df_zensus.loc[:, (slice(None), seniors)].groupby(level=0, axis=1).sum()
+    df_zensus = pd.concat([df_kids, df_adults, df_seniors], axis=1, keys=['Kids', 'Adults', 'Seniors'],
+                          names=['age', 'persons'])
+
+    # reduce column names to state only
+    mapping_state = {i: i.split()[1] for i in df_zensus.index.get_level_values(level=0)}
+
+    # rename index
+    df_zensus = df_zensus.rename(index=mapping_state, level=0)
+    # rename axis
+    df_zensus = df_zensus.rename_axis(['state', 'type'])
+    # unstack
+    df_zensus = df_zensus.unstack()
+    # reorder levels
+    df_zensus = df_zensus.reorder_levels(order=['type', 'persons', 'age'], axis=1)
+
+    return df_zensus
+
+
+def get_cell_demand_profile_ids(df_cell, pool_size, df_profiles):
+    """generates tuple of hh_type and random sample(without replacement) profile ids for cell"""
+    # maybe use instead
+    # np.random.default_rng().integers(low=0, high=pool_size[hh_type], size=sq) instead of random.sample
+    # use random.choice() if with replacement
+    # list of sample ids per hh_type in cell
+    cell_profile_ids = [(hh_type, random.sample(range(pool_size[hh_type]), k=sq)) \
+                        for hh_type, sq in zip(df_cell['hh_type'],
+                                               df_cell['hh_10types'].astype(int))]
+
+    # format to lists of tuples (hh_type, id)
+    cell_profile_ids = [list(zip(cycle([hh_type]), ids)) for hh_type, ids in cell_profile_ids]
+    # reduce to list
+    cell_profile_ids = [a for b in cell_profile_ids for a in b]
+
+    return cell_profile_ids
+
+
+# can be parallelized with grouping df_zensus_cells by grid_id/nuts3/nuts1
+def get_cell_demand_metadata(df_zensus_cells, df_profiles):
+    """generate table including demand profile ids for each cell using get_cell_demand_profile_ids"""
+
+    df_cell_demand_metadata = pd.DataFrame(index=df_zensus_cells.grid_id.unique(),
+                                           columns=['cell_profile_ids', 'nuts3', 'nuts1', '2035_factor',
+                                                    '2050_factor', ])
+    # 'peak_loads_hh', 'peak_load_cell',
+    df_cell_demand_metadata = df_cell_demand_metadata.rename_axis('cell_id')
+
+    pool_size = df_profiles.groupby(level=0, axis=1).size()
+
+    for cell_id, df_cell in df_zensus_cells.groupby(by='grid_id'):
+        # FIXME
+        # ! runden der Haushaltszahlen auf int
+        # ! kein Zurücklegen innerhalb einer Zelle ?!
+        cell_profile_ids = get_cell_demand_profile_ids(df_cell, pool_size, df_profiles)
+
+        df_cell_demand_metadata.at[cell_id, 'cell_profile_ids'] = cell_profile_ids
+        df_cell_demand_metadata.at[cell_id, 'nuts3'] = df_cell.loc[:, 'nuts3'].unique()[0]
+        df_cell_demand_metadata.at[cell_id, 'nuts1'] = df_cell.loc[:, 'nuts1'].unique()[0]
+
+    return df_cell_demand_metadata
+
+
+# can be parallelized with grouping df_zensus_cells by grid_id/nuts3/nuts1
+def adjust_to_demand_regio_nuts3_annual(df_cell_demand_metadata, df_profiles, df_demand_regio):
+    """computes the profile scaling factor by accumulated nuts3 cells and demand_regio data"""
+    for nuts3_id, df_nuts3 in df_cell_demand_metadata.groupby(by='nuts3'):
+        nuts3_cell_ids = df_nuts3.index
+        nuts3_profile_ids = df_nuts3.loc[:, 'cell_profile_ids'].sum()
+
+        # take all profiles of one nuts3, aggregate and sum
+        # profiles in Wh
+        nuts3_profiles_sum_annual = df_profiles.loc[:, nuts3_profile_ids].sum().sum()
+
+        # Scaling Factor
+        # ##############
+        # demand regio in MWh
+        # profiles in Wh
+        df_cell_demand_metadata.loc[nuts3_cell_ids, '2035_factor'] = df_demand_regio.loc[
+                                                                         (2035, nuts3_id), 'demand_mwha'] * 1e3 / (
+                                                                                 nuts3_profiles_sum_annual / 1e3)
+        df_cell_demand_metadata.loc[nuts3_cell_ids, '2050_factor'] = df_demand_regio.loc[
+                                                                         (2050, nuts3_id), 'demand_mwha'] * 1e3 / (
+                                                                                 nuts3_profiles_sum_annual / 1e3)
+
+    return df_cell_demand_metadata
+
+
+def get_load_area_max_load(df_profiles, df_cell_demand_metadata, load_area_ids, year):
+    """get max value of load area demand profile"""
+    timesteps = len(df_profiles)
+    full_load = pd.Series(data=np.zeros(timesteps), dtype=np.float64, index=range(timesteps))
+    load_area_meta = df_cell_demand_metadata.loc[load_area_ids, ['cell_profile_ids', 'nuts3', f'{year}_factor']]
+    for (nuts3, factor), df in load_area_meta.groupby(by=['nuts3', f'{year}_factor']):
+        part_load = df_profiles.loc[:, df['cell_profile_ids'].sum()].sum(axis=1) * factor / 1e3  # profiles in Wh
+        full_load = full_load.add(part_load)
+    return full_load.max()  #, full_load.idxmax()
