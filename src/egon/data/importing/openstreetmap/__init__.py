959cd83e1dbca683a65c1867f02229de08a04ab2
ClaraBuettner <clara.buettner@hs-flensburg.de>
2021-05-28 16:26:09 +0200

    Set nprocess of import_osm to 1

--- src/egon/data/importing/openstreetmap/__init__.py
+++ src/egon/data/importing/openstreetmap/__init__.py
@@ -42,7 +42,7 @@ def download_pbf_file():
         urlretrieve(source_url, target_file)
 
 
-def to_postgres(num_processes=4, cache_size=4096):
+def to_postgres(num_processes=1, cache_size=4096):
     """Import OSM data from a Geofabrik `.pbf` file into a PostgreSQL database.
 
     Parameters
