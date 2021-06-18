3db773e13bd310b391913614ffadc7ede97b59dc
Carlos Epia <carlosepiar@gmail.com>
2021-06-18 21:20:12 +0200

    modification write db"

--- src/egon/data/processing/calculate_dlr.py
+++ src/egon/data/processing/calculate_dlr.py
@@ -248,8 +248,7 @@ def Calculate_DLR():
     # Modify column "s_max_pu" to fit the requirement of the table
     trans_lines["s_max_pu"] = trans_lines.apply(
         lambda x: list(x["s_max_pu"]), axis= 1)
-    trans_lines.set_index('line_id', inplace=True)
-    #trans_lines.index.rename("", inplace=True)
+    trans_lines.index.rename("temp_id", inplace=True)
     # Insert into database
     trans_lines.to_sql('egon_pf_hv_line_timeseries',
                                schema='grid',
