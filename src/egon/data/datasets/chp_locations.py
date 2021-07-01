c5da9bffeda71b92ff6146e5c98d81fa5e2d6e25
ClaraBuettner <clara.buettner@hs-flensburg.de>
2021-07-01 15:03:53 +0200

    Add documentation

--- src/egon/data/datasets/chp_locations.py
+++ src/egon/data/datasets/chp_locations.py
@@ -50,9 +50,36 @@ def map_carrier_egon_mastr():
             'Sonstige_Energietraeger': 'other_non_renewable',
             }))
 
+def map_carrier_matching():
+    """Map carriers from NEP to carriers from MaStR used in matching
+
+    Returns
+    -------
+    pandas.Series
+        List of mapped carriers
+
+    """
+    return (
+        pd.Series(
+        data = {
+        'Kuppelgase': ['Erdgas', 'AndereGase','Mineraloelprodukte' ],
+        'Sonstige_Energietraeger':[
+                    'Erdgas', 'AndereGase','Mineraloelprodukte',
+                    'Waerme','NichtBiogenerAbfall'],
+        'Mineraloelprodukte':['Erdgas', 'Mineraloelprodukte' ],
+        'Erdgas': ['Erdgas'],
+        'Steinkohle': ['Steinkohle']}))
 
 #####################################   NEP treatment   #################################
 def select_chp_from_nep():
+    """ Select CHP plants with location from NEP's list of power plants
+
+    Returns
+    -------
+    pandas.DataFrame
+        CHP plants from NEP list
+
+    """
     sources = config.datasets()["chp_location"]["sources"]
 
     # Select CHP plants with geolocation from list of conventional power plants
@@ -78,32 +105,42 @@ def select_chp_from_nep():
     # Remove the subunits from the bnetza_id
     chp_NEP_data['bnetza_id'] = chp_NEP_data['bnetza_id'].str[0:7]
 
+    # Update carrier to match to MaStR
     map_carrier = map_carrier_nep_mastr()
-
     chp_NEP_data['carrier'] = map_carrier[chp_NEP_data['carrier'].values].values
 
+    # Initalize DataFrame
     chp_NEP = pd.DataFrame(
         columns = ['name', 'postcode', 'carrier', 'capacity',
                    'c2035_capacity', 'c2035_chp'])
 
+    # Insert rows from list without a name
     chp_NEP = chp_NEP.append(
         chp_NEP_data[chp_NEP_data.name.isnull()].loc[:, [
             'name', 'postcode', 'carrier', 'capacity',
             'c2035_capacity',  'c2035_chp']])
-
+    # Insert rows from list with a name
     chp_NEP = chp_NEP.append(
         chp_NEP_data.groupby(
             ['carrier', 'name', 'postcode', 'c2035_chp']
             )['capacity','c2035_capacity'].sum().reset_index()).reset_index()
 
-
     return chp_NEP.drop('index', axis=1)
 
 #####################################   MaStR treatment   #################################
 def select_chp_from_mastr():
+    """Select combustion CHP plants from MaStR
+
+    Returns
+    -------
+    MaStR_konv : pd.DataFrame
+        CHP plants from MaStR
+
+    """
 
     sources = config.datasets()["chp_location"]["sources"]
 
+    # Read-in data from MaStR
     MaStR_konv = pd.read_csv(
         sources["mastr_combustion"],
         delimiter = ',',
@@ -118,24 +155,26 @@ def select_chp_from_mastr():
                     'EinheitBetriebsstatus',
                     'LokationMastrNummer'])
 
+    # Rename columns
     MaStR_konv = MaStR_konv.rename(columns={ 'Kraftwerksnummer': 'bnetza_id',
                                       'Energietraeger': 'energietraeger_Ma',
                                       'Postleitzahl': 'plz_Ma',
                                       'Laengengrad': 'longitude',
                                       'Breitengrad': 'latitude'})
 
+    # Select only CHP plants which are in operation
     MaStR_konv = MaStR_konv[MaStR_konv.EinheitBetriebsstatus=='InBetrieb']
-    # 66931 of 68321
 
     # Insert geometry column
     MaStR_konv = MaStR_konv[ ~ ( MaStR_konv['longitude'].isnull()) ]
-    # 15777 of 68321
     MaStR_konv = geopandas.GeoDataFrame(
         MaStR_konv, geometry=geopandas.points_from_xy(
             MaStR_konv['longitude'], MaStR_konv['latitude']))
 
+    # Drop individual CHP
     MaStR_konv = MaStR_konv[(MaStR_konv['Nettonennleistung'] >= 100)]
 
+    # Drop rows without post code and update datatype of postcode
     MaStR_konv = MaStR_konv[~MaStR_konv['plz_Ma'].isnull()]
     MaStR_konv['plz_Ma'] = MaStR_konv['plz_Ma'].astype(int)
 
@@ -148,36 +187,51 @@ def select_chp_from_mastr():
 
 # ############################################   Match with plz and K   ############################################
 def match_nep_chp(chp_NEP, MaStR_konv, chp_NEP_matched, buffer_capacity=0.1):
+    """ Match CHP plants from MaStR to list of power plants from NEP
 
+    Parameters
+    ----------
+    chp_NEP : pandas.DataFrame
+        CHP plants from NEP which are not matched to MaStR
+    MaStR_konv : pandas.DataFrame
+        CHP plants from MaStR which are not matched to NEP
+    chp_NEP_matched : pandas.DataFrame
+        Already matched CHP
+    buffer_capacity : float, optional
+        Maximum difference in capacity in p.u. The default is 0.1.
+
+    Returns
+    -------
+    chp_NEP_matched : pandas.DataFrame
+        Matched CHP
+    MaStR_konv : pandas.DataFrame
+        CHP plants from MaStR which are not matched to NEP
+    chp_NEP : pandas.DataFrame
+        CHP plants from NEP which are not matched to MaStR
+
+    """
 
     for ET in chp_NEP['carrier'].unique():
 
         print('**********************  ' + ET + '  **********************')
-        if ET == 'Kuppelgase':
-            carrier = ['Erdgas', 'AndereGase','Mineraloelprodukte' ]
-
-        elif ET == 'Sonstige_Energietraeger':
-            carrier = [
-                    'Erdgas', 'AndereGase','Mineraloelprodukte',
-                    'Waerme','NichtBiogenerAbfall']
-
-        elif ET == 'Mineraloelprodukte':
-            carrier = ['Erdgas', 'Mineraloelprodukte' ]
-        else:
-            carrier = [ET]
+        map_carrier = map_carrier_matching()
 
         carrier_egon = map_carrier_egon_mastr()
 
-        for index, row in chp_NEP[(chp_NEP['carrier']  == ET)
-                                  & (chp_NEP['postcode']  != 'None')].iterrows():
-            K_NEP = row['capacity']
-            plz_NEP = row['postcode']
+        for index, row in chp_NEP[
+                (chp_NEP['carrier'] == ET)
+                & (chp_NEP['postcode'] != 'None')].iterrows():
+            # Select plants from MaStR that match carrier, PLZ
+            # and have a similar capacity
             selected = MaStR_konv[
-                (MaStR_konv.Nettonennleistung<= K_NEP * (1+buffer_capacity))
-                & (MaStR_konv.Nettonennleistung>= K_NEP * (1-buffer_capacity))
-                & (MaStR_konv.plz_Ma==int(plz_NEP))
-                & MaStR_konv.energietraeger_Ma.isin(carrier)]
+                (MaStR_konv.Nettonennleistung<= row['capacity']
+                 * (1+buffer_capacity))
+                & (MaStR_konv.Nettonennleistung>= row['capacity']
+                   * (1-buffer_capacity))
+                & (MaStR_konv.plz_Ma==int(row['postcode']))
+                & MaStR_konv.energietraeger_Ma.isin(map_carrier[ET])]
 
+            # If a plant could be matched, add this to chp_NEP_matched
             if len(selected) > 0:
                 chp_NEP_matched = chp_NEP_matched.append(
                     geopandas.GeoDataFrame(
@@ -194,22 +248,39 @@ def match_nep_chp(chp_NEP, MaStR_konv, chp_NEP_matched, buffer_capacity=0.1):
                             'geometry': selected.geometry.head(1),
                             'voltage_level': selected.voltage_level.head(1)
                         }))
+
+                # Drop matched CHP from chp_NEP and MaStR_konv
                 chp_NEP = chp_NEP.drop(index)
                 MaStR_konv = MaStR_konv.drop(selected.index)
 
     return chp_NEP_matched, MaStR_konv, chp_NEP
 
 def match_chp(chp_NEP, MaStR_konv, chp_NEP_matched, consider_carrier=True):
+    """ Match CHP plants from MaStR to list of power plants from NEP
 
-    map_carrier = pd.Series(
-        data = {
-        'Kuppelgase': ['Erdgas', 'AndereGase','Mineraloelprodukte' ],
-        'Sonstige_Energietraeger':[
-                    'Erdgas', 'AndereGase','Mineraloelprodukte',
-                    'Waerme','NichtBiogenerAbfall'],
-        'Mineraloelprodukte':['Erdgas', 'Mineraloelprodukte' ],
-        'Erdgas': ['Erdgas'],
-        'Steinkohle': ['Steinkohle']})
+    Parameters
+    ----------
+    chp_NEP : pandas.DataFrame
+        CHP plants from NEP which are not matched to MaStR
+    MaStR_konv : pandas.DataFrame
+        CHP plants from MaStR which are not matched to NEP
+    chp_NEP_matched : pandas.DataFrame
+        Already matched CHP
+    consider_carrier : boolean, optional
+        Choose if carrier is considered in matching. The default is True.
+
+    Returns
+    -------
+    chp_NEP_matched : pandas.DataFrame
+        Matched CHP
+    MaStR_konv : pandas.DataFrame
+        CHP plants from MaStR which are not matched to NEP
+    chp_NEP : pandas.DataFrame
+        CHP plants from NEP which are not matched to MaStR
+
+    """
+
+    map_carrier = map_carrier_matching()
 
     carrier_egon = map_carrier_egon_mastr()
 
@@ -230,6 +301,8 @@ def match_chp(chp_NEP, MaStR_konv, chp_NEP_matched, consider_carrier=True):
         selected_plants.loc[:, 'Nettonennleistung'] = (
                         row.c2035_capacity * selected_plants.Nettonennleistung/
                         selected_plants.Nettonennleistung.sum())
+
+        # If a plant could be matched, add this to chp_NEP_matched
         chp_NEP_matched = chp_NEP_matched.append(
             geopandas.GeoDataFrame(
                 data = {
@@ -243,8 +316,10 @@ def match_chp(chp_NEP, MaStR_konv, chp_NEP_matched, consider_carrier=True):
                     'th_capacity': selected_plants.ThermischeNutzleistung,
                     'scenario': 'eGon2035',
                     'geometry': selected_plants.geometry,
-                    'voltage_level': selected_plants.voltage_level.mean()
+                    'voltage_level': selected_plants.voltage_level
                     }))
+
+        # Drop matched CHP from chp_NEP and MaStR_konv
         if len(selected_plants) > 0:
             chp_NEP = chp_NEP.drop(i)
             MaStR_konv = MaStR_konv.drop(selected_plants.index)
@@ -253,42 +328,57 @@ def match_chp(chp_NEP, MaStR_konv, chp_NEP_matched, consider_carrier=True):
 
 ################################################### Final table ###################################################
 def insert_chp_egon2035():
+    """ Insert large CHP plants for eGon2035 considering NEP and MaStR
+
+    Returns
+    -------
+    None.
+
+    """
 
     target = config.datasets()["chp_location"]["targets"]["power_plants"]
 
+    # Select CHP from NEP list
     chp_NEP = select_chp_from_nep()
 
+    # Select CHP from MaStR
     MaStR_konv = select_chp_from_mastr()
 
-    # Assign voltage level
+    # Assign voltage level to MaStR
     MaStR_konv['voltage_level'] = assign_voltage_level(
         MaStR_konv, config.datasets()["chp_location"])
 
+    # Initalize DataFrame for match CHPs
     chp_NEP_matched = geopandas.GeoDataFrame(
         columns = [
             'carrier','chp','el_capacity','th_capacity', 'scenario','geometry',
             'MaStRNummer', 'source', 'voltage_level'])
 
+    # Match CHP from NEP list using PLZ, carrier and capacity
     chp_NEP_matched, MaStR_konv, chp_NEP = match_nep_chp(
         chp_NEP, MaStR_konv, chp_NEP_matched, buffer_capacity=0.1)
 
+    # Aggregate units from MaStR to one power plant
     MaStR_konv = MaStR_konv.groupby(
         ['plz_Ma', 'longitude', 'latitude','energietraeger_Ma', 'voltage_level']
         )[['Nettonennleistung', 'ThermischeNutzleistung', 'EinheitMastrNummer'
            ]].sum(numeric_only=False).reset_index()
-
     MaStR_konv['geometry'] = geopandas.points_from_xy(
         MaStR_konv['longitude'], MaStR_konv['latitude'])
 
+    # Match CHP from NEP list with aggregated MaStR units
     chp_NEP_matched, MaStR_konv, chp_NEP = match_nep_chp(
         chp_NEP, MaStR_konv, chp_NEP_matched, buffer_capacity=0.1)
 
+    # If some CHP's are not matched, drop capacity constraint
     chp_NEP_matched, chp_NEP, MaStR_konv = match_chp(
         chp_NEP, MaStR_konv, chp_NEP_matched, consider_carrier=True)
 
+    # If some CHP's are not matched, drop carrier constraint
     chp_NEP_matched, chp_NEP, MaStR_konv = match_chp(
         chp_NEP, MaStR_konv, chp_NEP_matched, consider_carrier=False)
 
+    # Prepare geometry for database import
     chp_NEP_matched["geometry_wkt"] = chp_NEP_matched["geometry"].apply(
         lambda geom: geom.wkt)
 
@@ -306,13 +396,14 @@ def insert_chp_egon2035():
     insert_chp['bus_id'] = assign_bus_id(
         insert_chp, config.datasets()["chp_location"]).bus_id
 
+    # Delete existing CHP in the target tabel
     db.execute_sql(
         f"""DELETE FROM {target['schema']}.{target['table']}
         WHERE carrier IN ('gas', 'other_non_renewable', 'oil')
         AND scenario='eGon2035'""")
 
+    # Insert into target table
     session = sessionmaker(bind=db.engine())()
-
     for i, row in insert_chp.iterrows():
         entry = EgonPowerPlants(
                 sources={
