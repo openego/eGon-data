9e8517ca60bebc3295aa6a846d91f228a8f5647a
Julian.Endres <julian.endres@rl-institut.de>
2021-07-05 14:55:44 +0200

    Define mastr_dataset_setup as partial of Dataset

--- src/egon/data/datasets/mastr.py
+++ src/egon/data/datasets/mastr.py
@@ -1,6 +1,7 @@
 from urllib.request import urlretrieve
 import os
-
+from functools import partial
+from egon.data.datasets import Dataset
 import egon.data.config
 
 
@@ -43,3 +44,12 @@ def download_mastr_data(data_stages=None):
     for filename in files:
         if not os.path.isfile(filename):
             urlretrieve(zenodo_files_url + filename, filename)
+
+
+mastr_data_setup = partial(
+    Dataset,
+    name="MastrData",
+    version="0.0.0",
+    dependencies=[],
+    tasks=(download_mastr_data,),
+)
