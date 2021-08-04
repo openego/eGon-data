3533061b9b33ce20c35889d103d315fa6303b420
ClaraBuettner <clara.buettner@hs-flensburg.de>
2021-08-04 16:47:35 +0200

    Rename egon_pf_hv to egon_etrago tables

--- src/egon/data/db.py
+++ src/egon/data/db.py
@@ -227,7 +227,7 @@ def next_etrago_id(component):
     """
     max_id = select_dataframe(
         f"""
-        SELECT MAX({component}_id) FROM grid.egon_pf_hv_{component}
+        SELECT MAX({component}_id) FROM grid.egon_etrago_{component}
         """)['max'][0]
 
     if max_id:
