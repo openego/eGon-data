8c8c22fd7f63455f328da4d0a4713280dd62653c
ClaraBuettner <clara.buettner@hs-flensburg.de>
2021-08-09 11:19:13 +0200

    Delete existing data from grid.egon_etrago_line_timeseries

--- src/egon/data/processing/calculate_dlr.py
+++ src/egon/data/processing/calculate_dlr.py
@@ -95,6 +95,13 @@ def Calculate_DLR():
     # Modify column "s_max_pu" to fit the requirement of the table
     trans_lines["s_max_pu"] = trans_lines.apply(lambda x: list(x["s_max_pu"]), axis=1)
     trans_lines["temp_id"] = 1
+
+    # Delete existing data
+    db.execute_sql(
+        """
+        DELETE FROM grid.egon_etrago_line_timeseries;
+        """)
+
     # Insert into database
     trans_lines.to_sql(
         "egon_etrago_line_timeseries",
