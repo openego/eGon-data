56c1e404b1e4071671e72c439abf46c5d5f4e64e
nesnoj <jonathan.amme@rl-institut.de>
2021-09-01 09:33:18 +0200

    Update SQL script: insert msising geoms

--- src/egon/data/importing/zensus/create_combined_zensus_table.sql
+++ src/egon/data/importing/zensus/create_combined_zensus_table.sql
@@ -3,10 +3,12 @@
 	If there's no data on buildings or apartments for a certain cell,
 	the value for building_count resp. apartment_count contains NULL.
 */
+
 DROP TABLE IF EXISTS society.egon_destatis_zensus_apartment_building_population_per_ha;
 CREATE TABLE society.egon_destatis_zensus_apartment_building_population_per_ha AS
-SELECT GREATEST(bld_apt.zensus_population_id, ze.gid) AS zensus_population_id,
-	   bld_apt.building_count,
+SELECT ze.grid_id,
+       GREATEST(bld_apt.zensus_population_id, ze.gid) AS zensus_population_id,
+       bld_apt.building_count,
 	   bld_apt.apartment_count,
 	   ze.population,
 	   ze.geom
@@ -14,8 +16,8 @@ SELECT GREATEST(bld_apt.zensus_population_id, ze.gid) AS zensus_population_id,
 		(SELECT GREATEST(bld.zensus_population_id, apt.zensus_population_id) AS zensus_population_id,
 				GREATEST(bld.grid_id, apt.grid_id) AS grid_id,
 				bld.quantity as building_count,
-				apt.quantity as apartment_count
-			FROM
+                apt.quantity as apartment_count
+            FROM
 				(SELECT *
 					FROM society.egon_destatis_zensus_building_per_ha
 					WHERE attribute = 'INSGESAMT' AND quantity_q < 2
@@ -33,4 +35,11 @@ SELECT GREATEST(bld_apt.zensus_population_id, ze.gid) AS zensus_population_id,
 		) ze
 	USING (grid_id)
 	ORDER BY zensus_population_id;
+
+UPDATE society.egon_destatis_zensus_apartment_building_population_per_ha AS t
+	SET geom = t2.geom
+	FROM
+	  society.destatis_zensus_population_per_ha AS t2
+	WHERE t.geom IS NULL AND t.grid_id = t2.grid_id;
+
 CREATE INDEX ON society.egon_destatis_zensus_apartment_building_population_per_ha USING gist (geom);
