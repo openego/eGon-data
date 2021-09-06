034b7d87f10273204f05e29d48640250010c0503
KathiEsterl <katharinaesterl@web.de>
2021-09-06 08:24:06 +0200

    versioning

--- src/egon/data/datasets/industrial_sites/__init__.py
+++ src/egon/data/datasets/industrial_sites/__init__.py
@@ -682,7 +682,7 @@ class MergeIndustrialSites(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="Merge_industrial_sites",
-            version="0.0.2.dev",
+            version="0.0.2",
             dependencies=dependencies,
             tasks=(download_import_industrial_sites, merge_inputs, map_nuts3),
         )
