6f7999a9a4c57b4b56abd4c75360a3c4e1622011
KathiEsterl <katharinaesterl@web.de>
2021-09-07 14:59:06 +0200

    solved issue with companyname

--- src/egon/data/datasets/industrial_sites/__init__.py
+++ src/egon/data/datasets/industrial_sites/__init__.py
@@ -112,7 +112,6 @@ class IndustrialSites(Base):
     address = Column(String(170))
     subsector = Column(String(100))
     wz = Column(Integer)
-    el_demand = Column(Float)
     nuts3 = Column(String(10))
     geom = Column(Geometry("POINT", 4326), index=True)
 
@@ -593,18 +592,17 @@ def merge_inputs():
     # Insert data from s-EEnergies
     db.execute_sql(
         f"""INSERT INTO {sites_table}
-              (companyname, address, subsector, wz, el_demand, geom)
+              (companyname, address, subsector, wz, geom)
                 SELECT  s.companyname,
                         s.address,
                         s.subsector,
                         s.wz,
-                        s.electricitydemand_tj * 9/2500,
                         s.geom
                 FROM {seenergies_table} s
                 WHERE   s.country = 'DE'
                 AND     geom IS NOT NULL
-                AND s.companyname NOT IN
-                    (SELECT h.companyname
+                AND LOWER(SUBSTRING(s.companyname, 1, 3)) NOT IN
+                    (SELECT LOWER(SUBSTRING(h.companyname, 1, 3))
                       FROM  {sites_table} h,
                             {seenergies_table} s
                       WHERE ST_DWithin (h.geom, s.geom, 0.01)
@@ -631,8 +629,8 @@ def merge_inputs():
                           FROM {seenergies_table} a
                           WHERE   a.country = 'DE'
                           AND     a.geom IS NOT NULL)
-                AND h.companyname NOT IN
-                    (SELECT s.companyname
+                AND LOWER(SUBSTRING(h.companyname, 1, 3)) NOT IN
+                    (SELECT LOWER(SUBSTRING(s.companyname, 1, 3))
                       FROM  {sites_table} s,
                             {hotmaps_table} h
                       WHERE ST_DWithin (s.geom, h.geom, 0.01)
