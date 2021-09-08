cd5baaf6d2f9e0b3aba7a73ef166d27e3ed219bb
nesnoj <jonathan.amme@rl-institut.de>
2021-09-08 16:14:50 +0200

    Add mapping table: OSM building -> zensus cell
    Table name: boundaries.egon_map_zensus_buildings_filtered

--- src/egon/data/datasets/osm_buildings_streets/osm_buildings_preprocessing.sql
+++ src/egon/data/datasets/osm_buildings_streets/osm_buildings_preprocessing.sql
@@ -6,6 +6,7 @@
  * calculate area
  * create centroids
 */
+
 DROP TABLE IF EXISTS openstreetmap.osm_buildings;
 CREATE TABLE openstreetmap.osm_buildings AS
     SELECT
@@ -155,6 +156,19 @@ CREATE TABLE openstreetmap.osm_buildings_filtered as
 
 CREATE INDEX ON openstreetmap.osm_buildings_filtered USING gist (geom);
 CREATE INDEX ON openstreetmap.osm_buildings_filtered USING gist (geom_point);
+
+-- create mapping table of buildings to zensus cells
+drop table if exists boundaries.egon_map_zensus_buildings_filtered;
+CREATE TABLE boundaries.egon_map_zensus_buildings_filtered as
+    select * from (
+        select
+			bld.osm_id,
+			zensus.grid_id
+        from openstreetmap.osm_buildings_filtered bld
+        left join society.egon_destatis_zensus_apartment_building_population_per_ha zensus
+        on ST_Within(bld.geom_point, zensus.geom)
+    ) bld2
+    where bld2.osm_id is not null;
 
 -- merge n_apartments representing households to buildings_filtered
 -- first a temp table is needed due to wanna add a count to share n_apartments for n_buildings
