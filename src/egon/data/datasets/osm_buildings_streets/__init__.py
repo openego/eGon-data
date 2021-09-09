1183694813579a5413cd443584aa93501b4ff3b1
nesnoj <jonathan.amme@rl-institut.de>
2021-09-09 11:16:13 +0200

    Move SQL scripts into separate functions/tasks

--- src/egon/data/datasets/osm_buildings_streets/__init__.py
+++ src/egon/data/datasets/osm_buildings_streets/__init__.py
@@ -4,24 +4,40 @@ from egon.data import db
 from egon.data.datasets import Dataset
 
 
-def data_extract():
+def execute_sql_script(script):
+    """Execute SQL script
+
+    Parameters
+    ----------
+    script : str
+        Filename of script
+    """
+    db.execute_sql_script(
+        os.path.join(
+            os.path.dirname(__file__),
+            script
+        )
+    )
+
+
+def extract_buildings():
     sql_scripts = [
         'osm_amenities_shops_preprocessing.sql',
         'osm_buildings_filter.sql',
         'osm_buildings_zensus_mapping.sql',
         'osm_buildings_temp_tables.sql',
-        'osm_buildings_amentities_results.sql',
-        'osm_ways_preprocessing.sql',
-        'drop_temp_tables.sql'
+        'osm_buildings_amentities_results.sql'
     ]
     for script in sql_scripts:
-        db.execute_sql_script(
-            os.path.join(
-                os.path.dirname(__file__),
-                script
-            )
-        )
+        execute_sql_script(script)
 
+
+def extract_ways():
+    execute_sql_script('osm_ways_preprocessing.sql')
+
+
+def drop_temp_tables():
+    execute_sql_script('drop_temp_tables.sql')
 
 def add_metadata():
     pass
@@ -33,5 +49,10 @@ class OsmBuildingsStreets(Dataset):
             name="OsmBuildingsStreets",
             version="0.0.0",
             dependencies=dependencies,
-            tasks=(data_extract, add_metadata),
+            tasks=(
+                extract_buildings,
+                extract_ways,
+                drop_temp_tables,
+                add_metadata
+            ),
         )
