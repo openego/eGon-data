239d190c3d29e4c67b2d1904dfa126d51fdf570a
IlkaCu <ilka.cussmann@hs-flensburg.de>
2021-10-04 10:53:25 +0200

    Add scripts dealing with storage units

---
+++ src/egon/data/datasets/storages/__init__.py
@@ -0,0 +1,221 @@
+"""The central module containing all code dealing with power plant data.
+"""
+from geoalchemy2 import Geometry
+from sqlalchemy import (
+    BigInteger,
+    Boolean,
+    Column,
+    Float,
+    Integer,
+    Sequence,
+    String,
+)
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from egon.data.datasets.storages.pumped_hydro import select_mastr_pumped_hydro, select_nep_pumped_hydro, match_storage_units, get_location, apply_voltage_level_thresholds
+from egon.data.datasets.power_plants import assign_voltage_level
+import geopandas as gpd
+import numpy as np
+import pandas as pd
+
+from egon.data import db, config
+from egon.data.datasets import Dataset
+
+
+
+Base = declarative_base()
+
+
+class EgonStorages(Base):
+    __tablename__ = "egon_storages"
+    __table_args__ = {"schema": "supply"}
+    id = Column(BigInteger, Sequence("storage_seq"), primary_key=True)
+    sources = Column(JSONB)
+    source_id = Column(JSONB)
+    carrier = Column(String)
+    el_capacity = Column(Float)
+    bus_id = Column(Integer)
+    voltage_level = Column(Integer)
+    scenario = Column(String)
+    geom = Column(Geometry("POINT", 4326))
+
+
+class PumpedHydro(Dataset):
+
+    def __init__(self, dependencies):
+        super().__init__(
+            name="Storages",
+            version="0.0.0",
+            dependencies=dependencies,
+            tasks=(
+                create_tables,
+                allocate_pumped_hydro,
+            ),
+        )
+
+
+def create_tables():
+    """Create tables for power plant data
+    Returns
+    -------
+    None.
+    """
+
+    cfg = config.datasets()["storages"]
+    db.execute_sql(f"CREATE SCHEMA IF NOT EXISTS {cfg['target']['schema']};")
+    engine = db.engine()
+    db.execute_sql(
+        f"""DROP TABLE IF EXISTS
+        {cfg['target']['schema']}.{cfg['target']['table']}"""
+    )
+
+    db.execute_sql("""DROP SEQUENCE IF EXISTS pp_seq""")
+    EgonStorages.__table__.create(bind=engine, checkfirst=True)
+
+def allocate_pumped_hydro():
+
+    carrier = "pumped_hydro"
+
+    cfg = config.datasets()["power_plants"]
+
+    nep = select_nep_pumped_hydro()
+    mastr = select_mastr_pumped_hydro()
+
+    # Assign voltage level to MaStR
+    mastr["voltage_level"] = assign_voltage_level(
+        mastr.rename({"el_capacity": "Nettonennleistung"}, axis=1), cfg
+    )
+
+    # Initalize DataFrame for matching power plants
+    matched = gpd.GeoDataFrame(
+        columns=[
+            "carrier",
+            "el_capacity",
+            "scenario",
+            "geometry",
+            "MaStRNummer",
+            "source",
+            "voltage_level",
+        ]
+    )
+
+    # Match pumped_hydro units from NEP list
+    # using PLZ and capacity
+    matched, mastr, nep = match_storage_units(
+        nep, mastr, matched, buffer_capacity=0.1, consider_carrier=False
+    )
+
+
+    # Match plants from NEP list using plz,
+    # neglecting the capacity
+    matched, mastr, nep = match_storage_units(
+        nep,
+        mastr,
+        matched,
+        consider_location="plz",
+        consider_carrier=False,
+        consider_capacity=False,
+    )
+
+    # Match plants from NEP list using city,
+    # neglecting the capacity
+    matched, mastr, nep = match_storage_units(
+        nep,
+        mastr,
+        matched,
+        consider_location="city",
+        consider_carrier=False,
+        consider_capacity=False,
+    )
+
+    # Match remaining plants from NEP using the federal state
+    matched, mastr, nep = match_storage_units(
+        nep,
+        mastr,
+        matched,
+        buffer_capacity=0.1,
+        consider_location="federal_state",
+        consider_carrier=False,
+    )
+
+    # Match remaining plants from NEP using the federal state
+    matched, mastr, nep = match_storage_units(
+        nep,
+        mastr,
+        matched,
+        buffer_capacity=0.7,
+        consider_location="federal_state",
+        consider_carrier=False,
+    )
+
+    print(f"{matched.el_capacity.sum()} MW of {carrier} matched")
+    print(f"{nep.c2035_capacity.sum()} MW of {carrier} not matched")
+
+    # Get location using geolocator and city information
+
+    located, unmatched = get_location(nep)
+
+    # Bring both dataframes together
+
+    matched= matched.append(located[["carrier", "el_capacity", "scenario", "geometry", "source", "MaStRNummer"]], ignore_index=True)
+
+    # Set CRS
+    matched.crs = "EPSG:4326"
+
+    # Assign voltage level
+    matched = apply_voltage_level_thresholds(matched)
+
+    # Assign bus_id
+    # Load grid district polygons
+    mv_grid_districts = db.select_geodataframe(
+        f"""
+    SELECT * FROM {cfg['sources']['egon_mv_grid_district']}
+    """,
+        epsg=4326,
+    )
+
+    ehv_grid_districts = db.select_geodataframe(
+        f"""
+    SELECT * FROM {cfg['sources']['ehv_voronoi']}
+    """,
+        epsg=4326,
+    )
+
+    # Perform spatial joins for plants in ehv and hv level seperately
+    power_plants_hv = gpd.sjoin(
+        matched[matched.voltage_level >= 3],
+        mv_grid_districts[["bus_id", "geom"]],
+        how="left",
+    ).drop(columns=["index_right"])
+    power_plants_ehv = gpd.sjoin(
+        matched[matched.voltage_level < 3],
+        ehv_grid_districts[["bus_id", "geom"]],
+        how="left",
+    ).drop(columns=["index_right"])
+
+    # Combine both dataframes
+    power_plants = pd.concat([power_plants_hv, power_plants_ehv])
+
+    # Delete existing units in the target table
+    db.execute_sql(
+        f""" DELETE FROM {cfg ['target']['schema']}.{cfg ['target']['table']}
+        WHERE carrier IN ('pumped_hydro')
+        AND scenario='eGon2035';"""
+    )
+
+    # Insert into target table
+    session = sessionmaker(bind=db.engine())()
+    for i, row in power_plants.iterrows():
+        entry = EgonStorages(
+            sources={"el_capacity": row.source},
+            source_id={"MastrNummer": row.MaStRNummer},
+            carrier=row.carrier,
+            el_capacity=row.el_capacity,
+            voltage_level=row.voltage_level,
+            bus_id=row.bus_id,
+            scenario=row.scenario,
+            geom=f"SRID=4326;POINT({row.geometry.x} {row.geometry.y})",
+        )
+        session.add(entry)
+    session.commit()
---
+++ src/egon/data/datasets/storages/pumped_hydro.py
@@ -0,0 +1,329 @@
+"""
+The module containing code allocating pumped hydro plants based on
+data from MaStR and NEP.
+"""
+
+import pandas as pd
+import geopandas as gpd
+import egon.data.config
+from egon.data import db, config
+from egon.data.datasets.power_plants import (
+    assign_voltage_level, assign_bus_id, assign_gas_bus_id,
+    filter_mastr_geometry, select_target)
+from egon.data.datasets.chp.match_nep import match_nep_chp, map_carrier_nep_mastr, map_carrier_egon_mastr, map_carrier_matching
+from egon.data.datasets.chp.small_chp import assign_use_case
+from sqlalchemy.orm import sessionmaker
+
+
+def select_nep_pumped_hydro():
+    """ Select pumped hydro plants from NEP power plants list
+
+
+    Returns
+    -------
+    pandas.DataFrame
+        Pumped hydro plants from NEP list
+    """
+    cfg = egon.data.config.datasets()["power_plants"]
+
+    carrier = 'pumped_hydro'
+
+    # Select plants with geolocation from list of conventional power plants
+    nep_ph = db.select_dataframe(
+        f"""
+        SELECT bnetza_id, name, carrier, postcode, capacity, city,
+        federal_state, c2035_capacity
+        FROM {cfg['sources']['nep_conv']}
+        WHERE carrier = '{carrier}'
+        AND c2035_capacity > 0
+        AND postcode != 'None';
+        """
+        )
+
+
+    # Removing plants out of Germany
+    nep_ph['postcode'] = nep_ph['postcode'].astype(str)
+    nep_ph = nep_ph[ ~ nep_ph['postcode'].str.contains('A')]
+    nep_ph = nep_ph[ ~ nep_ph['postcode'].str.contains('L')]
+    nep_ph = nep_ph[ ~ nep_ph['postcode'].str.contains('nan') ]
+
+    # Remove the subunits from the bnetza_id
+    nep_ph['bnetza_id'] = nep_ph['bnetza_id'].str[0:7]
+
+
+    return nep_ph
+
+def select_mastr_pumped_hydro():
+    """ Select pumped hydro plants from MaStR
+
+
+    Returns
+    -------
+    pandas.DataFrame
+        Pumped hydro plants from MaStR
+    """
+    sources = egon.data.config.datasets()["power_plants"]["sources"]
+
+    # Read-in data from MaStR
+    mastr_ph = pd.read_csv(
+        sources["mastr_storage"],
+        delimiter = ',',
+        usecols = ['Nettonennleistung',
+                    'EinheitMastrNummer',
+                    'Kraftwerksnummer',
+                    'Technologie',
+                    'Postleitzahl',
+                    'Laengengrad',
+                    'Breitengrad',
+                    'EinheitBetriebsstatus',
+                    'LokationMastrNummer',
+                    'Ort',
+                    'Bundesland'])
+
+    # Rename columns
+    mastr_ph = mastr_ph.rename(columns={
+        'Kraftwerksnummer': 'bnetza_id',
+        'Technologie': 'carrier',
+        'Postleitzahl': 'plz',
+        'Ort': 'city',
+        'Bundesland':'federal_state',
+        'Nettonennleistung': 'el_capacity'})
+
+    # Select only pumped hydro units
+    mastr_ph = mastr_ph[mastr_ph.carrier=='Pumpspeicher']
+
+
+    # Select only pumped hydro units which are in operation
+    mastr_ph = mastr_ph[mastr_ph.EinheitBetriebsstatus=='InBetrieb']
+
+    # Insert geometry column
+    mastr_ph = mastr_ph[ ~ ( mastr_ph['Laengengrad'].isnull()) ]
+    mastr_ph = gpd.GeoDataFrame(
+        mastr_ph, geometry=gpd.points_from_xy(
+            mastr_ph['Laengengrad'], mastr_ph['Breitengrad']))
+
+
+    # Drop rows without post code and update datatype of postcode
+    mastr_ph = mastr_ph[~mastr_ph['plz'].isnull()]
+    mastr_ph['plz'] = mastr_ph['plz'].astype(int)
+
+    # Calculate power in MW
+    mastr_ph.loc[:, 'el_capacity'] *=1e-3
+
+    mastr_ph = mastr_ph.set_crs(4326)
+
+    mastr_ph = mastr_ph[ ~ ( mastr_ph['federal_state'].isnull()) ]
+
+    # Drop CHP outside of Germany/ outside the test mode area
+    mastr_ph = filter_mastr_geometry(mastr_ph, federal_state=None)
+
+    return mastr_ph
+
+def match_storage_units(
+    nep,
+    mastr,
+    matched,
+    buffer_capacity=0.1,
+    consider_location="plz",
+    consider_carrier=True,
+    consider_capacity=True,
+):
+    """ Match storage_units (in this case only pumped hydro) from MaStR
+    to list of power plants from NEP
+
+    Parameters
+    ----------
+    nep : pandas.DataFrame
+        storage units from NEP which are not matched to MaStR
+    mastr : pandas.DataFrame
+        Pstorage_units from MaStR which are not matched to NEP
+    matched : pandas.DataFrame
+        Already matched storage_units
+    buffer_capacity : float, optional
+        Maximum difference in capacity in p.u. The default is 0.1.
+
+    Returns
+    -------
+    matched : pandas.DataFrame
+        Matched CHP
+    mastr : pandas.DataFrame
+        storage_units from MaStR which are not matched to NEP
+    nep : pandas.DataFrame
+        storage_units from NEP which are not matched to MaStR
+
+    """
+
+    list_federal_states = pd.Series(
+        {
+            "Hamburg": "HH",
+            "Sachsen": "SN",
+            "MecklenburgVorpommern": "MV",
+            "Thueringen": "TH",
+            "SchleswigHolstein": "SH",
+            "Bremen": "HB",
+            "Saarland": "SL",
+            "Bayern": "BY",
+            "BadenWuerttemberg": "BW",
+            "Brandenburg": "BB",
+            "Hessen": "HE",
+            "NordrheinWestfalen": "NW",
+            "Berlin": "BE",
+            "Niedersachsen": "NI",
+            "SachsenAnhalt": "ST",
+            "RheinlandPfalz": "RP",
+        }
+    )
+
+    carrier = 'pumped_hydro'
+
+    for index, row in nep[
+        (nep["carrier"] == carrier) & (nep["postcode"] != "None")
+    ].iterrows():
+
+        # Select plants from MaStR that match carrier, PLZ
+        # and have a similar capacity
+        # Create a copy of all power plants from MaStR
+        selected = mastr.copy()
+
+        # Set capacity constraint using buffer
+        if consider_capacity:
+            selected = selected[
+                (
+                    selected.el_capacity
+                    <= row["capacity"] * (1 + buffer_capacity)
+                )
+                & (
+                    selected.el_capacity
+                    >= row["capacity"] * (1 - buffer_capacity)
+                )
+            ]
+
+        # Set geographic constraint, either choose power plants
+        # with the same postcode, city or federal state
+        if consider_location == "plz":
+            selected = selected[
+                selected.plz.astype(int).astype(str) == row["postcode"]
+            ]
+        elif consider_location == "city":
+            selected = selected[
+                selected.city == row.city.replace("\n", " ")
+            ]
+        elif consider_location == "federal_state":
+            selected.loc[:, "federal_state"] = list_federal_states[
+                selected.federal_state
+            ].values
+            selected = selected[
+                selected.federal_state == row.federal_state
+            ]
+
+        # Set capacity constraint if selected
+        if consider_carrier:
+            selected = selected[selected.carrier==carrier]
+
+        # If a plant could be matched, add this to matched
+        if len(selected) > 0:
+            matched = matched.append(
+                gpd.GeoDataFrame(
+                    data={
+                        "source": "MaStR scaled with NEP 2021 list",
+                        "MaStRNummer": selected.EinheitMastrNummer.head(1),
+                        "carrier": carrier,
+                        "el_capacity": row.c2035_capacity,
+                        "scenario": "eGon2035",
+                        "geometry": selected.geometry.head(1),
+                        "voltage_level": selected.voltage_level.head(1),
+                    }
+                )
+            )
+
+            # Drop matched storage units from nep
+            nep = nep.drop(index)
+
+            # Drop matched storage units from MaStR list if the location is accurate
+            if consider_capacity & consider_carrier:
+                mastr = mastr.drop(selected.index)
+
+    return matched, mastr, nep
+
+
+def get_location(unmatched):
+    """ Gets a geolocation for units which couldn't be matched using MaStR data.
+    Uses geolocator and the city name from NEP data to create longitude and
+    latitude for a list of unmatched units.
+
+    Parameters
+    ----------
+    unmatched : pandas.DataFrame
+        storage units from NEP which are not matched to MaStR but containing
+        a city information
+
+    Returns
+    -------
+    unmatched: pandas.DataFrame
+        Units for which no geolocation could be identified
+
+    located : pandas.DataFrame
+        Units with a geolocation based on their city information
+
+    """
+
+    # import geolocator
+    from geopy.geocoders import Nominatim
+    geolocator = Nominatim(user_agent="egon_data")
+
+    # Create array of cities
+    cities = unmatched.city.values
+
+    # identify longitude and latitude for all cities in the array
+    for city in cities:
+        lon = geolocator.geocode(city).longitude
+        lat = geolocator.geocode(city).latitude
+
+        # write information on lon and lat to df
+        unmatched.loc[unmatched.city == city, "lon"] = lon
+        unmatched.loc[unmatched.city == city, "lat"] = lat
+
+    # Get a point geometry from lon and lat information
+    unmatched["geometry"] = gpd.points_from_xy(unmatched.lon, unmatched.lat)
+    unmatched.crs = "EPSG:4326"
+
+    # Copy units with lon and lat to a new dataframe
+    located = unmatched[["bnetza_id", "name", "carrier", "city", "c2035_capacity", "geometry"]].copy()
+    located.dropna(subset = ["geometry"], inplace = True)
+
+    # Rename columns for compatibility reasons
+    located = located.rename(columns={'c2035_capacity' : 'el_capacity', 'bnetza_id' : 'MaStRNummer'})
+    located["scenario"] = "eGon2035"
+    located["source"] = "NEP power plants geolocated using city"
+
+    unmatched = unmatched.drop(located.index.values)
+
+    return located, unmatched
+
+def apply_voltage_level_thresholds(power_plants):
+    """Assigns voltage level to power plants based on thresholds defined for
+    the egon project.
+
+    Parameters
+    ----------
+    power_plants : pandas.DataFrame
+        Power plants and their electrical capacity
+    Returns
+    -------
+    pandas.DataFrame
+        Power plants including voltage_level
+    """
+
+    # Identify voltage_level for every power plant taking thresholds into
+    # account which were defined in the eGon project. Existing entries on voltage
+    # will be overwritten
+
+    power_plants.loc[power_plants["el_capacity"] < 0.1, "voltage_level"] = 7
+    power_plants.loc[power_plants["el_capacity"] > 0.1, "voltage_level"] = 6
+    power_plants.loc[power_plants["el_capacity"] > 0.2, "voltage_level"] = 5
+    power_plants.loc[power_plants["el_capacity"] > 5.5, "voltage_level"] = 4
+    power_plants.loc[power_plants["el_capacity"] > 20, "voltage_level"] = 3
+    power_plants.loc[power_plants["el_capacity"] > 120, "voltage_level"] = 1
+
+    return power_plants
+
