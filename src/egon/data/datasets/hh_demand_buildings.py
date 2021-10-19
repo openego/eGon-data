39ca00d38674b5e93d11b6395871af0d33476e80
Julian.Endres <julian.endres@rl-institut.de>
2021-10-19 11:01:18 +0200

    Update imports

--- src/egon/data/datasets/hh_demand_buildings.py
+++ src/egon/data/datasets/hh_demand_buildings.py
@@ -1,23 +1,21 @@
 from pathlib import Path
-import importlib
-import os
 import random
 
-from shapely.geometry import Point, Polygon
-from sqlalchemy import Column, Integer, String, Table, inspect
+from shapely.geometry import Point
+from geoalchemy2 import Geometry
+from sqlalchemy import Column, Integer, String, Table, inspect, ARRAY, REAL
 from sqlalchemy.ext.declarative import declarative_base
 import geopandas as gpd
-import importlib_resources as resources
-import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
 from egon.data import db
 from egon.data.datasets import Dataset
-from egon.data.datasets.hh_demand_profiles import (
-    HouseholdElectricityProfilesInCensusCells,
-)
+from egon.data.datasets.hh_demand_profiles import HouseholdElectricityProfilesInCensusCells
 import egon.data.config
+
+engine = db.engine()
+data_config = egon.data.config.datasets()
 
 Base = declarative_base()
 
