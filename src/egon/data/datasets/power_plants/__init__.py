1ab999fc43a93b9c2163b0752eb075d7ad76f62d
Carlos Epia <carlosepiar@gmail.com>
2021-10-29 11:50:57 +0200

    Solar ground mounted centroid to representative_point

--- src/egon/data/datasets/power_plants/__init__.py
+++ src/egon/data/datasets/power_plants/__init__.py
@@ -52,7 +52,7 @@ class PowerPlants(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="PowerPlants",
-            version="0.0.2",
+            version="0.0.3",
             dependencies=dependencies,
             tasks=(
                 create_tables,
