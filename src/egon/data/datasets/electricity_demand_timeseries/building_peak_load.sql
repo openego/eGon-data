4dd4ca7b16eb6c8f458be9c077211486a64e25eb
nesnoj <jonathan.amme@rl-institut.de>
2021-11-18 18:40:24 +0100

    Adapt peak load SQL script to use new column names

--- src/egon/data/datasets/electricity_demand_timeseries/building_peak_load.sql
+++ src/egon/data/datasets/electricity_demand_timeseries/building_peak_load.sql
@@ -1,19 +1,18 @@
-CREATE INDEX IF NOT EXISTS osm_buildings_filtered_osm_id_idx ON openstreetmap.osm_buildings_filtered (osm_id);
 CREATE INDEX IF NOT EXISTS egon_household_electricity_profile_of_buildings_id_idx ON demand.egon_household_electricity_profile_of_buildings (cell_id);
 
-INSERT INTO demand.egon_building_peak_loads (cell_osm_ids, building_peak_load_in_wh_2035,
+INSERT INTO demand.egon_building_peak_loads (building_ids, building_peak_load_in_wh_2035,
                                              building_peak_load_in_wh_2050)
 
-SELECT d.cell_osm_ids,
+SELECT d.building_ids,
 -- 		max(d.building_load_in_kWh) as building_peak_load_in_kWh,
        cast(max(d.building_load_in_wh) * d.factor_2035 as REAL) as "building_peak_load_in_wh_2035",
        cast(max(d.building_load_in_wh) * d.factor_2050 as REAL) as "building_peak_load_in_wh_2050"
 FROM (
-         SELECT SUM(demand) as building_load_in_wh, timestep, b.cell_osm_ids, b.factor_2035, b.factor_2050
+         SELECT SUM(demand) as building_load_in_wh, timestep, b.building_ids, b.factor_2035, b.factor_2050
          FROM (
-                  SELECT t.cell_osm_ids, demand, timestep, t.factor_2035, t.factor_2050
+                  SELECT t.building_ids, demand, timestep, t.factor_2035, t.factor_2050
                   FROM (
-                           SELECT buildings.cell_osm_ids, profiles.load_in_wh, census.factor_2035, census.factor_2050
+                           SELECT buildings.building_ids, profiles.load_in_wh, census.factor_2035, census.factor_2050
                            FROM demand.egon_household_electricity_profile_in_census_cell AS census,
                                 demand.egon_household_electricity_profile_of_buildings AS buildings
 
@@ -27,7 +26,7 @@ FROM (
                        UNNEST(t.load_in_wh) WITH ORDINALITY x(demand, timestep)
               ) as b
 
-         GROUP BY cell_osm_ids, timestep, factor_2035, factor_2050
-         ORDER BY cell_osm_ids, timestep
+         GROUP BY building_ids, timestep, factor_2035, factor_2050
+         ORDER BY building_ids, timestep
      ) as d
-GROUP BY d.cell_osm_ids, d.factor_2035, d.factor_2050;
+GROUP BY d.building_ids, d.factor_2035, d.factor_2050;
