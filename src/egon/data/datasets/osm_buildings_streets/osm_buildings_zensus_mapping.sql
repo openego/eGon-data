ebe4ae869425252529233dcdabc9cc8bc79d8718
nesnoj <jonathan.amme@rl-institut.de>
2021-11-19 08:34:17 +0100

    Fix column name

--- src/egon/data/datasets/osm_buildings_streets/osm_buildings_zensus_mapping.sql
+++ src/egon/data/datasets/osm_buildings_streets/osm_buildings_zensus_mapping.sql
@@ -17,4 +17,4 @@ CREATE TABLE boundaries.egon_map_zensus_buildings_filtered as
         left join society.egon_destatis_zensus_apartment_building_population_per_ha zensus
         on ST_Within(bld.geom_point, zensus.geom)
     ) bld2
-    where bld2.osm_id is not null and bld2.grid_id is not null;
+    where bld2.id is not null and bld2.grid_id is not null;
