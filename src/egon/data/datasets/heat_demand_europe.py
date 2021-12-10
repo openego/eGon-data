b52ec92d4ee5c3ee11eecc8339f864eb3158a405
ClaraBuettner <clara.buettner@hs-flensburg.de>
2021-12-10 13:56:41 +0100

    Update datasets.yml for heat_demand_europe

--- src/egon/data/datasets/heat_demand_europe.py
+++ src/egon/data/datasets/heat_demand_europe.py
@@ -26,16 +26,14 @@ class HeatDemandEurope(Dataset):
     data_config = egon.data.config.datasets()
 
     hotmapsheatdemands_config = data_config[
-        "hotmaps_current_policy_scenario_heat_demands_buildings"][
-        "original_data"
-    ]
-
-    target_file = hotmapsheatdemands_config["target"]["path"]
+        "hotmaps_current_policy_scenario_heat_demands_buildings"]
+    
+    target_file = hotmapsheatdemands_config["targets"]["path"]
 
     def __init__(self, dependencies):
         super().__init__(
             name="heat-demands-europe",
-            version=self.target_file + "_hotmaps.0.0",
+            version=self.target_file + "_hotmaps.0.1",
             dependencies=dependencies,
             tasks=(download))
 
@@ -55,32 +53,19 @@ def download():
     -------
         None
 
-    ToDo
-    -------
-        Check version numbering method. See also class definition!
-
-    Notes
-    -----
-        (For the version management we can assume that the dataset
-        will not change, unless the code is changed.)
-
-    	Create a folder to save the downloaded csv in?
-
     """
 
     data_config = egon.data.config.datasets()
 
     # heat demands
     hotmapsheatdemands_config = data_config[
-        "hotmaps_current_policy_scenario_heat_demands_buildings"][
-        "original_data"
-    ]
+        "hotmaps_current_policy_scenario_heat_demands_buildings"]
 
-    target_file = hotmapsheatdemands_config["target"]["path"]
+    target_file = hotmapsheatdemands_config["targets"]["path"]
 
     if not os.path.isfile(target_file):
         subprocess.run(
-            f"curl { hotmapsheatdemands_config['source']['url']} > {target_file}",
+            f"curl { hotmapsheatdemands_config['sources']['url']} > {target_file}",
             shell=True,
         )
     return None
