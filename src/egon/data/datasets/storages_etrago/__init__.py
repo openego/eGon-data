5e90ffec020c50f4e0b1fcfba017d6375aa5814d
IlkaCu <ilka.cussmann@hs-flensburg.de>
2021-12-14 12:35:12 +0100

    Increase version number

--- src/egon/data/datasets/storages_etrago/__init__.py
+++ src/egon/data/datasets/storages_etrago/__init__.py
@@ -18,7 +18,7 @@ class StorageEtrago(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="StorageEtrago",
-            version="0.0.1",
+            version="0.0.2",
             dependencies=dependencies,
             tasks=(insert_PHES),
         )
