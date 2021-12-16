6d5fa8707f3184ed4eba13f2bce8afe39cd58313
AmeliaNadal <nada_am@torch>
2021-12-16 17:39:54 +0100

    Adjust version number

--- src/egon/data/datasets/gas_grid.py
+++ src/egon/data/datasets/gas_grid.py
@@ -23,7 +23,7 @@ class GasNodesandPipes(Dataset):
      def __init__(self, dependencies):
          super().__init__(
              name="GasNodesandPipes",
-             version="0.0.0",
+             version="0.0.1",
              dependencies=dependencies,
              tasks=(insert_gas_data),
          )
