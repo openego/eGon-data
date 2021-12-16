3d0c9d195aa769effbd0f36f7dde1d481625100a
ClaraBuettner <clara.buettner@hs-flensburg.de>
2021-12-16 09:39:10 +0100

    Use PHS parameters from technology-data

--- src/egon/data/datasets/scenario_parameters/parameters.py
+++ src/egon/data/datasets/scenario_parameters/parameters.py
@@ -102,14 +102,7 @@ def electricity(scenario):
 
         parameters = {
             "grid_topology": "Status Quo",
-            "re_marginal_cost_fixed": 0,
-            "phes_efficiency_store": 0.88, # according to Acatech2015
-            "phes_efficiency_dispatch": 0.89, # according to Acatech2015
-            "phes_standing_loss": 0.00052, # according to Acatech2015
-            "phes_max_hours": 6, # max_hours as an average for existing German PSH, as in open_eGo
-            "phes_control": "PV",
-            "phes_p_nom_extendable": False
-                      }
+        }
         # Insert effciencies in p.u.
         parameters["efficiency"] = {
             "oil": read_costs(costs, "oil", "efficiency"),
