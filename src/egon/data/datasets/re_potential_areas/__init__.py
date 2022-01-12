2d303aca222e181d89c9d20f5b62e06954062282
nesnoj <jonathan.amme@rl-institut.de>
2022-01-12 02:20:06 +0100

    Write only id and geom to DB tables

--- src/egon/data/datasets/re_potential_areas/__init__.py
+++ src/egon/data/datasets/re_potential_areas/__init__.py
@@ -97,7 +97,7 @@ def insert_data():
         schema = pa_config["target"].get("schema", "supply")
 
         # create database table from geopandas dataframe
-        data.to_postgis(
+        data[['id', 'geom']].to_postgis(
             table,
             engine,
             schema=schema,
