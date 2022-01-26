c5ebaad250d5bcefd3e912fd5263e9c3e3b38ed0
Francesco Witte <github@witte.sh>
2022-01-26 16:37:38 +0100

    Filter for country in bus deletion

--- src/egon/data/datasets/etrago_helpers.py
+++ src/egon/data/datasets/etrago_helpers.py
@@ -29,7 +29,7 @@ def initialise_bus_insertion(carrier, target, scenario="eGon2035"):
         f"""
         DELETE FROM {target['schema']}.{target['table']}
         WHERE scn_name = '{scenario}'
-        AND carrier = '{carrier}'
+        AND carrier = '{carrier}' AND country = 'DE'
         """
     )
 
