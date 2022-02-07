90fe6bd4e2fcab7e7fdf658f66927598d7e74526
Francesco Witte <github@witte.sh>
2022-02-07 10:26:08 +0100

    Update CHANGELOG and Dataset version

--- src/egon/data/datasets/hydrogen_etrago/__init__.py
+++ src/egon/data/datasets/hydrogen_etrago/__init__.py
@@ -50,7 +50,7 @@ class HydrogenMethaneLinkEtrago(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="HydrogenMethaneLinkEtrago",
-            version="0.0.0",
+            version="0.0.1",
             dependencies=dependencies,
             tasks=(insert_h2_to_ch4_to_h2),
         )
