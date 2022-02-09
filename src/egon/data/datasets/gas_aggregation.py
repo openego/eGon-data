f06ff0ca55a0140a9bd6f8737f481697cd5052b2
AmeliaNadal <nada_am@torch>
2022-02-09 10:47:45 +0100

    Make use of datasets.yml

--- src/egon/data/datasets/gas_aggregation.py
+++ src/egon/data/datasets/gas_aggregation.py
@@ -5,7 +5,7 @@ The central module aggregating ch4 store caverns (stores) and ch4 generators
 import pandas as pd
 import requests
 
-from egon.data import db
+from egon.data import config, db
 from egon.data.datasets import Dataset
 
 
@@ -34,29 +34,33 @@ def aggregate_gas(scn_name="eGon2035"):
     # Connect to local database
     engine = db.engine()
 
+    # Select sources and targets from dataset configuration
+    sources = config.datasets()["gas_aggregation"]["sources"]
+    targets = config.datasets()["gas_aggregation"]["targets"]
+
     components = [
         # Generator
         {
             "columns": "scn_name, generator_id, bus, p_nom, carrier",
-            "table": "egon_etrago_generator",
+            "name": "generators",
             "strategies": {
                 "scn_name": "first",
                 "generator_id": "first",
                 "p_nom": "sum",
                 "bus": "first",
-                "carrier": "first"
+                "carrier": "first",
             },
         },
         # Store
         {
             "columns": "scn_name, store_id, bus, e_nom, carrier",
-            "table": "egon_etrago_store",
+            "name": "stores",
             "strategies": {
                 "scn_name": "first",
                 "store_id": "first",
                 "e_nom": "sum",
                 "bus": "first",
-                "carrier": "first"
+                "carrier": "first",
             },
         },
     ]
@@ -64,7 +68,7 @@ def aggregate_gas(scn_name="eGon2035"):
     for comp in components:
         df = db.select_dataframe(
             f"""SELECT {comp["columns"]}
-                    FROM grid.{comp["table"]}
+                    FROM {sources[comp["name"]]['schema']}.{targets[comp["name"]]['table']}
                     WHERE scn_name = '{scn_name}' 
                     AND carrier = 'CH4';"""
         )
@@ -74,16 +78,16 @@ def aggregate_gas(scn_name="eGon2035"):
 
         # Clean table
         db.execute_sql(
-            f"""DELETE FROM grid.{comp["table"]} 
+            f"""DELETE FROM {sources[comp["name"]]['schema']}.{targets[comp["name"]]['table']} 
                         WHERE carrier = 'CH4'
                         AND scn_name = '{scn_name}';"""
         )
 
         # Insert data to db
         df.to_sql(
-            comp["table"],
+            targets[comp["name"]]["table"],
             engine,
-            schema="grid",
+            schema=sources[comp["name"]]["schema"],
             index=False,
             if_exists="append",
         )
