f0383bfea01cc897706abdd85657bfa9aac30792
nesnoj <jonathan.amme@rl-institut.de>
2022-02-21 17:25:16 +0100

    Bump HH buidings' dataset version

--- src/egon/data/datasets/electricity_demand_timeseries/hh_buildings.py
+++ src/egon/data/datasets/electricity_demand_timeseries/hh_buildings.py
@@ -821,7 +821,7 @@ def map_houseprofiles_to_buildings():
 setup = partial(
     Dataset,
     name="Demand_Building_Assignment",
-    version="0.0.1",
+    version="0.0.2",
     dependencies=[],
     tasks=(map_houseprofiles_to_buildings, get_building_peak_loads),
 )
