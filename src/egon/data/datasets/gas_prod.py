5423a9901df432682c3e471a968cb3573fe08008
AmeliaNadal <nada_am@torch>
2022-03-04 11:35:27 +0100

    Increase dataset version

--- src/egon/data/datasets/gas_prod.py
+++ src/egon/data/datasets/gas_prod.py
@@ -20,7 +20,7 @@ class CH4Production(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="CH4Production",
-            version="0.0.5",
+            version="0.0.6",
             dependencies=dependencies,
             tasks=(import_gas_generators),
         )
