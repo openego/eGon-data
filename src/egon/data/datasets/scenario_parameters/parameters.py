0721cd1ed5ddff8ad7fc57c169b602584a6021f2
IlkaCu <ilka.cussmann@hs-flensburg.de>
2022-04-25 14:19:46 +0200

    Add VOM for gas chp

--- src/egon/data/datasets/scenario_parameters/parameters.py
+++ src/egon/data/datasets/scenario_parameters/parameters.py
@@ -391,6 +391,7 @@ def gas(scenario):
             * global_settings(scenario)["co2_emissions"]["gas"],
             "OCGT": read_costs(costs, "OCGT", "VOM"),
             "biogas": global_settings(scenario)["fuel_costs"]["gas"],
+            "chp_gas": read_costs(costs, "central gas chp", "VOM"),
         }
 
         # Insert max gas production (generator) over the year
