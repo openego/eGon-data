8b7cb81d386ce4a8c442f4e5adafb38a56762347
AmeliaNadal <nada_am@torch>
2022-06-07 18:06:04 +0200

    Add retrofitted_capacity_share parameter to the scenario parameters

--- src/egon/data/datasets/scenario_parameters/parameters.py
+++ src/egon/data/datasets/scenario_parameters/parameters.py
@@ -552,6 +552,7 @@ def gas(scenario):
         parameters = {
             "main_gas_carrier": "H2",
             "retrofitted_CH4pipeline-to-H2pipeline_share": 0.75,
+            "retrofitted_capacity_share": 0.8,
         }
         # Insert effciencies in p.u.
         parameters["efficiency"] = {
