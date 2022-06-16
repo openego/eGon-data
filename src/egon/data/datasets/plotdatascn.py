6fa17c9c29acc9034adf92ec5686fddbd7fd66af
alonsoju95 <alonsoju@gmail.com>
2022-06-16 14:25:58 +0200

    plots for generation-installed capacity for eGon2035 and eGon100RE

--- src/egon/data/datasets/plotdatascn.py
+++ src/egon/data/datasets/plotdatascn.py
@@ -58,10 +58,65 @@ Carriers = pd.read_sql(sqlCarrier,con)
 Carriers = Carriers.loc[Carriers['scn_name'] == 'eGon2035']
 Carriers = Carriers.set_index("bus")
 
-#Solar rooftop 
-SolarRooftop = Carriers.loc[Carriers['carrier'] == 'solar_rooftop']
-SolarRooftopGEO = pd.merge(SolarRooftop, distr, on ='bus')
+Carrier= Carriers.loc[Carriers['carrier'] == 'wind_onshore']
 
+Merge= pd.merge(Carrier, distr, on ='bus', how="outer")
+#Merge=Merge['p_nom'] = Merge['p_nom'].fillna(0)
+Eliminate= Merge.loc[Merge ["carrier"] != "wind_onshore", "p_nom" ] = 0
+
+
+
+
+gdf = gpd.GeoDataFrame(Merge , geometry='geom')
+
+fig, ax = plt.subplots(figsize=(10,10))
+ax.set_axis_off();
+plt.title("SolarCapacity in Germany")
+gdf.plot(column='p_nom', ax=ax, legend=True,  legend_kwds={'label': "p_nom",
+
+                        'orientation': "vertical"})
+
+
+
+
+
+Merge= pd.merge(Carriers, distr, on ='bus')
+
+Eliminate= Merge.loc[Merge ["carrier"] != "wind_onshore", "p_nom" ] = 0
+
+gdf = gpd.GeoDataFrame(Merge , geometry='geom')
+
+
+fig, ax = plt.subplots(figsize=(10,10))
+ax.set_axis_off();
+plt.title("SolarCapacity in Germany")
+gdf.plot(column='p_nom', ax=ax, legend=True,  legend_kwds={'label': "p_nom",
+
+                        'orientation': "vertical"})
+
+
+CarrierGEO= Merge.loc[Merge['carrier'] == 'wind_onshore']
+
+gdf = gpd.GeoDataFrame(CarrierGEO , geometry='geom')
+
+
+
+fig, ax = plt.subplots(figsize=(10,10))
+ax.set_axis_off();
+plt.title("SolarCapacity in Germany")
+gdf.plot(column='p_nom', ax=ax, legend=True,  legend_kwds={'label': "p_nom",
+
+                        'orientation': "vertical"})
+
+
+
+
+
+#Solar rooftop 
+SolarRooftop = Carriers.loc[Carriers['carrier'] == 'wind_onshore']
+#SolarRooftopGEO = pd.merge(SolarRooftop, distr, on ='bus', how="left").fillna(value="0")
+
+SolarRooftopGEO = pd.merge(SolarRooftop, distr, on ='bus')
 
 
 gdf = gpd.GeoDataFrame(SolarRooftopGEO , geometry='geom')
@@ -69,14 +124,75 @@ gdf = gpd.GeoDataFrame(SolarRooftopGEO , geometry='geom')
 
 
 fig, ax = plt.subplots(figsize=(10,10))
-
 ax.set_axis_off();
+plt.title("SolarCapacity in Germany")
+gdf.plot(column='p_nom', ax=ax, legend=True,  legend_kwds={'label': "p_nom",
 
-gdf.plot(column='p_nom', ax=ax, legend=True)
+                        'orientation': "vertical"})
+
+
+# import logging
+# import os
+from matplotlib import pyplot as plt
+import matplotlib.patches as mpatches
+import matplotlib
+#import pandas as pd
+#import numpy as np
+#from math import sqrt, log10
+#from pyproj import Proj, transform
+# import tilemapbase
+#import geopandas as gpd
+#from egon.data import db
+#from egon.data.config import settings
+#from egon.data.datasets import Dataset
+#import egon.data.config
+import pandas as pd
+from egon.data import db
+from egon.data.datasets import Dataset
+import egon.data.config
+import geopandas as gpd
+
+def plot_generation(
+              carrier,scenario
+            ):
+    con = db.engine()
+    
+    sql = "SELECT bus_id, geom FROM grid.egon_mv_grid_district"
+    distr = gpd.GeoDataFrame.from_postgis(sql, con)
+    distr = distr.rename({'bus_id': 'bus'},axis=1)
+    distr = distr.set_index("bus")
+    
+    sqlCarrier = "SELECT carrier, p_nom, bus FROM grid.egon_etrago_generator"
+    sqlCarrier = "SELECT * FROM grid.egon_etrago_generator"
+    Carriers = pd.read_sql(sqlCarrier,con)
+    Carriers = Carriers.loc[Carriers['scn_name'] == scenario]
+    Carriers = Carriers.set_index("bus")
+
+     
+    CarrierGen = Carriers.loc[Carriers['carrier'] == carrier]
+    CarrierGenGEO = pd.merge(CarrierGen, distr, on ='bus')
 
 
 
+    gdf = gpd.GeoDataFrame(CarrierGenGEO , geometry='geom')
 
+
+
+    fig, ax = plt.subplots(figsize=(10,10))
+    ax.set_axis_off();
+    plt.title(carrier)
+    gdf.plot(column='p_nom', ax=ax, legend=True,  legend_kwds={'label': "p_nom",
+
+                            'orientation': "vertical"})
+    return 0
+    plot_generation(carrier, scenario)
+    
+    
+    
+    
+    
+    
+   
 
 
 #Carriers.rename(columns={"bus": "bus_id"})
