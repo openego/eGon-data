6e3a3d9e01b6d2c5ff093e001f8b56ce2d50393e
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2022-06-30 10:46:00 +0200

    added timers

--- src/egon/data/datasets/power_plants/pv_rooftop_buildings.py
+++ src/egon/data/datasets/power_plants/pv_rooftop_buildings.py
@@ -209,9 +209,7 @@ def timer_func(func):
         result = func(*args, **kwargs)
         end_time = perf_counter()
         total_time = end_time - start_time
-        print(
-            f"Function {func.__name__}{args} {kwargs} Took {total_time:.4f} seconds"
-        )
+        print(f"Function {func.__name__} took {total_time:.4f} seconds.")
         return result
 
     return timeit_wrapper
