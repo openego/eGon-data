ffb571028a2b6839fc079bf7f2455e8290748963
Julian.Endres <julian.endres@rl-institut.de>
2022-06-30 17:42:32 +0200

    Add functions to tool

--- src/egon/data/datasets/electricity_demand_timeseries/tools.py
+++ src/egon/data/datasets/electricity_demand_timeseries/tools.py
@@ -34,3 +34,26 @@ def random_point_in_square(geom, tol):
     points = gpd.GeoSeries(points, crs="epsg:3035")
 
     return points
+
+
+# distribute amenities evenly
+def specific_int_until_sum(s_sum, i_int):
+    """
+    Generate list `i_int` summing to `s_sum`. Last value will be <= `i_int`
+    """
+    list_i = [] if [s_sum % i_int] == [0] else [s_sum % i_int]
+    list_i += s_sum // i_int * [i_int]
+    return list_i
+
+
+def random_ints_until_sum(s_sum, m_max):
+    """
+    Generate non-negative random integers < `m_max` summing to `s_sum`.
+    """
+    list_r = []
+    while s_sum > 0:
+        r = np.random.randint(1, m_max + 1)
+        r = r if r <= m_max and r < s_sum else s_sum
+        list_r.append(r)
+        s_sum -= r
+    return list_r
