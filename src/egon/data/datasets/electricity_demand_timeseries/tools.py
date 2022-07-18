a296b1efcde161f6a0849ff7ee5f11475750b372
Julian.Endres <julian.endres@rl-institut.de>
2022-07-18 16:39:00 +0200

    Add two tabel to db functions

--- src/egon/data/datasets/electricity_demand_timeseries/tools.py
+++ src/egon/data/datasets/electricity_demand_timeseries/tools.py
@@ -4,6 +4,9 @@ import numpy as np
 import pandas as pd
 
 
+from egon.data import db
+engine = db.engine()
+
 def random_point_in_square(geom, tol):
     """
     Generate a random point within a square
@@ -57,3 +60,55 @@ def random_ints_until_sum(s_sum, m_max):
         list_r.append(r)
         s_sum -= r
     return list_r
+
+
+def write_table_to_postgis(df, table, drop=True):
+    """
+    Append table
+    """
+
+    # Only take existing columns
+    columns = [
+        column.key for column in table.__table__.columns
+    ]
+
+    # Only take existing columns
+    df = df.loc[:, columns]
+
+    if drop:
+        table.__table__.drop(bind=engine, checkfirst=True)
+        table.__table__.create(bind=engine)
+
+    dtypes = {i: table.__table__.columns[i].type for i in table.__table__.columns.keys()}
+
+    # Write new buildings incl coord into db
+    df.to_postgis(
+        name=table.__tablename__,
+        con=engine,
+        if_exists="append",
+        schema=table.__table_args__["schema"],
+        dtype=dtypes,
+    )
+
+
+def write_table_to_postgres(df, table, drop=True):
+    """"""
+
+    # Only take existing columns
+    columns = [
+        column.key for column in table.__table__.columns
+    ]
+
+    # Only take existing columns
+    df = df.loc[:, columns]
+
+    if drop:
+        table.__table__.drop(bind=engine, checkfirst=True)
+        table.__table__.create(bind=engine)
+
+    # Write peak loads into db
+    with db.session_scope() as session:
+        session.bulk_insert_mappings(
+            table,
+            df.to_dict(orient="records"),
+        )
