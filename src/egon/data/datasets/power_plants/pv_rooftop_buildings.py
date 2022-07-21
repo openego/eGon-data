0f48a68fbf087794f465cf6563e11ef41e3e4f46
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2022-07-21 11:24:51 +0200

    minor change

--- src/egon/data/datasets/power_plants/pv_rooftop_buildings.py
+++ src/egon/data/datasets/power_plants/pv_rooftop_buildings.py
@@ -1362,7 +1362,7 @@ def scenario_data(
         query.statement, query.session.bind, index_col="index"
     ).sort_index()
 
-    logger.debug(f"Scenario capacity data loaded. {df}")
+    logger.debug("Scenario capacity data loaded.")
 
     return df
 
