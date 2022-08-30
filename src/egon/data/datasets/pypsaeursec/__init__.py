b495188d6b18ab21204cbb352ffe01c33c0e1bd2
IlkaCu <ilka.cussmann@hs-flensburg.de>
2022-08-30 16:27:46 +0200

    Move if statement to function

--- src/egon/data/datasets/pypsaeursec/__init__.py
+++ src/egon/data/datasets/pypsaeursec/__init__.py
@@ -22,111 +22,116 @@ import egon.data.subprocess as subproc
 
 
 def run_pypsa_eur_sec():
+    
+    # Skip execution of pypsa-eur-sec by default until optional task is implemented
+    execute_pypsa_eur_sec = False
 
-    cwd = Path(".")
-    filepath = cwd / "run-pypsa-eur-sec"
-    filepath.mkdir(parents=True, exist_ok=True)
+    if execute_pypsa_eur_sec:
+        
+        cwd = Path(".")
+        filepath = cwd / "run-pypsa-eur-sec"
+        filepath.mkdir(parents=True, exist_ok=True)
 
-    pypsa_eur_repos = filepath / "pypsa-eur"
-    pypsa_eur_repos_data = pypsa_eur_repos / "data"
-    technology_data_repos = filepath / "technology-data"
-    pypsa_eur_sec_repos = filepath / "pypsa-eur-sec"
-    pypsa_eur_sec_repos_data = pypsa_eur_sec_repos / "data"
+        pypsa_eur_repos = filepath / "pypsa-eur"
+        pypsa_eur_repos_data = pypsa_eur_repos / "data"
+        technology_data_repos = filepath / "technology-data"
+        pypsa_eur_sec_repos = filepath / "pypsa-eur-sec"
+        pypsa_eur_sec_repos_data = pypsa_eur_sec_repos / "data"
 
-    if not pypsa_eur_repos.exists():
-        subproc.run(
-            [
-                "git",
-                "clone",
-                "--branch",
-                "v0.4.0",
-                "https://github.com/PyPSA/pypsa-eur.git",
-                pypsa_eur_repos,
-            ]
-        )
-
-        # subproc.run(
-        #     ["git", "checkout", "4e44822514755cdd0289687556547100fba6218b"],
-        #     cwd=pypsa_eur_repos,
-        # )
-
-        file_to_copy = os.path.join(
-            __path__[0], "datasets", "pypsaeursec", "pypsaeur", "Snakefile"
-        )
-
-        subproc.run(["cp", file_to_copy, pypsa_eur_repos])
-
-        # Read YAML file
-        path_to_env = pypsa_eur_repos / "envs" / "environment.yaml"
-        with open(path_to_env, "r") as stream:
-            env = yaml.safe_load(stream)
-
-        env["dependencies"].append("gurobi")
-
-        # Write YAML file
-        with open(path_to_env, "w", encoding="utf8") as outfile:
-            yaml.dump(
-                env, outfile, default_flow_style=False, allow_unicode=True
+        if not pypsa_eur_repos.exists():
+            subproc.run(
+                [
+                    "git",
+                    "clone",
+                    "--branch",
+                    "v0.4.0",
+                    "https://github.com/PyPSA/pypsa-eur.git",
+                    pypsa_eur_repos,
+                ]
             )
 
-        datafile = "pypsa-eur-data-bundle.tar.xz"
-        datapath = pypsa_eur_repos / datafile
+            # subproc.run(
+            #     ["git", "checkout", "4e44822514755cdd0289687556547100fba6218b"],
+            #     cwd=pypsa_eur_repos,
+            # )
+
+            file_to_copy = os.path.join(
+                __path__[0], "datasets", "pypsaeursec", "pypsaeur", "Snakefile"
+            )
+
+            subproc.run(["cp", file_to_copy, pypsa_eur_repos])
+
+            # Read YAML file
+            path_to_env = pypsa_eur_repos / "envs" / "environment.yaml"
+            with open(path_to_env, "r") as stream:
+                env = yaml.safe_load(stream)
+
+            env["dependencies"].append("gurobi")
+
+            # Write YAML file
+            with open(path_to_env, "w", encoding="utf8") as outfile:
+                yaml.dump(
+                    env, outfile, default_flow_style=False, allow_unicode=True
+                )
+
+            datafile = "pypsa-eur-data-bundle.tar.xz"
+            datapath = pypsa_eur_repos / datafile
+            if not datapath.exists():
+                urlretrieve(
+                    f"https://zenodo.org/record/3517935/files/{datafile}", datapath
+                )
+                tar = tarfile.open(datapath)
+                tar.extractall(pypsa_eur_repos_data)
+
+        if not technology_data_repos.exists():
+            subproc.run(
+                [
+                    "git",
+                    "clone",
+                    "--branch",
+                    "v0.3.0",
+                    "https://github.com/PyPSA/technology-data.git",
+                    technology_data_repos,
+                ]
+            )
+
+        if not pypsa_eur_sec_repos.exists():
+            subproc.run(
+                [
+                    "git",
+                    "clone",
+                    "https://github.com/openego/pypsa-eur-sec.git",
+                    pypsa_eur_sec_repos,
+                ]
+            )
+
+        datafile = "pypsa-eur-sec-data-bundle.tar.gz"
+        datapath = pypsa_eur_sec_repos_data / datafile
         if not datapath.exists():
             urlretrieve(
-                f"https://zenodo.org/record/3517935/files/{datafile}", datapath
+                f"https://zenodo.org/record/5824485/files/{datafile}", datapath
             )
             tar = tarfile.open(datapath)
-            tar.extractall(pypsa_eur_repos_data)
+            tar.extractall(pypsa_eur_sec_repos_data)
 
-    if not technology_data_repos.exists():
+        with open(filepath / "Snakefile", "w") as snakefile:
+            snakefile.write(
+                resources.read_text("egon.data.datasets.pypsaeursec", "Snakefile")
+            )
+
         subproc.run(
             [
-                "git",
-                "clone",
-                "--branch",
-                "v0.3.0",
-                "https://github.com/PyPSA/technology-data.git",
-                technology_data_repos,
+                "snakemake",
+                "-j1",
+                "--directory",
+                filepath,
+                "--snakefile",
+                filepath / "Snakefile",
+                "--use-conda",
+                "--conda-frontend=conda",
+                "Main",
             ]
         )
-
-    if not pypsa_eur_sec_repos.exists():
-        subproc.run(
-            [
-                "git",
-                "clone",
-                "https://github.com/openego/pypsa-eur-sec.git",
-                pypsa_eur_sec_repos,
-            ]
-        )
-
-    datafile = "pypsa-eur-sec-data-bundle.tar.gz"
-    datapath = pypsa_eur_sec_repos_data / datafile
-    if not datapath.exists():
-        urlretrieve(
-            f"https://zenodo.org/record/5824485/files/{datafile}", datapath
-        )
-        tar = tarfile.open(datapath)
-        tar.extractall(pypsa_eur_sec_repos_data)
-
-    with open(filepath / "Snakefile", "w") as snakefile:
-        snakefile.write(
-            resources.read_text("egon.data.datasets.pypsaeursec", "Snakefile")
-        )
-
-    subproc.run(
-        [
-            "snakemake",
-            "-j1",
-            "--directory",
-            filepath,
-            "--snakefile",
-            filepath / "Snakefile",
-            "--use-conda",
-            "--conda-frontend=conda",
-            "Main",
-        ]
-    )
 
 
 def read_network():
@@ -965,21 +970,11 @@ def neighbor_reduction():
             index_label="line_id",
         )
 
-
-# Skip execution of pypsa-eur-sec by default until optional task is implemented
-execute_pypsa_eur_sec = False
-
-if execute_pypsa_eur_sec:
-    tasks = (run_pypsa_eur_sec, clean_database, neighbor_reduction)
-else:
-    tasks = (clean_database, neighbor_reduction)
-
-
 class PypsaEurSec(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="PypsaEurSec",
-            version="0.0.7",
+            version="0.0.8",
             dependencies=dependencies,
-            tasks=tasks,
+            tasks=(run_pypsa_eur_sec, clean_database, neighbor_reduction),
         )
