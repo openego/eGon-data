428814047206c783a7dd81ce0e8e04d7c8ab0850
Julian.Endres <julian.endres@rl-institut.de>
2022-09-06 12:28:22 +0200

    Bump version number to 4

--- src/egon/data/datasets/sanity_checks.py
+++ src/egon/data/datasets/sanity_checks.py
@@ -16,7 +16,7 @@ class SanityChecks(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="SanityChecks",
-            version="0.0.3",
+            version="0.0.4",
             dependencies=dependencies,
             tasks={
                 etrago_eGon2035_electricity,
