c91a0d08042eb6261c17015b323c05205981b303
AmeliaNadal <nada_am@torch>
2022-10-11 11:56:19 +0200

    Improve documentation

--- src/egon/data/datasets/gas_stores_germany.py
+++ src/egon/data/datasets/gas_stores_germany.py
@@ -168,11 +168,11 @@ def import_gas_grid_capacity(scn_name, carrier):
     Define dataframe containing the modelling of the grid storage
     capacity. The whole storage capacity of the grid (130000 MWh,
     estimation of the Bundesnetzagentur) is split uniformly between
-    all the german gas nodes of the grid.
-    In eGon100RE, the storage capacity of the grid is slip between H2
+    all the german gas nodes of the grid (without consideration of the
+    capacities of the pipes).
+    In eGon100RE, the storage capacity of the grid is split between H2
     and CH4 stores, with the same share than the pipes capacity (value
     calculated in the p-e-s run).
-    The capacities of the pipes are not considerated.
 
     Parameters
     ----------
