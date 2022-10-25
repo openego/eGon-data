f0e17de32a4202f349bbdabcd14863f313e34c10
Julian.Endres <julian.endres@rl-institut.de>
2022-10-26 00:32:04 +0200

    Fix typo

--- src/egon/data/datasets/heat_supply/individual_heating.py
+++ src/egon/data/datasets/heat_supply/individual_heating.py
@@ -1626,7 +1626,7 @@ def determine_hp_cap_peak_load_mvgd_ts_2035(mvgd_ids):
 
     logger.info(
         f"Dropped duplicated buildings: "
-        f"{duplicates.loc['building_id', 'hp_capacity']}"
+        f"{duplicates.loc[:,['building_id', 'hp_capacity']]}"
     )
 
     df_hp_cap_per_building_2035_db.drop_dupliactes("building_id", inplace=True)
