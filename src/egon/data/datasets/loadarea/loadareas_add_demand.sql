da07eb6f1ef3449959964190bf9e059995cb3ebb
nesnoj <jonathan.amme@rl-institut.de>
2022-11-08 21:37:45 +0100

    Add industrial peak load
    Use new demand and peak load tables from #1001

--- src/egon/data/datasets/loadarea/loadareas_add_demand.sql
+++ src/egon/data/datasets/loadarea/loadareas_add_demand.sql
@@ -123,34 +123,64 @@ UPDATE demand.egon_loadarea AS t1
         ) AS t2
     WHERE   t1.id = t2.id;
 
--- Add industrial consumption
+-- Add industrial consumption and peak load
+-- 1) Industry from OSM landuse areas
 UPDATE demand.egon_loadarea AS t1
-    SET sector_consumption_industrial_2035 = t2.demand
+    SET
+        sector_peakload_industrial_2035 = t2.peak_load,
+        sector_consumption_industrial_2035 = t2.demand
     FROM (
         SELECT  a.id AS id,
-                SUM(b.demand)::float AS demand
+                SUM(b.demand)::float AS demand,
+                SUM(b.peak_load)::float AS peak_load,
         FROM    demand.egon_loadarea AS a,
                 (
                     SELECT
-                        sum(dem.demand) as demand,
-					    ST_PointOnSurface(osm.geom) AS geom_surfacepoint
+                        sum(ind_osm.demand) as demand,
+                        sum(ind_osm.peak_load) as peak_load,
+                        ST_PointOnSurface(landuse.geom) AS geom_surfacepoint
                     FROM
-                        openstreetmap.osm_landuse as osm,
-                        demand.egon_demandregio_osm_ind_electricity as dem
+                        openstreetmap.osm_landuse as landuse,
+                        demand.egon_osm_ind_load_curves_individual as ind_osm
                     WHERE
-                        dem.scenario = 'eGon2035' AND
-                        dem.osm_id = osm.id
-                    GROUP BY osm.id
+                        ind_osm.scn_name = 'eGon2035' AND
+                        ind_osm.osm_id = landuse.id
+                    GROUP BY landuse.id
                 ) AS b
         WHERE   a.geom && b.geom_surfacepoint AND
                 ST_CONTAINS(a.geom, b.geom_surfacepoint)
-        GROUP BY a.id
+        GROUP BY a.id;
         ) AS t2
     WHERE   t1.id = t2.id;
 
-
--- Add industrial peak load
--- TBD!
+-- 2) Industry from industrial sites
+UPDATE demand.egon_loadarea AS t1
+    SET
+        sector_peakload_industrial_2035 = sector_peakload_industrial_2035 + t2.peak_load,
+        sector_consumption_industrial_2035 = sector_consumption_industrial_2035 + t2.demand
+    FROM (
+        SELECT  a.id AS id,
+                SUM(b.demand)::float AS demand,
+                SUM(b.peak_load)::float AS peak_load
+        FROM    demand.egon_loadarea AS a,
+                (
+                    SELECT
+                        ind_sites.id,
+                        ind_loads.demand,
+                        ind_loads.peak_load,
+                        ind_sites.geom
+                    FROM
+                        demand.egon_industrial_sites as ind_sites,
+                        demand.egon_sites_ind_load_curves_individual as ind_loads
+                    WHERE
+                        ind_loads.scn_name = 'eGon2035' AND
+                        ind_loads.site_id = ind_sites.id
+                ) AS b
+        WHERE   a.geom && b.geom AND
+                ST_CONTAINS(a.geom, b.geom)
+        GROUP BY a.id;
+        ) AS t2
+    WHERE   t1.id = t2.id;
 
 -------------------------
 -- Scenario: eGon100RE --
@@ -293,5 +323,61 @@ UPDATE demand.egon_loadarea AS t1
         ) AS t2
     WHERE   t1.id = t2.id;
 
--- Add industrial peak load
--- TBD!
+-- Add industrial consumption and peak load
+-- 1) Industry from OSM landuse areas
+UPDATE demand.egon_loadarea AS t1
+    SET
+        sector_peakload_industrial_2050 = t2.peak_load,
+        sector_consumption_industrial_2050 = t2.demand
+    FROM (
+        SELECT  a.id AS id,
+                SUM(b.demand)::float AS demand,
+                SUM(b.peak_load)::float AS peak_load,
+        FROM    demand.egon_loadarea AS a,
+                (
+                    SELECT
+                        sum(ind_osm.demand) as demand,
+                        sum(ind_osm.peak_load) as peak_load,
+                        ST_PointOnSurface(landuse.geom) AS geom_surfacepoint
+                    FROM
+                        openstreetmap.osm_landuse as landuse,
+                        demand.egon_osm_ind_load_curves_individual as ind_osm
+                    WHERE
+                        ind_osm.scn_name = 'eGon100RE' AND
+                        ind_osm.osm_id = landuse.id
+                    GROUP BY landuse.id
+                ) AS b
+        WHERE   a.geom && b.geom_surfacepoint AND
+                ST_CONTAINS(a.geom, b.geom_surfacepoint)
+        GROUP BY a.id;
+        ) AS t2
+    WHERE   t1.id = t2.id;
+
+-- 2) Industry from industrial sites
+UPDATE demand.egon_loadarea AS t1
+    SET
+        sector_peakload_industrial_2050 = sector_peakload_industrial_2050 + t2.peak_load,
+        sector_consumption_industrial_2050 = sector_consumption_industrial_2050 + t2.demand
+    FROM (
+        SELECT  a.id AS id,
+                SUM(b.demand)::float AS demand,
+                SUM(b.peak_load)::float AS peak_load
+        FROM    demand.egon_loadarea AS a,
+                (
+                    SELECT
+                        ind_sites.id,
+                        ind_loads.demand,
+                        ind_loads.peak_load,
+                        ind_sites.geom
+                    FROM
+                        demand.egon_industrial_sites as ind_sites,
+                        demand.egon_sites_ind_load_curves_individual as ind_loads
+                    WHERE
+                        ind_loads.scn_name = 'eGon100RE' AND
+                        ind_loads.site_id = ind_sites.id
+                ) AS b
+        WHERE   a.geom && b.geom AND
+                ST_CONTAINS(a.geom, b.geom)
+        GROUP BY a.id;
+        ) AS t2
+    WHERE   t1.id = t2.id;
