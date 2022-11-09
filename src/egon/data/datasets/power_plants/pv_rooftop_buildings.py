774af46e56d2febd9b0f75c71d26d771d59b1043
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2022-11-09 09:57:50 +0100

    add check

--- src/egon/data/datasets/power_plants/pv_rooftop_buildings.py
+++ src/egon/data/datasets/power_plants/pv_rooftop_buildings.py
@@ -1181,9 +1181,7 @@ def allocate_pv(
             assert len_build == len(gens)
 
         for quant in gens.quant.unique():
-            q_buildings = buildings.loc[
-                (buildings.quant == quant) & (buildings.gens_id.isna())
-            ]
+            q_buildings = buildings.loc[buildings.quant == quant]
             q_gens = gens.loc[gens.quant == quant]
 
             len_build = len(q_buildings)
@@ -1225,6 +1223,7 @@ def allocate_pv(
 
             # q_mastr_gdf.loc[q_gens.index, "building_id"] = chosen_buildings
             q_buildings_gdf.loc[chosen_buildings, "gens_id"] = q_gens.index
+            buildings = buildings.drop(chosen_buildings)
 
         if count % 100 == 0:
             logger.debug(
