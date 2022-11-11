eb2a5e2bfaeab836c4b8ba70d74121c1d5f7902b
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2022-11-11 09:22:12 +0100

    version id

--- src/egon/data/datasets/power_plants/__init__.py
+++ src/egon/data/datasets/power_plants/__init__.py
@@ -49,7 +49,7 @@ class PowerPlants(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="PowerPlants",
-            version="0.0.12dev",
+            version="0.0.13dev",
             dependencies=dependencies,
             tasks=(
                 create_tables,
