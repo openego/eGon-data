df10d4c647be4eea17a9276927eb5312a08d0fc5
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2022-11-17 13:13:12 +0100

    change dataset version

--- src/egon/data/datasets/power_plants/__init__.py
+++ src/egon/data/datasets/power_plants/__init__.py
@@ -49,7 +49,7 @@ class PowerPlants(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="PowerPlants",
-            version="0.0.14dev",
+            version="0.0.15dev",
             dependencies=dependencies,
             tasks=(
                 create_tables,
