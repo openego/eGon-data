21c2f57abf3b9ff6201d6fbd69f173188041bfc9
nesnoj <jonathan.amme@rl-institut.de>
2022-11-28 12:15:33 +0100

    Add table definitions for pv, wind, biomass and hydro

--- src/egon/data/datasets/power_plants/__init__.py
+++ src/egon/data/datasets/power_plants/__init__.py
@@ -1,7 +1,9 @@
 """The central module containing all code dealing with power plant data.
 """
 from geoalchemy2 import Geometry
-from sqlalchemy import BigInteger, Column, Float, Integer, Sequence, String
+from sqlalchemy import (
+    BigInteger, Column, Float, Integer, Sequence, String, DateTime, Boolean
+)
 from sqlalchemy.dialects.postgresql import JSONB
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
@@ -28,6 +30,114 @@ import egon.data.datasets.power_plants.wind_farms as wind_onshore
 import egon.data.datasets.power_plants.wind_offshore as wind_offshore
 
 Base = declarative_base()
+
+
+class EgonPowerPlantsPv(Base):
+    __tablename__ = "egon_power_plants_pv"
+    __table_args__ = {"schema": "supply"}
+
+    id = Column(Integer, Sequence("pp_pv_seq"), primary_key=True)
+    bus_id = Column(Integer, nullable=True) # Grid district id
+    gens_id = Column(String, nullable=True) # EinheitMastrNummer
+
+    status = Column(String, nullable=True)  # EinheitBetriebsstatus
+    commissioning_date = Column(DateTime, nullable=True) # Inbetriebnahmedatum
+    postcode = Column(String(5), nullable=True) # Postleitzahl
+    city = Column(String(50), nullable=True) # Ort
+    federal_state = Column(String(21), nullable=True) # Bundesland
+
+    site_type = Column(String(69), nullable=True)  # Lage
+    usage_sector = Column(String(36), nullable=True) # Nutzungsbereich
+    orientation_primary = Column(String(11), nullable=True) # Hauptausrichtung
+    orientation_primary_angle = Column(String(18), nullable=True)  # HauptausrichtungNeigungswinkel
+    orientation_secondary = Column(String(11), nullable=True) # Nebenausrichtung
+    orientation_secondary_angle = Column(String(18), nullable=True)  # NebenausrichtungNeigungswinkel
+    orientation_uniform = Column(Boolean, nullable=True) # EinheitlicheAusrichtungUndNeigungswinkel
+    module_count = Column(Float, nullable=True)  # AnzahlModule
+
+    capacity = Column(Float) # Nettonennleistung
+    capacity_inverter = Column(Float, nullable=True) # ZugeordneteWirkleistungWechselrichter in MW
+    feedin_type = Column(String(47), nullable=True) # Einspeisungsart
+    voltage_level = Column(Integer)
+
+    geom = Column(Geometry("POINT", 4326), index=True)
+
+
+class EgonPowerPlantsWind(Base):
+    __tablename__ = "egon_power_plants_wind"
+    __table_args__ = {"schema": "supply"}
+
+    id = Column(Integer, Sequence("pp_wind_seq"), primary_key=True)
+    bus_id = Column(Integer, nullable=True) # Grid district id
+    gens_id = Column(String, nullable=True) # EinheitMastrNummer
+
+    status = Column(String, nullable=True)  # EinheitBetriebsstatus
+    commissioning_date = Column(DateTime, nullable=True) # Inbetriebnahmedatum
+    postcode = Column(String(5), nullable=True) # Postleitzahl
+    city = Column(String(50), nullable=True) # Ort
+    federal_state = Column(String(21), nullable=True) # Bundesland
+
+    site_type = Column(String(17), nullable=True) # Lage
+    manufacturer_name = Column(String(100), nullable=True) # Hersteller
+    type_name = Column(String(100), nullable=True) # Typenbezeichnung
+    hub_height = Column(Float, nullable=True) # Nabenhoehe
+    rotor_diameter = Column(Float, nullable=True) # Rotordurchmesser
+
+    capacity = Column(Float) # Nettonennleistung
+    feedin_type = Column(String(47), nullable=True)  # Einspeisungsart
+    voltage_level = Column(Integer)
+
+    geom = Column(Geometry("POINT", 4326), index=True)
+
+
+class EgonPowerPlantsBiomass(Base):
+    __tablename__ = "egon_power_plants_biomass"
+    __table_args__ = {"schema": "supply"}
+
+    id = Column(Integer, Sequence("pp_biomass_seq"), primary_key=True)
+    bus_id = Column(Integer, nullable=True) # Grid district id
+    gens_id = Column(String, nullable=True) # EinheitMastrNummer
+
+    status = Column(String, nullable=True)  # EinheitBetriebsstatus
+    commissioning_date = Column(DateTime, nullable=True) # Inbetriebnahmedatum
+    postcode = Column(String(5), nullable=True) # Postleitzahl
+    city = Column(String(50), nullable=True) # Ort
+    federal_state = Column(String(21), nullable=True) # Bundesland
+
+    technology = Column(String(45), nullable=True) # Technologie
+    fuel_name = Column(String(52), nullable=True) # Hauptbrennstoff
+    fuel_type = Column(String(19), nullable=True)  # Biomasseart
+
+    capacity = Column(Float) # Nettonennleistung
+    th_capacity = Column(Float, nullable=True) # ThermischeNutzleistung
+    feedin_type = Column(String(47), nullable=True)  # Einspeisungsart
+    voltage_level = Column(Integer)
+
+    geom = Column(Geometry("POINT", 4326), index=True)
+
+
+class EgonPowerPlantsHydro(Base):
+    __tablename__ = "egon_power_plants_hydro"
+    __table_args__ = {"schema": "supply"}
+
+    id = Column(Integer, Sequence("pp_hydro_seq"), primary_key=True)
+    bus_id = Column(Integer, nullable=True) # Grid district id
+    gens_id = Column(String, nullable=True) # EinheitMastrNummer
+
+    status = Column(String, nullable=True)  # EinheitBetriebsstatus
+    commissioning_date = Column(DateTime, nullable=True) # Inbetriebnahmedatum
+    postcode = Column(String(5), nullable=True) # Postleitzahl
+    city = Column(String(50), nullable=True) # Ort
+    federal_state = Column(String(21), nullable=True) # Bundesland
+
+    type = Column(Integer, nullable=True) # ArtDerWasserkraftanlage
+    water_origin = Column(String(20), nullable=True)  # ArtDesZuflusses
+
+    capacity = Column(Float, nullable=False) # Nettonennleistung
+    feedin_type = Column(String(47), nullable=True)  # Einspeisungsart
+    voltage_level = Column(Integer)
+
+    geom = Column(Geometry("POINT", 4326), index=True)
 
 
 class EgonPowerPlants(Base):
