f1543110f16d8f8257ec6f94102d8eb845ec5bea
nesnoj <jonathan.amme@rl-institut.de>
2022-12-01 01:10:42 +0100

    Add new MaStR dataset to MaStR download task

--- src/egon/data/datasets/mastr.py
+++ src/egon/data/datasets/mastr.py
@@ -1,56 +1,77 @@
+"""
+Download Marktstammdatenregister (MaStR) datasets unit registry.
+It incorporates two different datasets:
+
+Dump 2021-05-03
+* Source: https://sandbox.zenodo.org/record/808086
+* Used technologies: PV plants, wind turbines, biomass, hydro plants,
+  combustion, nuclear, gsgk, storage
+* Data is further processed in dataset
+  :py:class:`egon.data.datasets.power_plants.PowerPlants`
+
+Dump 2022-11-17
+* Source: https://sandbox.zenodo.org/record/1132839
+* Used technologies: PV plants, wind turbines, biomass, hydro plants
+* Data is further processed in module
+  :py:mod:`egon.data.datasets.power_plants.mastr` `PowerPlants`
+
+Todo: Finish docstring
+TBD
+"""
+
 from functools import partial
+from pathlib import Path
 from urllib.request import urlretrieve
 import os
 
 from egon.data.datasets import Dataset
 import egon.data.config
 
+WORKING_DIR = Path(".", "bnetza_mastr")
 
-def download_mastr_data(data_stages=None):
-    """
-    Download MaStR data from Zenodo
 
-    Parameters
-    ----------
-    data_stages: list
-        Select data stages you want to download data for. Possible values:
-        'raw', 'cleaned'. Defaults to 'cleaned' if omitted.
-    """
-    # Process inputs
-    if not data_stages:
-        data_stages = ["cleaned"]
+def download_mastr_data():
+    """Download MaStR data from Zenodo"""
 
-    # Get parameters from config and set download URL
-    data_config = egon.data.config.datasets()["mastr"]
-    zenodo_files_url = (
-        f"https://sandbox.zenodo.org/record/{data_config['deposit_id']}/files/"
-    )
+    def download(dataset_name, download_dir):
+        print(f"Downloading dataset {dataset_name} to {download_dir} ...")
+        # Get parameters from config and set download URL
+        data_config = egon.data.config.datasets()[dataset_name]
+        zenodo_files_url = (
+            f"https://sandbox.zenodo.org/record/"
+            f"{data_config['deposit_id']}/files/"
+        )
 
-    files = []
-    for technology in data_config["technologies"]:
-        # Download raw data
-        if "raw" in data_stages:
-            files.append(
-                f"{data_config['file_basename']}_{technology}_raw.csv"
-            )
-        # Download cleaned data
-        if "cleaned" in data_stages:
+        files = []
+        for technology in data_config["technologies"]:
             files.append(
                 f"{data_config['file_basename']}_{technology}_cleaned.csv"
             )
-        files.append("datapackage.json")
         files.append("location_elec_generation_raw.csv")
 
-    # Retrieve specified files
-    for filename in files:
-        if not os.path.isfile(filename):
-            urlretrieve(zenodo_files_url + filename, filename)
+        # Retrieve specified files
+        for filename in files:
+            if not os.path.isfile(filename):
+                urlretrieve(
+                    zenodo_files_url + filename, download_dir / filename
+                )
+
+    dump_dir_old = WORKING_DIR / "dump_2021-05-03"
+    dump_dir_new = WORKING_DIR / "dump_2022-11-17"
+
+    if not os.path.exists(dump_dir_old):
+        dump_dir_old.mkdir(exist_ok=True, parents=True)
+    if not os.path.exists(dump_dir_new):
+        dump_dir_new.mkdir(exist_ok=True, parents=True)
+
+    download(dataset_name="mastr", download_dir=dump_dir_old)
+    download(dataset_name="mastr_new", download_dir=dump_dir_new)
 
 
 mastr_data_setup = partial(
     Dataset,
     name="MastrData",
-    version="0.0.0",
+    version="0.0.1",
     dependencies=[],
     tasks=(download_mastr_data,),
 )
