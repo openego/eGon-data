c435e8966c7a979f1b96840bac148d23e8198c1a
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2022-12-02 09:32:11 +0100

    minor change

--- src/egon/data/datasets/DSM_cts_ind.py
+++ src/egon/data/datasets/DSM_cts_ind.py
@@ -294,7 +294,7 @@ def ind_sites_vent_data_import(ind_vent_share, wz):
         f"""
         SELECT bus, scn_name, p_set FROM
         {sources['schema']}.{sources['table']}
-        WHERE wz = '{wz}'
+        WHERE wz = {wz}
         """
     )
 
@@ -332,7 +332,7 @@ def ind_sites_vent_data_import_individual(ind_vent_share, wz):
         SELECT site_id, bus_id as bus, scn_name, p_set FROM
         {sources['schema']}.{sources['table']}
         WHERE scn_name IN ('eGon2035', 'eGon100RE')
-        AND wz = '{wz}'
+        AND wz = {wz}
         """
     )
 
