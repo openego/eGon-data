4ea27554e9033d388d5cd0e489a726d60835c787
Stephan Günther <gnn.code@gmail.com>
2022-12-03 03:34:30 +0100

    Stop wrapping log messages at 72 characters
    It's not that useful and mostly worsens readability more than it
    improves it, especially for stack traces.

--- src/egon/data/__init__.py
+++ src/egon/data/__init__.py
@@ -1,19 +1,8 @@
-from textwrap import wrap
-
 from loguru import logger
 import click
 
 __version__ = "0.0.0"
 
 
-def echo(message):
-    prefix, message = message.split(" - ")
-    lines = message.split("\n")
-    width = min(72, click.get_terminal_size()[0])
-    wraps = ["\n".join(wrap(line, width)) for line in lines]
-    message = "\n".join([prefix] + wraps)
-    click.echo(message, err=True)
-
-
 logger.remove()
-logger.add(echo, colorize=True)
+logger.add(lambda m: click.echo(m, err=True), colorize=True)
