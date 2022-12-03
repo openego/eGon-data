bbdb85b1ef4f4c210473421213f64657cf57f137
Stephan Günther <gnn.code@gmail.com>
2022-12-03 03:38:25 +0100

    Use "rich" to format logged exceptions
    The package has a particularly useful way of formatting how exceptions
    are printed, because it prints the context of the code in which the
    exception occurred, as well as the variables currently in scope along
    with their values. All that while still keeping things pretty readable,
    at least IMHO. Unfortunately it's not enough to simply use rich's
    default way of switching on pretty printing of exceptions, because rich
    also colors the stack traces and the markup used to get those colors
    interferes badly with how Airflow displays logged messages in its web
    application. For this reason, it's necessary to change the markup when
    formatting stack traces.

--- src/egon/data/__init__.py
+++ src/egon/data/__init__.py
@@ -1,8 +1,38 @@
+import re
+
 from loguru import logger
 import click
+import loguru
+import rich.console
+import rich.traceback
 
 __version__ = "0.0.0"
 
 
+def format(record):
+    result = loguru._defaults.LOGURU_FORMAT
+    exception = record["exception"]
+    if exception:
+        console = rich.console.Console(color_system=None)
+        traceback = rich.traceback.Traceback(
+            rich.traceback.Traceback.extract(
+                exc_type=exception.type,
+                exc_value=exception.value,
+                traceback=exception.traceback,
+                show_locals=True,
+            )
+        )
+        with console.capture() as capture:
+            console.print(traceback)
+        capture = capture.get()
+        # Regular expression copied from:
+        #   https://loguru.readthedocs.io/en/stable/api/logger.html#color
+        capture = re.sub(r"\\?</?((?:[fb]g\s)?[^<>\s]*)>", r"\\\g<0>", capture)
+        capture = re.sub("{", "{{", capture)
+        capture = re.sub("}", "}}", capture)
+        result = f"{result}\n{capture}"
+    return result
+
+
 logger.remove()
-logger.add(lambda m: click.echo(m, err=True), colorize=True)
+logger.add(lambda m: click.echo(m, err=True), format=format, enqueue=True)
