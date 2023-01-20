aee77c039ac8e229a66c8b36c816db59d395e016
Kilian Helfenbein <Kilian.Helfenbein@rl-institut.de>
2023-01-20 14:08:45 +0100

    changed data source to database

--- src/egon/data/datasets/emobility/heavy_duty_transport/__init__.py
+++ src/egon/data/datasets/emobility/heavy_duty_transport/__init__.py
@@ -7,8 +7,8 @@ heavy duty transport.
 **Contents of this module**
 * Creation of DB tables
 * Download and preprocessing of vehicle registration data from BAST
-* Calculation of hydrogen demand based on a Voronoi distribution of counted truck
-  traffic among NUTS 3 regions.
+* Calculation of hydrogen demand based on a Voronoi distribution of counted
+  truck traffic among NUTS 3 regions.
 * Write results to DB
 * Map demand to H2 buses and write to DB
 
@@ -21,37 +21,38 @@ The config of this dataset can be found in *datasets.yml* in section
 
 Assumptions can be changed within the *datasets.yml*.
 
-In the context of the eGon project, it is assumed that e-trucks will be completely
-hydrogen-powered and in both scenarios the hydrogen consumption is assumed to be
-6.68 kgH2 per 100 km with an additional
+In the context of the eGon project, it is assumed that e-trucks will be
+completely hydrogen-powered and in both scenarios the hydrogen consumption is
+assumed to be 6.68 kgH2 per 100 km with an additional
 [supply chain leakage rate of 0.5 %](
 https://www.energy.gov/eere/fuelcells/doe-technical-targets-hydrogen-delivery).
 
 ### Scenario NEP C 2035
 
-The ramp-up figures are taken from [Scenario C 2035 Grid Development Plan 2021-2035](
+The ramp-up figures are taken from
+[Scenario C 2035 Grid Development Plan 2021-2035](
 https://www.netzentwicklungsplan.de/sites/default/files/paragraphs-files/
-NEP_2035_V2021_2_Entwurf_Teil1.pdf). According to this, 100,000 e-trucks are expected
-in Germany in 2035, each covering an average of 100,000 km per year. In total this means
-10 Billion km.
+NEP_2035_V2021_2_Entwurf_Teil1.pdf). According to this, 100,000 e-trucks are
+expected in Germany in 2035, each covering an average of 100,000 km per year.
+In total this means 10 Billion km.
 
 ### Scenario eGon100RE
 
-In the case of the eGon100RE scenario it is assumed that the HGV traffic is completely
-hydrogen-powered. The total freight traffic with 40 Billion km is taken from the
+In the case of the eGon100RE scenario it is assumed that the HGV traffic is
+completely hydrogen-powered. The total freight traffic with 40 Billion km is
+taken from the
 [BMWk Langfristszenarien GHG-emission free scenarios (SNF > 12 t zGG)](
 https://www.langfristszenarien.de/enertile-explorer-wAssets/docs/
 LFS3_Langbericht_Verkehr_final.pdf#page=17).
 
 ## Methodology
 
-Using a Voronoi interpolation, the censuses of the BASt data is distributed according to
-the area fractions of the Voronoi fields within each mv grid or any other geometries
-like NUTS-3.
+Using a Voronoi interpolation, the censuses of the BASt data is distributed
+according to the area fractions of the Voronoi fields within each mv grid or
+any other geometries like NUTS-3.
 """
 from pathlib import Path
 import csv
-import zipfile
 
 from loguru import logger
 import requests
@@ -64,7 +65,7 @@ from egon.data.datasets.emobility.heavy_duty_transport.create_h2_buses import (
 from egon.data.datasets.emobility.heavy_duty_transport.db_classes import (
     EgonHeavyDutyTransportVoronoi,
 )
-from egon.data.datasets.emobility.heavy_duty_transport.h2_demand_distribution import (
+from egon.data.datasets.emobility.heavy_duty_transport.h2_demand_distribution import (  # noqa: E501
     run_egon_truck,
 )
 
@@ -104,31 +105,12 @@ def download_hgv_data():
 
     logger.debug("Downloaded BAST data.")
 
-    if not TESTMODE_OFF:
-        url = sources["NUTS"]["url"]
-
-        r = requests.get(url, stream=True)
-        file = WORKING_DIR / sources["NUTS"]["file"]
-
-        with open(file, "wb") as fd:
-            for chunk in r.iter_content(chunk_size=512):
-                fd.write(chunk)
-
-        directory = WORKING_DIR / "_".join(
-            sources["NUTS"]["file"].split(".")[:-1]
-        )
-
-        with zipfile.ZipFile(file, "r") as zip_ref:
-            zip_ref.extractall(directory)
-
-        logger.debug("Downloaded NUTS data.")
-
 
 class HeavyDutyTransport(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="HeavyDutyTransport",
-            version="0.0.1",
+            version="0.0.2.dev",
             dependencies=dependencies,
             tasks=(
                 {
