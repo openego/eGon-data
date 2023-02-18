59e17b7a36a9f11b1ba4b8d8b0bde453f81aca9f
Stephan Günther <gnn.code@gmail.com>
2023-02-18 13:01:02 +0100

    Bump `PypsaEurSec`'s dataset version

--- src/egon/data/datasets/pypsaeursec/__init__.py
+++ src/egon/data/datasets/pypsaeursec/__init__.py
@@ -1060,7 +1060,7 @@ class PypsaEurSec(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="PypsaEurSec",
-            version="0.0.9",
+            version="0.0.10",
             dependencies=dependencies,
             tasks=tasks,
         )
