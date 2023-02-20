6e78e3bf8eb2481b130201a8efada49a67a6708c
AmeliaNadal <amelia.nadal@dlr.de>
2023-02-20 14:15:55 +0100

    Increase dataset version

--- src/egon/data/datasets/pypsaeursec/__init__.py
+++ src/egon/data/datasets/pypsaeursec/__init__.py
@@ -1144,7 +1144,7 @@ class PypsaEurSec(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="PypsaEurSec",
-            version="0.0.9",
+            version="0.0.10",
             dependencies=dependencies,
             tasks=tasks,
         )
