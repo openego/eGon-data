c0714dfdcffc042f9b68ad5181ef57db467d01bc
AmeliaNadal <amelia.nadal@dlr.de>
2023-02-22 12:28:44 +0100

    Improve clean_database function

--- src/egon/data/datasets/pypsaeursec/__init__.py
+++ src/egon/data/datasets/pypsaeursec/__init__.py
@@ -259,9 +259,11 @@ def clean_database():
         )
 
     db.execute_sql(
-        "DELETE FROM grid.egon_etrago_bus "
-        "WHERE scn_name = '{scn_name}' "
-        "AND country <> 'DE'"
+        f"""
+        DELETE FROM grid.egon_etrago_bus
+        WHERE scn_name = '{scn_name}'
+        AND country != 'DE'
+        """
     )
 
 
