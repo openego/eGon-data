d5662036fb68e739bde9706d94230b508ebfe949
ClaraBuettner <clara.buettner@hs-flensburg.de>
2023-05-15 17:34:08 +0200

    Remove heavy duty transport for status2019

--- src/egon/data/airflow/dags/pipeline_status_quo.py
+++ src/egon/data/airflow/dags/pipeline_status_quo.py
@@ -25,9 +25,6 @@ from egon.data.datasets.electricity_demand_timeseries import (
 from egon.data.datasets.electricity_demand_timeseries.cts_buildings import (
     CtsDemandBuildings,
 )
-from egon.data.datasets.emobility.heavy_duty_transport import (
-    HeavyDutyTransport,
-)
 from egon.data.datasets.emobility.motorized_individual_travel import (
     MotorizedIndividualTravel,
 )
@@ -487,11 +484,6 @@ with airflow.DAG(
 
     mit_charging_infrastructure = MITChargingInfrastructure(
         dependencies=[mv_grid_districts, hh_demand_buildings_setup]
-    )
-
-    # eMobility: heavy duty transport TODO: adjust for SQ
-    heavy_duty_transport = HeavyDutyTransport(
-        dependencies=[vg250, setup_etrago, create_gas_polygons_status2019]
     )
 
     cts_demand_buildings = CtsDemandBuildings(
