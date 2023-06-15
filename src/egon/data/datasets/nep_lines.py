7732b27e860d3ccf20deae40f0bc7f72c9e566b8
ClaraBuettner <clara.buettner@hs-flensburg.de>
2023-06-15 16:04:28 +0200

    Add first script for adding lines from NEP

---
+++ src/egon/data/datasets/nep_lines.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pandas as pd 
+import geopandas as gpd
+from sqlalchemy import create_engine
+
+engine = create_engine(
+    f"postgresql+psycopg2://postgres:"
+    f"postgres@localhost:"
+    f"5432/egon",
+    echo=False,
+)
+
+substation_df = pd.read_sql(
+    """
+    SELECT * FROM 
+    grid.egon_ehv_substation
+    
+    """
+    , engine)
+
+substation_df = gpd.read_postgis(
+    """
+    SELECT * FROM 
+    grid.egon_ehv_substation
+    
+    """
+    , engine, geom_col="point")
+
+
+
+lines_df = pd.read_excel(
+    
+    )
