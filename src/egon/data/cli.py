9ee28ac81cd6723f0f9f30f82420d7b0b7f977af
ClaraBuettner <clara.buettner@hs-flensburg.de>
2023-07-04 16:44:13 +0200

    Make sure that the existing configuration file is used

--- src/egon/data/cli.py
+++ src/egon/data/cli.py
@@ -277,16 +277,19 @@ def egon_data(context, **kwargs):
         with open(config.paths(pid="*")[0]) as f:
             options = yaml.load(f, Loader=yaml.SafeLoader)
     else:  # len(config.paths(pid="*")) == 0, so need to create one.
+
         with open(config.paths()[0]) as f:
             options["file"] = yaml.load(f, Loader=yaml.SafeLoader)
+
         options = dict(
             options.get("file", {}),
             **{
-                flag: options["cli"][flag]
+                flag: options["file"][flag]
                 for flag in options["cli"]
                 if options["cli"][flag] != options["defaults"][flag]
             },
         )
+
         with open(config.paths(pid="current")[0], "w") as f:
             f.write(yaml.safe_dump(options))
 
