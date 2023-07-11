bf670b314b9630eb3e36214fd0c56aa04bf6adab
ClaraBuettner <clara.buettner@hs-flensburg.de>
2023-07-11 13:50:38 +0200

    Balck

--- src/egon/data/datasets/power_plants/conventional.py
+++ src/egon/data/datasets/power_plants/conventional.py
@@ -144,11 +144,9 @@ def match_nep_no_chp(
     )
 
     for ET in nep["carrier"].unique():
-
         for index, row in nep[
             (nep["carrier"] == ET) & (nep["postcode"] != "None")
         ].iterrows():
-
             # Select plants from MaStR that match carrier, PLZ
             # and have a similar capacity
             # Create a copy of all power plants from MaStR
