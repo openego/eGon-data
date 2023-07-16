c0774e5c9354fff3395f729ab923a66ba2bf2ba8
Lennart Zimmermann <lennartzimmermann@outlook.de>
2023-07-16 17:50:20 +0200

    buildyear extraction for final NEP table

--- src/egon/data/datasets/buildyear_for_NEP_table.py
+++ src/egon/data/datasets/buildyear_for_NEP_table.py
@@ -16,23 +16,6 @@ for index1, row1 in lines_df.iterrows():
         if scn_name1 == scn_name2:
             lines_df.loc[index1, 'build_year'] = row2['anvisierte Inbetriebnahme']
 
-#merged_df = pd.merge(lines_df, buildyear_df, on='scn_name', how='inner')
-
-#buildyear_df_filtered = buildyear_df[buildyear_df['scn_name'].isin(lines_df['scn_name'])]
-
-# Auswahl der relevanten Spalten
-#merged_df = merged_df[['scn_name', 'anvisierte Inbetriebnahme']]
-
-#print('länge von merge:'+{len(merged_df)}+ ',länge von buildyear:' + len(buildyear_df))
-
-# Iteration über die Zeilen des merged_df DataFrames
-#for index, row in merged_df.iterrows():
- #   lines_df['build_year'] = merged_df['anvisierte Inbetriebnahme']
-
-
-#for index, row in lines_df.iterrows():
- #   if lines_df.loc[index,'scn_name'] == buildyear_df_filtered.loc[index,'scn_name']:
-  #      lines_df.loc[index,'build_year'] = buildyear_df_filtered.loc[index,'anvisierte Inbetriebnahme']
 
 lines_df.to_csv('/home/student/Documents/Powerd/egon_etrago_line_new.csv', index=False)
 
