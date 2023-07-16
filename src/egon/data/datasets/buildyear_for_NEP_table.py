ca1f85402bb9bb8784352595ad8146819019f7e9
Lennart Zimmermann <lennartzimmermann@outlook.de>
2023-07-16 18:14:03 +0200

    buildyear extraction for final NEP table

--- src/egon/data/datasets/buildyear_for_NEP_table.py
+++ src/egon/data/datasets/buildyear_for_NEP_table.py
@@ -1,14 +1,11 @@
 import pandas as pd
-import geopandas as gpd
-import psycopg2
 
 
 
 lines_df = pd.read_csv("/home/student/Documents/Powerd/egon_etrago_line_new.csv")
 buildyear_df = pd.read_csv("/home/student/Documents/Powerd/NEP_tables_finalVersion.csv")
 buildyear_df['anvisierte Inbetriebnahme'] = buildyear_df["anvisierte Inbetriebnahme"].fillna('').str.split(r'[,/-]').apply(lambda x: x[1].strip() if len(x) > 1 else x[0].strip())
-#buildyear_df ['anvisierte Inbetriebnahme']=buildyear_df["anvisierte Inbetriebnahme"].str.split(',').str[1].str.strip()
-buildyear_df.to_csv('/home/student/Documents/Powerd/buildyear_test.csv', index=False)
+
 for index1, row1 in lines_df.iterrows():
     scn_name1 = str(row1['scn_name'])
     for index2, row2 in buildyear_df.iterrows():
