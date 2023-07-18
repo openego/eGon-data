ac2dc2365645b78523bfbef8e4d4a5c13365b1a7
Lennart Zimmermann <lennartzimmermann@outlook.de>
2023-07-18 11:05:51 +0200

    adding buiuldyear to py-file

--- src/egon/data/datasets/egon_etrago_line/egon_etrago_line_new.py
+++ src/egon/data/datasets/egon_etrago_line/egon_etrago_line_new.py
@@ -4,6 +4,7 @@ from sqlalchemy import create_engine
 from geopy.distance import geodesic
 import difflib
 
+
 # Create connection with pgAdmin4 - Offline
 engine = create_engine(
     f"postgresql+psycopg2://postgres:"
