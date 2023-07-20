cb25cb9dea3b351212ea7c6bad24adfe45d67f07
ClaraBuettner <clara.buettner@hs-flensburg.de>
2023-07-20 09:52:07 +0200

    Add dataset description for TYNDP

--- src/egon/data/datasets/tyndp.py
+++ src/egon/data/datasets/tyndp.py
@@ -1,4 +1,4 @@
-"""The central module containing all code dealing with tyndp data
+"""The central module containing all code dealing with downloading tyndp data
 """
 
 import os
@@ -6,18 +6,40 @@ from egon.data import config
 from egon.data.datasets import Dataset
 from urllib.request import urlretrieve
 
+
 class Tyndp(Dataset):
+    """
+    Downloads data for foreign countries from Ten-Year-Network-Developement Plan
+
+    This dataset downloads installed generation capacities and load time series for
+    foreign countries from the website of the Ten-Year-Network-Developement Plan 2020 from ENTSO-E.
+    That data is stored into files and later on written into the database
+    (see :py:class:`ElectricalNeighbours <egon.data.datasets.electrical_neighbours.ElectricalNeighbours>`).
+
+
+    *Dependencies*
+      * :py:class:`Setup <egon.data.datasets.database.Setup>`
+
+    *Resulting tables*
+
+    """
+
+    #:
+    name: str = "Tyndp"
+    #:
+    version: str = "0.0.1"
 
     def __init__(self, dependencies):
         super().__init__(
-            name="Tyndp",
-            version="0.0.1",
+            name=self.name,
+            version=self.version,
             dependencies=dependencies,
             tasks=(download),
         )
 
+
 def download():
-    """ Download input data from TYNDP 2020
+    """Download input data from TYNDP 2020
     Returns
     -------
     None.
