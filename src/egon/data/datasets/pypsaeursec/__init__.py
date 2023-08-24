283566d310857f5ab67568dc29d3d8ad17a667b4
ClaraBuettner <clara.buettner@hs-flensburg.de>
2023-08-24 10:03:31 +0200

    Import function to manipulate prepared network

--- src/egon/data/datasets/pypsaeursec/__init__.py
+++ src/egon/data/datasets/pypsaeursec/__init__.py
@@ -18,9 +18,11 @@ import yaml
 
 from egon.data import __path__, db, logger, config
 from egon.data.datasets import Dataset
+from egon.data.datasets.manipulate_network import execute
 from egon.data.datasets.scenario_parameters import get_sector_parameters
 import egon.data.config
 import egon.data.subprocess as subproc
+
 
 
 def download():
