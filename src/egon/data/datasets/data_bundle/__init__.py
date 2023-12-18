b57fb9878f3a547430e3c1e6c211bf7027de03c3
CarlosEpia <carlosepiar@gmail.com>
2023-12-18 15:33:11 +0100

    change sandbox to zenodo

--- src/egon/data/datasets/data_bundle/__init__.py
+++ src/egon/data/datasets/data_bundle/__init__.py
@@ -24,7 +24,7 @@ def download():
         shutil.rmtree(data_bundle_path)
     # Get parameters from config and set download URL
     sources = egon.data.config.datasets()["data-bundle"]["sources"]["zenodo"]
-    url = f"""https://sandbox.zenodo.org/record/{sources['deposit_id']}/files/data_bundle_egon_data.zip"""
+    url = f"""https://zenodo.org/record/{sources['deposit_id']}/files/data_bundle_egon_data.zip"""
     target_file = egon.data.config.datasets()["data-bundle"]["targets"]["file"]
 
     # Retrieve files
@@ -32,6 +32,7 @@ def download():
 
     with zipfile.ZipFile(target_file, "r") as zip_ref:
         zip_ref.extractall(".")
+
 
 class DataBundle(Dataset):
     def __init__(self, dependencies):
