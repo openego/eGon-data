912c56231c4bc99de8bf56a837c346478f590666
Julian.Endres <julian.endres@rl-institut.de>
2024-01-17 18:42:16 +0100

    Add wrapped_partial function for dynamic tasks
    Partials need to be updated with further information like
    __module__, __name__. This info is copied from the actual
    function.

--- src/egon/data/datasets/__init__.py
+++ src/egon/data/datasets/__init__.py
@@ -4,7 +4,7 @@ from __future__ import annotations
 
 from collections import abc
 from dataclasses import dataclass
-from functools import reduce
+from functools import partial, reduce, update_wrapper
 from typing import Callable, Iterable, Set, Tuple, Union
 import re
 
@@ -17,6 +17,13 @@ from egon.data import config, db, logger
 
 Base = declarative_base()
 SCHEMA = "metadata"
+
+
+def wrapped_partial(func, *args, **kwargs):
+    partial_func = partial(func, *args, **kwargs)
+    # update partial to look like func
+    update_wrapper(partial_func, func)
+    return partial_func
 
 
 def setup():
