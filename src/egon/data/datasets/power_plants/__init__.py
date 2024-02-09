e9977e69c238793d65dc759b3c1931ded4b28b6e
Julian.Endres <julian.endres@rl-institut.de>
2024-02-09 11:00:05 +0100

    Dynamise powerplants status quo

--- src/egon/data/datasets/power_plants/__init__.py
+++ src/egon/data/datasets/power_plants/__init__.py
@@ -13,7 +13,7 @@ import logging
 import pandas as pd
 
 from egon.data import db
-from egon.data.datasets import Dataset
+from egon.data.datasets import Dataset, wrapped_partial
 from egon.data.datasets.mastr import (
     WORKING_DIR_MASTR_NEW,
     WORKING_DIR_MASTR_OLD,
@@ -1233,9 +1233,10 @@ tasks = (
     create_tables,
     import_mastr,
 )
-
-if "status2019" in egon.data.config.settings()["egon-data"]["--scenarios"]:
-    tasks = tasks + (power_plants_status_quo,)
+for scn_name in egon.data.config.settings()["egon-data"]["--scenarios"]:
+    if "status" in scn_name:
+        tasks += (wrapped_partial(
+            power_plants_status_quo, scn_name=scn_name, postfix=f"_{scn_name[-4:]}"),)
 
 if (
     "eGon2035" in egon.data.config.settings()["egon-data"]["--scenarios"]
