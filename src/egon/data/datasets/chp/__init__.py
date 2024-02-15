8ecc530c4272a90bda7e431954a04cbe9093fbfe
CarlosEpia <carlosepiar@gmail.com>
2024-02-15 14:01:43 +0100

    include biomass CHP generators

--- src/egon/data/datasets/chp/__init__.py
+++ src/egon/data/datasets/chp/__init__.py
@@ -322,6 +322,12 @@ def insert_chp_statusquo():
         WORKING_DIR_MASTR_NEW / "bnetza_mastr_combustion_cleaned.csv"
     )
 
+    mastr_biomass = pd.read_csv(
+        WORKING_DIR_MASTR_NEW / "bnetza_mastr_biomass_cleaned.csv"
+    )
+
+    mastr = pd.concat([mastr, mastr_biomass]).reset_index(drop=True)
+
     mastr = mastr.loc[mastr.ThermischeNutzleistung > 0]
 
     mastr = mastr.loc[
@@ -333,6 +339,7 @@ def insert_chp_statusquo():
                 "nicht biogener Abfall",
                 "Braunkohle",
                 "Steinkohle",
+                "Biomasse",
             ]
         )
     ]
