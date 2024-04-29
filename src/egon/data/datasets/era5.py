d2b1a6296159a9a476eab89c8b2b6e3da947e1b7
CarlosEpia <carlosepiar@gmail.com>
2024-04-29 09:59:55 +0200

    make era5 scenario friendly

--- src/egon/data/datasets/era5.py
+++ src/egon/data/datasets/era5.py
@@ -69,53 +69,54 @@ def import_cutout(boundary="Europe"):
         Weather data stored in cutout
 
     """
-    weather_year = get_sector_parameters("global", "status2019")["weather_year"]
-
-    if boundary == "Europe":
-        xs = slice(-12.0, 35.1)
-        ys = slice(72.0, 33.0)
-
-    elif boundary == "Germany":
-        geom_de = (
-            gpd.read_postgis(
-                "SELECT geometry as geom FROM boundaries.vg250_sta_bbox",
-                db.engine(),
+    for scn in set(egon.data.config.settings()["egon-data"]["--scenarios"]):
+        weather_year = get_sector_parameters("global", scn)["weather_year"]
+    
+        if boundary == "Europe":
+            xs = slice(-12.0, 35.1)
+            ys = slice(72.0, 33.0)
+    
+        elif boundary == "Germany":
+            geom_de = (
+                gpd.read_postgis(
+                    "SELECT geometry as geom FROM boundaries.vg250_sta_bbox",
+                    db.engine(),
+                )
+                .to_crs(4326)
+                .geom
             )
-            .to_crs(4326)
-            .geom
+            xs = slice(geom_de.bounds.minx[0], geom_de.bounds.maxx[0])
+            ys = slice(geom_de.bounds.miny[0], geom_de.bounds.maxy[0])
+    
+        elif boundary == "Germany-offshore":
+            xs = slice(5.5, 14.5)
+            ys = slice(55.5, 53.5)
+    
+        else:
+            print(
+                f"Boundary {boundary} not defined. "
+                "Choose either 'Europe' or 'Germany'"
+            )
+    
+        directory = (
+            Path(".")
+            / (
+                egon.data.config.datasets()["era5_weather_data"]["targets"][
+                    "weather_data"
+                ]["path"]
+            )
+            / f"{boundary.lower()}-{str(weather_year)}-era5.nc"
         )
-        xs = slice(geom_de.bounds.minx[0], geom_de.bounds.maxx[0])
-        ys = slice(geom_de.bounds.miny[0], geom_de.bounds.maxy[0])
-
-    elif boundary == "Germany-offshore":
-        xs = slice(5.5, 14.5)
-        ys = slice(55.5, 53.5)
-
-    else:
-        print(
-            f"Boundary {boundary} not defined. "
-            "Choose either 'Europe' or 'Germany'"
+    
+        cutout = atlite.Cutout(
+            path=directory.absolute(),
+            module="era5",
+            x=xs,
+            y=ys,
+            years=slice(weather_year, weather_year),
         )
-
-    directory = (
-        Path(".")
-        / (
-            egon.data.config.datasets()["era5_weather_data"]["targets"][
-                "weather_data"
-            ]["path"]
-        )
-        / f"{boundary.lower()}-{str(weather_year)}-era5.nc"
-    )
-
-    cutout = atlite.Cutout(
-        path=directory.absolute(),
-        module="era5",
-        x=xs,
-        y=ys,
-        years=slice(weather_year, weather_year),
-    )
-
-    return cutout
+    
+        return cutout
 
 
 def download_era5():
