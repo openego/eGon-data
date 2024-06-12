7e415290250265ca4746f2dee2b2c6b18bcce29d
ClaraBuettner <clara.buettner@hs-flensburg.de>
2024-06-12 16:21:40 +0200

    Remove outdated op_kwargs to delete the table

--- src/egon/data/datasets/electricity_demand_timeseries/hh_profiles.py
+++ src/egon/data/datasets/electricity_demand_timeseries/hh_profiles.py
@@ -206,7 +206,6 @@ class HouseholdDemands(Dataset):
                 task_id="MV-hh-electricity-load-2019",
                 python_callable=mv_grid_district_HH_electricity_load,
                 op_args=["status2019", 2019],
-                op_kwargs={"drop_table": True},
             )
 
             tasks = tasks + (mv_hh_electricity_load_2035,)
@@ -219,7 +218,6 @@ class HouseholdDemands(Dataset):
                 task_id="MV-hh-electricity-load-2035",
                 python_callable=mv_grid_district_HH_electricity_load,
                 op_args=["eGon2035", 2035],
-                op_kwargs={"drop_table": True},
             )
 
             tasks = tasks + (mv_hh_electricity_load_2035,)
@@ -232,14 +230,13 @@ class HouseholdDemands(Dataset):
                 task_id="MV-hh-electricity-load-2050",
                 python_callable=mv_grid_district_HH_electricity_load,
                 op_args=["eGon100RE", 2050],
-                op_kwargs={"drop_table": True},
             )
 
             tasks = tasks + (mv_hh_electricity_load_2050,)
 
         super().__init__(
             name="Household Demands",
-            version="0.0.11",
+            version="0.0.12",
             dependencies=dependencies,
             tasks=tasks,
         )
