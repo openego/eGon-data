3fc85fde77f3ad2582b35416ed1ea95b61ba1107
CarlosEpia <carlosepiar@gmail.com>
2024-06-27 15:02:11 +0200

    make func insert_h2_to_ch4_to_h2 works for all scn

--- src/egon/data/datasets/hydrogen_etrago/h2_to_ch4.py
+++ src/egon/data/datasets/hydrogen_etrago/h2_to_ch4.py
@@ -15,8 +15,7 @@ These links are modelling:
 
 from geoalchemy2.types import Geometry
 
-from egon.data import db
-from egon.data.datasets.etrago_helpers import copy_and_modify_links
+from egon.data import db, config
 from egon.data.datasets.etrago_setup import link_geom_from_buses
 from egon.data.datasets.scenario_parameters import get_sector_parameters
 
@@ -39,110 +38,104 @@ def insert_h2_to_ch4_to_h2():
     This function inserts data into the database and has no return.
 
     """
-    # Connect to local database
-    engine = db.engine()
+    scenarios = config.settings()["egon-data"]["--scenarios"]
 
-    scn_name = "eGon2035"
-    # Select CH4 and corresponding H2 buses
-    # No geometry required in this case!
-    buses = db.select_dataframe(
-        f"""
-        SELECT * FROM grid.egon_etrago_ch4_h2 WHERE scn_name = '{scn_name}'
-        """
-    )
-
-    methanation = buses.copy().rename(
-        columns={"bus_H2": "bus0", "bus_CH4": "bus1"}
-    )
-    SMR = buses.copy().rename(columns={"bus_H2": "bus1", "bus_CH4": "bus0"})
-    feed_in = methanation.copy()
-
-    # Delete old entries
-    db.execute_sql(
-        f"""
-            DELETE FROM grid.egon_etrago_link WHERE "carrier" IN
-            ('H2_to_CH4', 'H2_feedin', 'CH4_to_H2') AND scn_name = '{scn_name}'
-            AND bus0 NOT IN (
-               SELECT bus_id FROM grid.egon_etrago_bus
-               WHERE scn_name = '{scn_name}' AND country != 'DE'
-            ) AND bus1 NOT IN (
-               SELECT bus_id FROM grid.egon_etrago_bus
-               WHERE scn_name = '{scn_name}' AND country != 'DE'
+    for scn_name in scenarios:
+        # Connect to local database
+        engine = db.engine()
+    
+        # Select CH4 and corresponding H2 buses
+        # No geometry required in this case!
+        buses = db.select_dataframe(
+            f"""
+            SELECT * FROM grid.egon_etrago_ch4_h2 WHERE scn_name = '{scn_name}'
+            """
+        )
+    
+        methanation = buses.copy().rename(
+            columns={"bus_H2": "bus0", "bus_CH4": "bus1"}
+        )
+        SMR = buses.copy().rename(columns={"bus_H2": "bus1", "bus_CH4": "bus0"})
+        feed_in = methanation.copy()
+    
+        # Delete old entries
+        db.execute_sql(
+            f"""
+                DELETE FROM grid.egon_etrago_link WHERE "carrier" IN
+                ('H2_to_CH4', 'H2_feedin', 'CH4_to_H2') AND scn_name = '{scn_name}'
+                AND bus0 NOT IN (
+                   SELECT bus_id FROM grid.egon_etrago_bus
+                   WHERE scn_name = '{scn_name}' AND country != 'DE'
+                ) AND bus1 NOT IN (
+                   SELECT bus_id FROM grid.egon_etrago_bus
+                   WHERE scn_name = '{scn_name}' AND country != 'DE'
+                );
+            """
+        )
+    
+        scn_params = get_sector_parameters("gas", scn_name)
+    
+        pipeline_capacities = db.select_dataframe(
+            f"""
+            SELECT bus0, bus1, p_nom FROM grid.egon_etrago_link
+            WHERE scn_name = '{scn_name}' AND carrier = 'CH4'
+            AND (
+                bus0 IN (
+                    SELECT bus_id FROM grid.egon_etrago_bus
+                    WHERE scn_name = '{scn_name}' AND country = 'DE'
+                ) OR bus1 IN (
+                    SELECT bus_id FROM grid.egon_etrago_bus
+                    WHERE scn_name = '{scn_name}' AND country = 'DE'
+                )
             );
-        """
-    )
-
-    scn_params = get_sector_parameters("gas", scn_name)
-
-    pipeline_capacities = db.select_dataframe(
-        f"""
-        SELECT bus0, bus1, p_nom FROM grid.egon_etrago_link
-        WHERE scn_name = '{scn_name}' AND carrier = 'CH4'
-        AND (
-            bus0 IN (
-                SELECT bus_id FROM grid.egon_etrago_bus
-                WHERE scn_name = '{scn_name}' AND country = 'DE'
-            ) OR bus1 IN (
-                SELECT bus_id FROM grid.egon_etrago_bus
-                WHERE scn_name = '{scn_name}' AND country = 'DE'
+            """
+        )
+    
+        feed_in["p_nom"] = 0
+        feed_in["p_nom_extendable"] = False
+        # calculation of H2 energy share via volumetric share outsourced
+        # in a mixture of H2 and CH4 with 15 %vol share
+        H2_share = scn_params["H2_feedin_volumetric_fraction"]
+        H2_energy_share = H2_CH4_mix_energy_fractions(H2_share)
+    
+        for bus in feed_in["bus1"].values:
+            # calculate the total pipeline capacity connected to a specific bus
+            nodal_capacity = pipeline_capacities.loc[
+                (pipeline_capacities["bus0"] == bus)
+                | (pipeline_capacities["bus1"] == bus),
+                "p_nom",
+            ].sum()
+            # multiply total pipeline capacity with H2 energy share corresponding
+            # to volumetric share
+            feed_in.loc[feed_in["bus1"] == bus, "p_nom"] = (
+                nodal_capacity * H2_energy_share
             )
-        );
-        """
-    )
-
-    feed_in["p_nom"] = 0
-    feed_in["p_nom_extendable"] = False
-    # calculation of H2 energy share via volumetric share outsourced
-    # in a mixture of H2 and CH4 with 15 %vol share
-    H2_share = scn_params["H2_feedin_volumetric_fraction"]
-    H2_energy_share = H2_CH4_mix_energy_fractions(H2_share)
-
-    for bus in feed_in["bus1"].values:
-        # calculate the total pipeline capacity connected to a specific bus
-        nodal_capacity = pipeline_capacities.loc[
-            (pipeline_capacities["bus0"] == bus)
-            | (pipeline_capacities["bus1"] == bus),
-            "p_nom",
-        ].sum()
-        # multiply total pipeline capacity with H2 energy share corresponding
-        # to volumetric share
-        feed_in.loc[feed_in["bus1"] == bus, "p_nom"] = (
-            nodal_capacity * H2_energy_share
-        )
-
-    # Write new entries
-    for table, carrier in zip(
-        [methanation, SMR, feed_in], ["H2_to_CH4", "CH4_to_H2", "H2_feedin"]
-    ):
-
-        # set parameters according to carrier name
-        table["carrier"] = carrier
-        table["efficiency"] = scn_params["efficiency"][carrier]
-        if carrier != "H2_feedin":
-            table["p_nom_extendable"] = True
-            table["capital_cost"] = scn_params["capital_cost"][carrier]
-            table["lifetime"] = scn_params["lifetime"][carrier]
-        new_id = db.next_etrago_id("link")
-        table["link_id"] = range(new_id, new_id + len(table))
-
-        table = link_geom_from_buses(table, scn_name)
-
-        table.to_postgis(
-            "egon_etrago_link",
-            engine,
-            schema="grid",
-            index=False,
-            if_exists="append",
-            dtype={"topo": Geometry()},
-        )
-
-
-def insert_h2_to_ch4_eGon100RE():
-    """Copy H2/CH4 links from the eGon2035 to the eGon100RE scenario."""
-    copy_and_modify_links(
-        "eGon2035", "eGon100RE", ["H2_to_CH4", "CH4_to_H2"], "gas"
-    )
-
+    
+        # Write new entries
+        for table, carrier in zip(
+            [methanation, SMR, feed_in], ["H2_to_CH4", "CH4_to_H2", "H2_feedin"]
+        ):
+    
+            # set parameters according to carrier name
+            table["carrier"] = carrier
+            table["efficiency"] = scn_params["efficiency"][carrier]
+            if carrier != "H2_feedin":
+                table["p_nom_extendable"] = True
+                table["capital_cost"] = scn_params["capital_cost"][carrier]
+                table["lifetime"] = scn_params["lifetime"][carrier]
+            new_id = db.next_etrago_id("link")
+            table["link_id"] = range(new_id, new_id + len(table))
+    
+            table = link_geom_from_buses(table, scn_name)
+    
+            table.to_postgis(
+                "egon_etrago_link",
+                engine,
+                schema="grid",
+                index=False,
+                if_exists="append",
+                dtype={"topo": Geometry()},
+            )
 
 def H2_CH4_mix_energy_fractions(x, T=25, p=50):
     """
--- src/egon/data/datasets/scenario_parameters/parameters.py
+++ src/egon/data/datasets/scenario_parameters/parameters.py
@@ -788,6 +788,7 @@ def gas(scenario):
         parameters = {
             "main_gas_carrier": "H2",
             "retrofitted_CH4pipeline-to-H2pipeline_share": 0.23,
+            "H2_feedin_volumetric_fraction": 0.15,
             # p-e-s result, this value is overwritten if p-e-s is run
         }
         # Insert effciencies in p.u.
@@ -797,6 +798,7 @@ def gas(scenario):
             "CH4_to_H2": read_costs(costs, "SMR", "efficiency"),
             "H2_to_CH4": read_costs(costs, "methanation", "efficiency"),
             "OCGT": read_costs(costs, "OCGT", "efficiency"),
+            "H2_feedin": 1,
         }
 
         # Insert FOM in %
