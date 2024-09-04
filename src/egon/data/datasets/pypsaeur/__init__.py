4b7347b859d1d491ec2ac56d0ad41abb6b1e68f3
CarlosEpia <carlosepiar@gmail.com>
2024-09-04 16:04:49 +0200

    fix delete statement

--- src/egon/data/datasets/pypsaeur/__init__.py
+++ src/egon/data/datasets/pypsaeur/__init__.py
@@ -377,11 +377,12 @@ def clean_database():
             ;"""
         )
 
-    db.execute_sql(
-        "DELETE FROM grid.egon_etrago_bus "
-        "WHERE scn_name = '{scn_name}' "
-        "AND country <> 'DE' "
-        "AND carrier <> 'AC'"
+    db.execute_sql(f"""
+        DELETE FROM grid.egon_etrago_bus
+        WHERE scn_name = '{scn_name}'
+        AND country <> 'DE'
+        AND carrier <> 'AC'
+        """
     )
 
 
