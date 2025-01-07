f100620b0f8324e23406b356f65d3d150b01b481
ClaraBuettner <clara.buettner@hs-flensburg.de>
2025-01-07 17:23:56 +0100

    Use read_network function to access results from pypsa-eur

--- src/egon/data/datasets/chp/__init__.py
+++ src/egon/data/datasets/chp/__init__.py
@@ -36,6 +36,8 @@ from egon.data.datasets.power_plants import (
     scale_prox2now,
 )
 
+from egon.data.datasets.pypsaeur import read_network
+
 Base = declarative_base()
 
 
@@ -588,16 +590,9 @@ def insert_chp_egon100re():
 
     if config.settings()["egon-data"]["--dataset-boundary"] != "Everything":
         additional_capacity /= 16
-    target_file = (
-        Path(".")
-        / "data_bundle_egon_data"
-        / "pypsa_eur_sec"
-        / "2022-07-26-egondata-integration"
-        / "postnetworks"
-        / "elec_s_37_lv2.0__Co2L0-1H-T-H-B-I-dist1_2050.nc"
-    )
 
-    network = pypsa.Network(str(target_file))
+    network = read_network()
+
     chp_index = "DE0 0 urban central gas CHP"
 
     standard_chp_th = 10
