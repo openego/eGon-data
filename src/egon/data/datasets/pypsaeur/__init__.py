e1e9aac8d35a7a6c72e9ad67309f3a6cec9fc72c
CarlosEpia <caepiar@unal.edu.co>
2025-02-20 11:27:27 +0100

    update version

--- src/egon/data/datasets/pypsaeur/__init__.py
+++ src/egon/data/datasets/pypsaeur/__init__.py
@@ -43,7 +43,7 @@ class RunPypsaEur(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="SolvePypsaEur",
-            version="0.0.40",
+            version="0.0.41",
             dependencies=dependencies,
             tasks=(
                 prepare_network_2,
