477b7dddfffcb017e4a38222603f2dd7e69648ce
ClaraBuettner <clara.buettner@hs-flensburg.de>
2025-06-19 14:55:34 +0200

    Remove dependencies from the e-mobility dataset that were only set to prevent id conflicts

--- src/egon/data/airflow/dags/pipeline.py
+++ src/egon/data/airflow/dags/pipeline.py
@@ -649,18 +649,6 @@ with airflow.DAG(
             setup_etrago,
             zensus_mv_grid_districts,
             zensus_vg250,
-            storage_etrago,
-            hts_etrago_table,
-            chp_etrago,
-            components_dsm,
-            heat_etrago,
-            fill_etrago_generators,
-            create_ocgt,
-            insert_H2_storage,
-            insert_power_to_h2_installations,
-            insert_h2_to_ch4_grid_links,
-            gas_production_insert_data,
-            insert_data_ch4_storages,
         ]
     )
 
