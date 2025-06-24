e4aed4ba3961c51bdadfec08e7ef30ff6232ba00
ClaraBuettner <clara.buettner@hs-flensburg.de>
2025-06-24 09:46:35 +0200

    Fix getting version of meta data

--- src/egon/data/datasets/storages/home_batteries.py
+++ src/egon/data/datasets/storages/home_batteries.py
@@ -334,7 +334,7 @@ def add_metadata():
         },
     }
 
-    dialect = get_dialect(meta_metadata())()
+    dialect = get_dialect(f"oep-v{meta_metadata()['metadataVersion'][4:7]}")()
 
     meta = dialect.compile_and_render(dialect.parse(json.dumps(meta)))
 
