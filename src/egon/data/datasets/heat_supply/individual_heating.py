cc8101db1b146ed27b5136be967cceff837e986f
ClaraBuettner <clara.buettner@hs-flensburg.de>
2025-07-15 13:01:38 +0200

    Fix typo in checking distribution method

--- src/egon/data/datasets/heat_supply/individual_heating.py
+++ src/egon/data/datasets/heat_supply/individual_heating.py
@@ -600,7 +600,7 @@ def cascade_per_technology(
 
     # Distribute heat pumps linear to remaining demand.
     if tech.index == "heat_pump":
-        if distribution_level == "federal_state":
+        if distribution_level == "federal_states":
             # Select target values per federal state
             target = db.select_dataframe(
                 f"""
