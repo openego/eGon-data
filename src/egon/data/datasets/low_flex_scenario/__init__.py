f9a38344010b04581cd0de28f3e3d8c83963126c
ClaraBuettner <clara.buettner@hs-flensburg.de>
2025-08-11 11:45:21 +0200

    Make sure that the connection settings are suitable for airflow version 2.10

--- src/egon/data/datasets/low_flex_scenario/__init__.py
+++ src/egon/data/datasets/low_flex_scenario/__init__.py
@@ -1,18 +1,35 @@
 """The central module to create low flex scenarios
 
 """
+from airflow.models import Connection
 from airflow.operators.postgres_operator import PostgresOperator
-from sqlalchemy.ext.declarative import declarative_base
+from airflow.settings import Session
 from importlib_resources import files
+from sqlalchemy.ext.declarative import declarative_base
 
 from egon.data.datasets import Dataset
-
 
 Base = declarative_base()
 
 
+def ensure_postgres_connection():
+    session = Session()
+
+    conn = (
+        session.query(Connection)
+        .filter(Connection.conn_id == "egon_data")
+        .one_or_none()
+    )
+
+    conn.conn_type = "postgres"
+
+    session.commit()
+    session.close()
+
+
 class LowFlexScenario(Dataset):
     def __init__(self, dependencies):
+        ensure_postgres_connection()
         super().__init__(
             name="low_flex_scenario",
             version="0.0.1",
