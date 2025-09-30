810e8221c5489937becba7b0864cd1dbe95b2227
CarlosEpia <caepiar@unal.edu.co>
2025-09-30 15:33:29 +0200

    use mastr data from 2025-02-09

--- src/egon/data/datasets/mastr.py
+++ src/egon/data/datasets/mastr.py
@@ -5,12 +5,14 @@ Download Marktstammdatenregister (MaStR) from Zenodo.
 
 from pathlib import Path
 from urllib.request import urlretrieve
+import pandas as pd
 import os
+import zipfile
 
 from egon.data.datasets import Dataset
 import egon.data.config
 
-WORKING_DIR_MASTR_NEW = Path(".", "bnetza_mastr", "dump_2024-01-08")
+WORKING_DIR_MASTR_NEW = Path(".", "bnetza_mastr", "dump_2025-02-09")
 
 
 def download_mastr_data():
@@ -24,24 +26,164 @@ def download_mastr_data():
             f"https://zenodo.org/record/" f"{data_config['deposit_id']}/files/"
         )
 
-        files = []
-        for technology in data_config["technologies"]:
-            files.append(
-                f"{data_config['file_basename']}_{technology}_cleaned.csv"
-            )
-        files.append("location_elec_generation_raw.csv")
+        dump_file_name = data_config["dump_name"] + ".zip"
 
-        # Retrieve specified files
-        for filename in files:
-            if not os.path.isfile(filename):
-                urlretrieve(
-                    zenodo_files_url + filename, download_dir / filename
-                )
+        if not os.path.isfile(dump_file_name):
+            urlretrieve(
+                zenodo_files_url + dump_file_name,
+                download_dir / dump_file_name,
+            )
 
     if not os.path.exists(WORKING_DIR_MASTR_NEW):
         WORKING_DIR_MASTR_NEW.mkdir(exist_ok=True, parents=True)
 
     download(dataset_name="mastr_new", download_dir=WORKING_DIR_MASTR_NEW)
+
+
+def extract_and_preprocess_mastr():
+
+    # Extract mastr
+    data_config = egon.data.config.datasets()["mastr_new"]
+    dump_file_name = data_config["dump_name"]
+    raw_data_path = WORKING_DIR_MASTR_NEW / dump_file_name
+
+    with zipfile.ZipFile(
+        WORKING_DIR_MASTR_NEW / (dump_file_name + ".zip"), "r"
+    ) as zip_ref:
+        zip_ref.extractall(WORKING_DIR_MASTR_NEW)
+
+    # prepocess mastr data
+    wind = pd.read_csv(raw_data_path / "bnetza_mastr_wind_raw.csv")
+    solar = pd.read_csv(raw_data_path / "bnetza_mastr_solar_raw.csv")
+    bio_with_th_power = pd.read_csv(
+        raw_data_path / "bnetza_mastr_biomass_raw.csv"
+    )
+    hydro = pd.read_csv(raw_data_path / "bnetza_mastr_hydro_raw.csv")
+    gsgk = pd.read_csv(raw_data_path / "bnetza_mastr_gsgk_raw.csv")
+    storage = pd.read_csv(raw_data_path / "bnetza_mastr_storage_raw.csv")
+    combustion_with_th_power = pd.read_csv(
+        raw_data_path / "bnetza_mastr_combustion_raw.csv"
+    )
+    nuclear = pd.read_csv(raw_data_path / "bnetza_mastr_nuclear_raw.csv")
+
+    loc = pd.read_csv(
+        raw_data_path / "bnetza_mastr_locations_extended_raw.csv"
+    )
+    gcp = pd.read_csv(raw_data_path / "bnetza_mastr_grid_connections_raw.csv")
+
+    loc_vlevel = loc.merge(
+        gcp,
+        left_on="Netzanschlusspunkte",
+        right_on="NetzanschlusspunktMastrNummer",
+        how="left",
+    )
+
+    loc_vlevel.replace(
+        {
+            "Spannungsebene": {
+                "Niederspannung (= Hausanschluss/Haushaltsstrom)": "Niederspannung",
+                "Umspannebene Mittelspannung/Niederspannung": "UmspannungZurNiederspannung",
+                "Umspannebene Hochspannung/Mittelspannung": "UmspannungZurMittelspannung",
+                "Umspannebene Höchstspannung/Hochspannung": "UmspannungZurHochspannung",
+            }
+        },
+        inplace=True,
+    )
+
+    # Locations and grid conn. points
+    cols_mapping = {"MastrNummer": "MaStRNummer"}
+    loc_vlevel.rename(columns=cols_mapping).to_csv(
+        WORKING_DIR_MASTR_NEW / "location_elec_generation_raw.csv",
+        index=None,
+        encoding="UTF-8",
+    )
+
+    # Fix solar
+    solar["Standort"] = solar.Postleitzahl.apply(str) + " " + solar.Ort
+    solar["Bruttoleistung_extended"] = solar.Bruttoleistung
+    solar["InstallierteLeistung"] = solar.Bruttoleistung
+
+    cols_mapping = {
+        "ZugeordneteWirkleistungWechselrichter": "zugeordneteWirkleistungWechselrichter"
+    }
+
+    solar.rename(columns=cols_mapping, inplace=True)
+
+    cols_mapping = {"MastrNummer": "MaStRNummer"}
+
+    states_renaming = {
+        "Thüringen": "Thueringen",
+        "Schleswig-Holstein": "SchleswigHolstein",
+        "Nordrhein-Westfalen": "NordrheinWestfalen",
+        "Rheinland-Pfalz": "RheinlandPfalz",
+        "Baden-Württemberg": "BadenWuerttemberg",
+        "Sachsen-Anhalt": "SachsenAnhalt",
+        "Mecklenburg-Vorpommern": "MecklenburgVorpommern",
+        "Ausschließliche Wirtschaftszone": "AusschliesslicheWirtschaftszone",
+    }
+    status_renaming = {
+        "In Betrieb": "InBetrieb",
+        "Vorübergehend stillgelegt": "VoruebergehendStillgelegt",
+        "Endgültig stillgelegt": "DauerhaftStillgelegt",
+        "In Planung": "InPlanung",
+    }
+    values_renaming = {
+        "Bundesland": states_renaming,
+        "EinheitBetriebsstatus": status_renaming,
+    }
+
+    # Export data
+    wind.rename(columns=cols_mapping).replace(values_renaming).to_csv(
+        WORKING_DIR_MASTR_NEW / "bnetza_mastr_wind_cleaned.csv",
+        index=None,
+        encoding="UTF-8",
+    )
+
+    solar.rename(columns=cols_mapping).replace(values_renaming).to_csv(
+        WORKING_DIR_MASTR_NEW / "bnetza_mastr_solar_cleaned.csv",
+        index=None,
+        encoding="UTF-8",
+    )
+
+    bio_with_th_power.rename(columns=cols_mapping).replace(
+        values_renaming
+    ).to_csv(
+        WORKING_DIR_MASTR_NEW / "bnetza_mastr_biomass_cleaned.csv",
+        index=None,
+        encoding="UTF-8",
+    )
+
+    hydro.rename(columns=cols_mapping).replace(values_renaming).to_csv(
+        WORKING_DIR_MASTR_NEW / "bnetza_mastr_hydro_cleaned.csv",
+        index=None,
+        encoding="UTF-8",
+    )
+
+    gsgk.rename(columns=cols_mapping).replace(values_renaming).to_csv(
+        WORKING_DIR_MASTR_NEW / "bnetza_mastr_gsgk_cleaned.csv",
+        index=None,
+        encoding="UTF-8",
+    )
+
+    storage.rename(columns=cols_mapping).replace(values_renaming).to_csv(
+        WORKING_DIR_MASTR_NEW / "bnetza_mastr_storage_cleaned.csv",
+        index=None,
+        encoding="UTF-8",
+    )
+
+    combustion_with_th_power.rename(columns=cols_mapping).replace(
+        values_renaming
+    ).to_csv(
+        WORKING_DIR_MASTR_NEW / "bnetza_mastr_combustion_cleaned.csv",
+        index=None,
+        encoding="UTF-8",
+    )
+
+    nuclear.rename(columns=cols_mapping).replace(values_renaming).to_csv(
+        WORKING_DIR_MASTR_NEW / "bnetza_mastr_nuclear_cleaned.csv",
+        index=None,
+        encoding="UTF-8",
+    )
 
 
 class mastr_data_setup(Dataset):
@@ -76,7 +218,7 @@ class mastr_data_setup(Dataset):
     #:
     version: str = "0.0.2"
     #:
-    tasks = (download_mastr_data,)
+    tasks = (download_mastr_data, extract_and_preprocess_mastr)
 
     def __init__(self, dependencies):
         super().__init__(
