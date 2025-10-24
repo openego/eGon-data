543cdfde529ff877a022a32fbf0ba3603c10450e
mheshammenisy <mhesham.menisy@aucegypt.edu>
2025-10-24 15:13:24 +0200

    fix: sources/targets and attributes

--- src/egon/data/datasets/osm/__init__.py
+++ src/egon/data/datasets/osm/__init__.py
@@ -34,46 +34,61 @@ import egon.data.subprocess as subprocess
 
 
 def download():
-    """Download OpenStreetMap `.pbf` file."""
-    # The old config variables are now removed.
+    
 
-    download_directory = Path("openstreetmap")
+    download_directory = Path(".") / "openstreetmap"
+    # Create the folder, if it does not exists already
     if not os.path.exists(download_directory):
         os.mkdir(download_directory)
 
-    # The logic now uses the new class attributes
     if settings()["egon-data"]["--dataset-boundary"] == "Everything":
         source_url = OpenStreetMap.sources.urls["germany"]
-        target_file = Path(OpenStreetMap.targets.files["pbf_germany"])
+        target_filename = Path(OpenStreetMap.targets.files["germany"])
     else:
         source_url = OpenStreetMap.sources.urls["schleswig-holstein"]
-        target_file = Path(OpenStreetMap.targets.files["pbf_schleswig-holstein"])
+        target_filename = Path(OpenStreetMap.targets.files["schleswig-holstein"])
+        
+    target_file = download_directory / target_filename
 
     if not os.path.isfile(target_file):
         urlretrieve(source_url, target_file)
         
 
 def to_postgres(cache_size=4096):
-    """Import OSM data from a Geofabrik `.pbf` file into a PostgreSQL database."""
+    """Import OSM data from a Geofabrik `.pbf` file into a PostgreSQL database.
+
+    Parameters
+    ----------
+    cache_size: int, optional
+        Memory used during data import
+
+    """
+    # Read maximum number of threads per task from egon-data.configuration.yaml
     num_processes = settings()["egon-data"]["--processes-per-task"]
+
+    # Read database configuration from docker-compose.yml
     docker_db_config = db.credentials()
 
-    # The old config variables are now removed.
-
-    # The logic now uses the new class attributes
+    # Drop old target tables (the list is in OpenStreetMap.targets.tables)
+    for table in OpenStreetMap.targets.tables:
+        db.execute_sql(f"DROP TABLE IF EXISTS {OpenStreetMap.schema}.{table} CASCADE;")
+        
     if settings()["egon-data"]["--dataset-boundary"] == "Everything":
-        input_file = Path(OpenStreetMap.targets.files["pbf_germany"])
+        input_filename = Path(OpenStreetMap.targets.files["germany"])
         logger.info("Using Everything DE dataset.")
     else:
-        input_file = Path(OpenStreetMap.targets.files["pbf_schleswig-holstein"])
+        input_filename = Path(OpenStreetMap.targets.files["schleswig-holstein"])
         logger.info("Using testmode SH dataset.")
 
-    style_file = Path("openstreetmap") / OpenStreetMap.sources.files["stylefile"]
+    input_file = Path(".") / "openstreetmap" / input_filename
+    style_file = (
+    Path(".") / "openstreetmap" / OpenStreetMap.sources.files["stylefile"]
+    )
     with resources.path(
         "egon.data.datasets.osm", OpenStreetMap.sources.files["stylefile"]
     ) as p:
         shutil.copy(p, style_file)
-
+        
     # Prepare osm2pgsql command
     cmd = [
         "osm2pgsql",
@@ -105,20 +120,20 @@ def to_postgres(cache_size=4096):
         env={"PGPASSWORD": docker_db_config["POSTGRES_PASSWORD"]},
         cwd=Path(__file__).parent,
     )
-    
+      
 
 
 def add_metadata():
     """Writes metadata JSON string into table comment."""
-    # The old config variable is now removed.
+    
 
-    # Logic is updated to use the new class attributes
+    
     if settings()["egon-data"]["--dataset-boundary"] == "Everything":
         osm_url = OpenStreetMap.sources.urls["germany"]
-        input_filename = OpenStreetMap.targets.files["pbf_germany"]
+        input_filename = OpenStreetMap.targets.files["germany"]
     else:
         osm_url = OpenStreetMap.sources.urls["schleswig-holstein"]
-        input_filename = OpenStreetMap.targets.files["pbf_schleswig-holstein"]
+        input_filename = OpenStreetMap.targets.files["schleswig-holstein"]
 
     (spatial_extend, osm_data_date) = re.compile(
         "^([\\w-]*).*-(\\d+)$"
@@ -130,9 +145,9 @@ def add_metadata():
     licenses = [license_odbl(attribution="© OpenStreetMap contributors")]
 
 
-    for schema_table in OpenStreetMap.targets.tables.values():
-        schema, table_name = schema_table.split(".")
-        table_suffix = table_name.split("_")[1]
+    for table in OpenStreetMap.targets.tables:
+        schema_table = ".".join([OpenStreetMap.schema, table])
+        table_suffix = table.split("_")[1]
         meta = {
             "name": schema_table,
             "title": f"OpenStreetMap (OSM) - Germany - {table_suffix}",
@@ -145,7 +160,52 @@ def add_metadata():
                 "The OpenStreetMap data here is the result of an PostgreSQL "
                 "database import using osm2pgsql with a custom style file."
             ),
-            # ... (rest of the metadata dictionary is unchanged, except for the 'resources' section) ...
+            "language": ["en-EN", "de-DE"],
+            "publicationDate": datetime.date.today().isoformat(),
+            "context": context(),
+            "spatial": {
+                "location": None,
+                "extent": f"{spatial_extend}",
+                "resolution": None,
+            },
+            "temporal": {
+                "referenceDate": f"{osm_data_date}",
+                "timeseries": {
+                    "start": None,
+                    "end": None,
+                    "resolution": None,
+                    "alignment": None,
+                    "aggregationType": None,
+                },
+            },
+            "sources": [
+                {
+                    "title": "OpenStreetMap Data Extracts (Geofabrik)",
+                    "description": (
+                        "Full data extract of OpenStreetMap data for defined "
+                        "spatial extent at ''referenceDate''"
+                    ),
+                    "path": f"{osm_url}",
+                    "licenses": licenses,
+                }
+            ],
+            "licenses": licenses,
+            "contributors": [
+                {
+                    "title": "Guido Pleßmann",
+                    "email": "http://github.com/gplssm",
+                    "date": time.strftime("%Y-%m-%d"),
+                    "object": None,
+                    "comment": "Imported data",
+                },
+                {
+                    "title": "Jonathan Amme",
+                    "email": "http://github.com/nesnoj",
+                    "date": time.strftime("%Y-%m-%d"),
+                    "object": None,
+                    "comment": "Metadata extended",
+                },
+            ],
             "resources": [
                 {
                     "profile": "tabular-data-resource",
@@ -155,7 +215,7 @@ def add_metadata():
                     "encoding": "UTF-8",
                     "schema": {
                         "fields": generate_resource_fields_from_db_table(
-                            schema, table_name  # This line is updated
+                            OpenStreetMap.schema, table
                         ),
                         "primaryKey": ["id"],
                         "foreignKeys": [],
@@ -166,7 +226,7 @@ def add_metadata():
             "metaMetadata": meta_metadata(),
         }
         meta_json = "'" + json.dumps(meta) + "'"
-        db.submit_comment(meta_json, schema, table_name)
+        db.submit_comment(meta_json, OpenStreetMap.schema, table)
 
 
 def modify_tables():
@@ -176,60 +236,91 @@ def modify_tables():
     * Indices (GIST, GIN) are reset
     * The tables are moved to the schema configured as the "output_schema".
     """
-    # Get the target schema name from one of the target tables
-    schema = OpenStreetMap.targets.get_table_schema("line")
-    db.execute_sql(f"CREATE SCHEMA IF NOT EXISTS {schema};")
 
-    # Loop through the target tables defined in the class
-    for key, final_table_name in OpenStreetMap.targets.tables.items():
-        # Define the initial table name created by osm2pgsql in the public schema
-        public_table_name = f"public.{OpenStreetMap.table_prefix}_{key}"
+    # Replace indices and primary keys
+    for table in [
+        f"{OpenStreetMap.table_prefix}_" + suffix
+        for suffix in ["line", "point", "polygon", "roads"]
+    ]:
 
-        sql_statements = [
-            f"DROP INDEX IF EXISTS {public_table_name}_index;",
-            f"DROP INDEX IF EXISTS {public_table_name}_pkey;",
-            f"ALTER TABLE {public_table_name} ADD id SERIAL;",
-            f"ALTER TABLE {public_table_name} ADD PRIMARY KEY (id);",
-            f"ALTER TABLE {public_table_name} RENAME COLUMN way TO geom;",
-            f"CREATE INDEX {public_table_name}_geom_idx ON {public_table_name} USING gist (geom);",
-            f"CREATE INDEX {public_table_name}_tags_idx ON {public_table_name} USING GIN (tags);",
-        ]
+        # Drop indices
+        sql_statements = [f"DROP INDEX IF EXISTS {table}_index;"]
 
-    for statement in sql_statements:
-            # Use try-except to avoid errors if a column/index doesn't exist
-            try:
-                db.execute_sql(statement)
-            except Exception:
-                logger.warning(f"Could not execute: {statement}")
+        # Drop primary keys
+        sql_statements.append(f"DROP INDEX IF EXISTS {table}_pkey;")
 
-    db.execute_sql(f"DROP TABLE IF EXISTS {final_table_name};")
-    db.execute_sql(
-            f"ALTER TABLE {public_table_name} SET SCHEMA {schema};"
+        # Add primary key on newly created column "id"
+        sql_statements.append(f"ALTER TABLE public.{table} ADD id SERIAL;")
+        sql_statements.append(
+            f"ALTER TABLE public.{table} ADD PRIMARY KEY (id);"
+        )
+        sql_statements.append(
+            f"ALTER TABLE public.{table} RENAME COLUMN way TO geom;"
         )
 
+        # Add indices (GIST and GIN)
+        sql_statements.append(
+            f"CREATE INDEX {table}_geom_idx ON public.{table} "
+            f"USING gist (geom);"
+        )
+        sql_statements.append(
+            f"CREATE INDEX {table}_tags_idx ON public.{table} "
+            f"USING GIN (tags);"
+        )
+
+        # Execute collected SQL statements
+        for statement in sql_statements:
+            db.execute_sql(statement)
+
+    db.execute_sql(
+        f"CREATE SCHEMA IF NOT EXISTS {OpenStreetMap.schema};"
+    )
+
+    for out_table in OpenStreetMap.targets.tables:
+        db.execute_sql(
+            f"DROP TABLE IF EXISTS "
+            f"{OpenStreetMap.schema}.{out_table};"
+        )
+
+        sql_statement = (
+            f"ALTER TABLE public.{out_table} "
+           f"SET SCHEMA {OpenStreetMap.schema};"
+        )
+
+        db.execute_sql(sql_statement)
+        
 class OpenStreetMap(Dataset):
     
+    #:
+    name: str = "OpenStreetMap"
+    #:
+    version: str = "0.0.7"
+    
+    table_prefix: str = "osm"
+    schema: str = "openstreetmap"
+    
     sources = DatasetSources(
+        files={"stylefile": "oedb.style"},
         urls={
-            "germany": "https://download.geofabrik.de/europe/germany-latest.osm.pbf",
-            "schleswig-holstein": "https://download.geofabrik.de/germany/schleswig-holstein-latest.osm.pbf",
+            "germany": "https://download.geofabrik.de/europe/germany-240101.osm.pbf",
+            "schleswig-holstein": "https://download.geofabrik.de/europe/germany/schleswig-holstein-240101.osm.pbf",
         },
-        files={"stylefile": "default.style"},
     )
     targets = DatasetTargets(
         files={
-            "pbf_germany": "openstreetmap/germany-latest.osm.pbf",
-            "pbf_schleswig-holstein": "openstreetmap/schleswig-holstein-latest.osm.pbf",
+            "germany": "germany-240101.osm.pbf",
+            "schleswig-holstein": "schleswig-holstein-240101.osm.pbf",
         },
-        tables={
-            "line": "openstreetmap.osm_line",
-            "point": "openstreetmap.osm_point",
-            "polygon": "openstreetmap.osm_polygon",
-            "roads": "openstreetmap.osm_roads",
-        },
-        
+        tables=[
+            "osm_line",
+            "osm_nodes",
+            "osm_point",
+            "osm_polygon",
+            "osm_rels",
+            "osm_roads",
+            "osm_ways",
+        ],
     )
-    table_prefix="osm",
     """
     Downloads OpenStreetMap data from Geofabrik and writes it to database.
 
@@ -248,11 +339,7 @@ class OpenStreetMap(Dataset):
     See documentation section :ref:`osm-ref` for more information.
 
     """
-
-    #:
-    name: str = "OpenStreetMap"
-    #:
-    version: str = "0.0.5"
+    
 
     def __init__(self, dependencies):
         super().__init__(
