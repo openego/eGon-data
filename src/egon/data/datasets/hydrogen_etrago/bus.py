31bc5003a5abccbdef0c40ebe14286ebf626291b
mheshammenisy <mhesham.menisy@aucegypt.edu>
2025-11-24 00:44:08 +0100

    etrago_hydrogen config in hydrogen bus

--- src/egon/data/datasets/hydrogen_etrago/bus.py
+++ src/egon/data/datasets/hydrogen_etrago/bus.py
@@ -28,9 +28,6 @@ from egon.data.datasets.etrago_helpers import (
     finalize_bus_insertion,
     initialise_bus_insertion,
 )
-from egon.data.datasets import load_sources_and_targets
-
-sources, targets = load_sources_and_targets("HydrogenBusEtrago")
 
 
 
@@ -57,7 +54,10 @@ def insert_hydrogen_buses(scn_name):
         lambda wkb_hex: loads(bytes.fromhex(wkb_hex))
     )
 
-    target_buses = targets.tables["hydrogen_buses"]
+    sources = config.datasets()["etrago_hydrogen"]["sources"]
+    target_buses = config.datasets()["etrago_hydrogen"]["targets"][
+        "hydrogen_buses"
+    ]
     h2_buses = initialise_bus_insertion(
         "H2_grid", target_buses, scenario=scn_name
     )
@@ -176,16 +176,16 @@ def insert_H2_buses_from_saltcavern(gdf, carrier, sources, target, scn_name):
     el_buses = db.select_dataframe(
         f"""
         SELECT bus_id
-        FROM  {sources.tables['saltcavern_data']['schema']}.
-        {sources.tables['saltcavern_data']['table']}"""
+        FROM  {sources['saltcavern_data']['schema']}.
+        {sources['saltcavern_data']['table']}"""
     )["bus_id"]
 
     # locations of electrical buses (filtering not necessarily required)
     locations = db.select_geodataframe(
         f"""
         SELECT bus_id, geom
-        FROM  {sources.tables['buses']['schema']}.
-        {sources.tables['buses']['table']} WHERE scn_name = '{scn_name}'
+        FROM  {sources['buses']['schema']}.
+        {sources['buses']['table']} WHERE scn_name = '{scn_name}'
         AND country = 'DE'""",
         index_col="bus_id",
     ).to_crs(epsg=4326)
@@ -211,9 +211,9 @@ def insert_H2_buses_from_saltcavern(gdf, carrier, sources, target, scn_name):
 
     # Insert data to db
     gdf_H2_cavern.to_sql(
-        sources.tables["H2_AC_map"]["table"],
+        "egon_etrago_ac_h2",
         db.engine(),
-        schema=sources.tables["H2_AC_map"]["schema"],
+        schema="grid",
         index=False,
         if_exists="replace",
     )
