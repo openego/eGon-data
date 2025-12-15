c3e939d47c808a1162ea49e9cc1e55e52492524e
jh-RLI <jonas.huber@rl-institut.de>
2025-12-15 14:51:14 +0100

    #1177: Add additional settings related to integration of omis new metadata system

--- src/egon/data/metadata/settings.py
+++ src/egon/data/metadata/settings.py
@@ -1,6 +1,18 @@
+from pathlib import Path
+
 # The oemetadata version identifier must comply with on of:
-# https://github.com/OpenEnergyPlatform/omi/blob/7e620182b0ea8eb2e437bd650b5cda3db0532307/src/omi/base.py#L17
+# https://github.com/OpenEnergyPlatform/omi/blob/7e620182b0ea8eb2e437bd650b5cda3db0532307/src/omi/base.py#L17 # noqa E501
 OEMETADATA_VERSION = "OEMetadata-2.0"
+
+# This is the id of the Datasets where n tables are included,
+# it is used by the OMI
+# tool to generate oemetadata
+OEM_DATASET_ID = "egon-data"
+
+# Path to the base directory where the dataset metadata YAML-files are stored
+# to be used by the OMI tool to generate oemetadata python dict objects or
+# json files
+OEM_BASE_DIR = Path("code/eGon-data/src/egon/data/metadata/dataset_metadata/")
 
 
 EGON_ATTRIBUTION: str = "© eGon development team"
