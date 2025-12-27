9c9771fa87f8f076fa91f13ee0343f5e1cd9f67e
mheshammenisy <mhesham.menisy@aucegypt.edu>
2025-12-27 22:32:15 +0100

    cts_buildings: define DatasetSources and DatasetTargets

--- src/egon/data/datasets/electricity_demand_timeseries/cts_buildings.py
+++ src/egon/data/datasets/electricity_demand_timeseries/cts_buildings.py
@@ -21,7 +21,7 @@ import saio
 
 from egon.data import config, db
 from egon.data import logger as log
-from egon.data.datasets import Dataset
+from egon.data.datasets import Dataset, DatasetSources, DatasetTargets
 from egon.data.datasets.electricity_demand import (
     EgonDemandRegioZensusElectricity,
 )
@@ -230,7 +230,82 @@ class CtsDemandBuildings(Dataset):
     #:
     name: str = "CtsDemandBuildings"
     #:
-    version: str = "0.0.4"
+    version: str = "0.0.5"
+    sources = DatasetSources(
+        tables={
+            "osm_buildings_filtered": {
+                "schema": "openstreetmap",
+                "table": "osm_buildings_filtered",
+            },
+            "osm_amenities_shops_filtered": {
+                "schema": "openstreetmap",
+                "table": "osm_amenities_shops_filtered",
+            },
+            "osm_amenities_not_in_buildings_filtered": {
+                "schema": "openstreetmap",
+                "table": "osm_amenities_not_in_buildings_filtered",
+            },
+            "osm_buildings_synthetic": {
+                "schema": "openstreetmap",
+                "table": "osm_buildings_synthetic",
+            },    
+            "map_zensus_buildings_filtered_all": {
+                "schema": "boundaries",
+                "table": "egon_map_zensus_buildings_filtered_all",
+             },
+
+            "zensus_electricity": {
+                "schema": "demand",
+                "table": "egon_demandregio_zensus_electricity",
+            },
+            "peta_heat": {
+                "schema": "demand",
+                "table": "egon_peta_heat",
+            },
+            "etrago_electricity_cts": {
+                "schema": "demand",
+                "table": "egon_etrago_electricity_cts",
+            },
+            "etrago_heat_cts": {
+                "schema": "demand",
+                "table": "egon_etrago_heat_cts",
+            },
+        }
+    )
+
+    targets = DatasetTargets(
+        tables={        
+            "cts_buildings": {
+                "schema": "openstreetmap",
+                "table": "egon_cts_buildings",
+            },
+            "cts_electricity_building_share": {
+                "schema": "demand",
+                "table": "egon_cts_electricity_demand_building_share",
+            },
+            "cts_heat_building_share": {
+                "schema": "demand",
+                "table": "egon_cts_heat_demand_building_share",
+            },
+            "osm_buildings_synthetic": {
+                "schema": "openstreetmap",
+                "table": "osm_buildings_synthetic",
+            },
+            "building_electricity_peak_loads": {
+                "schema": "demand",
+                "table": "egon_building_electricity_peak_loads",
+            },
+            "building_heat_peak_loads": {
+                "schema": "demand",
+                "table": "egon_building_heat_peak_loads",
+            },
+            "map_zensus_mvgd_buildings": {
+                "schema": "boundaries",
+                "table": "egon_map_zensus_mvgd_buildings",
+            },
+        }
+    )
+
 
     def __init__(self, dependencies):
         super().__init__(
