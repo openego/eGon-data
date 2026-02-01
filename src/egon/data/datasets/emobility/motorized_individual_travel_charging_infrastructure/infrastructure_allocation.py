f93c206acb86af1c9c7bd4b6628f9215ac698d1b
Amir <Amir.rezvanian@yahoo.com>
2026-02-01 22:09:36 +0100

    refactoring infrastructure allocation

--- src/egon/data/datasets/emobility/motorized_individual_travel_charging_infrastructure/infrastructure_allocation.py
+++ src/egon/data/datasets/emobility/motorized_individual_travel_charging_infrastructure/infrastructure_allocation.py
@@ -15,6 +15,7 @@ import numpy as np
 import pandas as pd
 
 from egon.data import config, db
+from egon.data.datasets import load_sources_and_targets
 from egon.data.datasets.emobility.motorized_individual_travel_charging_infrastructure.use_cases import (  # noqa: E501
     home,
     hpc,
@@ -23,7 +24,6 @@ from egon.data.datasets.emobility.motorized_individual_travel_charging_infrastru
 )
 
 WORKING_DIR = Path(".", "charging_infrastructure").resolve()
-DATASET_CFG = config.datasets()["charging_infrastructure"]
 
 
 def write_to_db(
@@ -42,21 +42,28 @@ def write_to_db(
         Calculated use case
 
     """
+    sources, targets = load_sources_and_targets("MITChargingInfrastructure")
+    sources.constants = sources.files["config_data"]
+
     if gdf.empty:
         return
 
     if "energy" in gdf.columns:
         gdf = gdf.assign(weight=gdf.energy.div(gdf.energy.sum()))
     else:
-        rng = np.random.default_rng(DATASET_CFG["constants"]["random_seed"])
+        rng = np.random.default_rng(sources.constants["random_seed"])
 
         gdf = gdf.assign(weight=rng.integers(low=0, high=100, size=len(gdf)))
 
         gdf = gdf.assign(weight=gdf.weight.div(gdf.weight.sum()))
 
+    target_conf = targets.charging_infrastructure
+    target_table = target_conf["table"]
+    target_schema = target_conf["schema"]
+
     max_id = db.select_dataframe(
-        """
-        SELECT MAX(cp_id) FROM grid.egon_emob_charging_infrastructure
+        f"""
+        SELECT MAX(cp_id) FROM {target_schema}.{target_table}
         """
     )["max"][0]
 
@@ -69,12 +76,11 @@ def write_to_db(
         use_case=use_case,
     )
 
-    targets = DATASET_CFG["targets"]
-    cols_to_export = targets["charging_infrastructure"]["cols_to_export"]
+    cols_to_export = sources.constants["cols_to_export"]
 
     gpd.GeoDataFrame(gdf[cols_to_export], crs=gdf.crs).to_postgis(
-        targets["charging_infrastructure"]["table"],
-        schema=targets["charging_infrastructure"]["schema"],
+        target_table,
+        schema=target_schema,
         con=db.engine(),
         if_exists="append",
     )
@@ -155,10 +161,12 @@ def get_data() -> dict[gpd.GeoDataFrame]:
     * miscellaneous found in *datasets.yml* in section *charging_infrastructure*
 
     Returns
-    -------
-
+    # ...
     """
-    tracbev_cfg = DATASET_CFG["original_data"]["sources"]["tracbev"]
+    sources, targets = load_sources_and_targets("MITChargingInfrastructure")
+    sources.constants = sources.files["config_data"]
+    
+    tracbev_cfg = sources.constants["tracbev_config"]
     srid = tracbev_cfg["srid"]
 
     # TODO: get zensus housing data from DB instead of gpkg?
@@ -247,26 +255,26 @@ def get_data() -> dict[gpd.GeoDataFrame]:
     )
 
     data_dict["work_dict"] = {
-        "retail": DATASET_CFG["constants"]["work_weight_retail"],
-        "commercial": DATASET_CFG["constants"]["work_weight_commercial"],
-        "industrial": DATASET_CFG["constants"]["work_weight_industrial"],
+        "retail": sources.constants["work_weight_retail"],
+        "commercial": sources.constants["work_weight_commercial"],
+        "industrial": sources.constants["work_weight_industrial"],
     }
 
-    data_dict["sfh_available"] = DATASET_CFG["constants"][
+    data_dict["sfh_available"] = sources.constants[
         "single_family_home_share"
     ]
-    data_dict["sfh_avg_spots"] = DATASET_CFG["constants"][
+    data_dict["sfh_avg_spots"] = sources.constants[
         "single_family_home_spots"
     ]
-    data_dict["mfh_available"] = DATASET_CFG["constants"][
+    data_dict["mfh_available"] = sources.constants[
         "multi_family_home_share"
     ]
-    data_dict["mfh_avg_spots"] = DATASET_CFG["constants"][
+    data_dict["mfh_avg_spots"] = sources.constants[
         "multi_family_home_spots"
     ]
 
     data_dict["random_seed"] = np.random.default_rng(
-        DATASET_CFG["constants"]["random_seed"]
+        sources.constants["random_seed"]
     )
 
     return data_dict
