e4845ab707b3fda5a74aa23cbab2862031753a44
Amir <Amir.rezvanian@yahoo.com>
2026-02-01 17:48:36 +0100

    Fixing the error

--- src/egon/data/datasets/emobility/motorized_individual_travel_charging_infrastructure/__init__.py
+++ src/egon/data/datasets/emobility/motorized_individual_travel_charging_infrastructure/__init__.py
@@ -105,35 +105,35 @@ class MITChargingInfrastructure(Dataset):
         tables={
             "mv_grid_districts": "grid.egon_mv_grid_district",
             "buildings": "demand.egon_map_houseprofiles_buildings"
-        },
-        constants={
-            "tracbev_config": {
-                "srid": 3035,
-                "files_to_use": [
-                    "hpc_positions.gpkg",
-                    "landuse.gpkg",
-                    "poi_cluster.gpkg",
-                    "public_positions.gpkg"
-                ]
-            },
-            # Constants from YAML
-            "work_weight_retail": 0.8,
-            "work_weight_commercial": 1.25,
-            "work_weight_industrial": 1,
-            "single_family_home_share": 0.6,
-            "single_family_home_spots": 1.5,
-            "multi_family_home_share": 0.4,
-            "multi_family_home_spots": 10,
-            "random_seed": 5,
-            
-            "cols_to_export": [
-                "mv_grid_id", 
-                "use_case", 
-                "weight", 
-                "geometry"
-            ]
         }
     )
+
+
+    sources.constants = {
+        "tracbev_config": {
+            "srid": 3035,
+            "files_to_use": [
+                "hpc_positions.gpkg",
+                "landuse.gpkg",
+                "poi_cluster.gpkg",
+                "public_positions.gpkg"
+            ]
+        },
+        "work_weight_retail": 0.8,
+        "work_weight_commercial": 1.25,
+        "work_weight_industrial": 1,
+        "single_family_home_share": 0.6,
+        "single_family_home_spots": 1.5,
+        "multi_family_home_share": 0.4,
+        "multi_family_home_spots": 10,
+        "random_seed": 5,
+        "cols_to_export": [
+            "mv_grid_id",
+            "use_case",
+            "weight",
+            "geometry"
+        ]
+    }
  
     targets = DatasetTargets(
         files={
@@ -187,7 +187,7 @@ class MITChargingInfrastructure(Dataset):
     #:
     name: str = "MITChargingInfrastructure"
     #:
-    version: str = "0.0.3"
+    version: str = "0.0.5"
 
     def __init__(self, dependencies):
         super().__init__(
