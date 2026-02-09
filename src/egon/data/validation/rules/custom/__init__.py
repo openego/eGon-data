88cb43086b12bd4d3dcd556e524926fb85c7da8a
sarah <sarah.sommer@rl-institut.de>
2026-02-09 10:46:50 +0100

    refactoring: auto custom rule initialization

--- src/egon/data/validation/rules/custom/__init__.py
+++ src/egon/data/validation/rules/custom/__init__.py
@@ -1,15 +1,7 @@
 """Custom validation rules for eGon data."""
 
-from .sanity import (
-    ResidentialElectricityAnnualSum,
-    ResidentialElectricityHhRefinement,
-    CtsElectricityDemandShare,
-    CtsHeatDemandShare,
-)
+from .sanity import *  # noqa: F401, F403
+from . import sanity
 
-__all__ = [
-    "ResidentialElectricityAnnualSum",
-    "ResidentialElectricityHhRefinement",
-    "CtsElectricityDemandShare",
-    "CtsHeatDemandShare",
-]
+# Re-export everything from sanity
+__all__ = sanity.__all__
