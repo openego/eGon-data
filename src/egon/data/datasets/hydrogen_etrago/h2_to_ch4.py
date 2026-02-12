e8eba336bc427fc3a0ddd044d35e3f8672bcaff3
mheshammenisy <mhesham.menisy@aucegypt.edu>
2026-02-12 21:58:49 +0100

    fix: restore sources and targets refactor h2_to_ch4

--- src/egon/data/datasets/hydrogen_etrago/h2_to_ch4.py
+++ src/egon/data/datasets/hydrogen_etrago/h2_to_ch4.py
@@ -40,15 +40,11 @@ def insert_h2_to_ch4_to_h2():
 
     """
     
-
+    sources, targets = load_sources_and_targets("HydrogenMethaneLinkEtrago")
     scenarios = config.settings()["egon-data"]["--scenarios"]
     con = db.engine()
-    target_links = config.datasets()["etrago_hydrogen"]["targets"][
-        "hydrogen_links"
-    ]
-    target_buses = config.datasets()["etrago_hydrogen"]["targets"][
-        "hydrogen_buses"
-    ]
+    target_links = targets.tables["hydrogen_links"]
+    target_buses = sources.tables["buses"]
 
 
     if "status2019" in scenarios:
