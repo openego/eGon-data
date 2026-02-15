dab05239091ba945b2c7420dee5f82eb07c8b959
mheshammenisy <mhesham.menisy@aucegypt.edu>
2026-02-15 11:18:30 +0100

    fix: bump DemandRegio version

--- src/egon/data/datasets/demandregio/__init__.py
+++ src/egon/data/datasets/demandregio/__init__.py
@@ -88,7 +88,7 @@ class DemandRegio(Dataset):
     #:
     name: str = "DemandRegio"
     #:
-    version: str = "0.0.17"
+    version: str = "0.0.18"
 
     def __init__(self, dependencies):
         super().__init__(
