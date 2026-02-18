55696877f3f314ad0950d1a1c01ae6242ce4c029
nesnoj <jonathan.amme@rl-institut.de>
2026-02-18 23:05:30 +0100

    Bump demandregio dataset version

--- src/egon/data/datasets/demandregio/__init__.py
+++ src/egon/data/datasets/demandregio/__init__.py
@@ -71,7 +71,7 @@ class DemandRegio(Dataset):
     #:
     name: str = "DemandRegio"
     #:
-    version: str = "0.0.13"
+    version: str = "0.0.14"
 
     def __init__(self, dependencies):
         super().__init__(
