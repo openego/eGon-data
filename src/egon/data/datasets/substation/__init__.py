32a76c7eb67a8943a29d19aef7b95271050bc131
mheshammenisy <mhesham.menisy@aucegypt.edu>
2026-02-20 13:36:03 +0100

    chore: bump SubstationExtraction dataset version

--- src/egon/data/datasets/substation/__init__.py
+++ src/egon/data/datasets/substation/__init__.py
@@ -92,7 +92,7 @@ class SubstationExtraction(Dataset):
     def __init__(self, dependencies):
         super().__init__(
             name="substation_extraction",
-            version="0.0.3",
+            version="0.0.4",
             dependencies=dependencies,
             tasks=(
                 create_tables,
