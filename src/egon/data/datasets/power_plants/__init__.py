36c9e8892d382a3fd8b1ef924e9d8c2893412d5f
mheshammenisy <mhesham.menisy@aucegypt.edu>
2026-02-22 15:19:36 +0100

    bump(power_plants): increase version to rerun

--- src/egon/data/datasets/power_plants/__init__.py
+++ src/egon/data/datasets/power_plants/__init__.py
@@ -1691,7 +1691,7 @@ class PowerPlants(Dataset):
     #:
     name: str = "PowerPlants"
     #:
-    version: str = "0.0.36"
+    version: str = "0.0.37"
 
     def __init__(self, dependencies):
         super().__init__(
