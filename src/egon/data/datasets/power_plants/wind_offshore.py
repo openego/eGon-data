d55fb28e5bbec2201a63ea09c7de6aa4fe174be2
Amir <Amir.rezvanian@yahoo.com>
2026-05-26 01:32:02 +0200

    Adding coordinates and synchronizing the connection points

--- src/egon/data/datasets/power_plants/wind_offshore.py
+++ src/egon/data/datasets/power_plants/wind_offshore.py
@@ -17,73 +17,77 @@ def map_id_bus(scenario, sources):
     osm_year = sources.files["osm_config"]
 
     if scenario in ["eGon2035", "eGon100RE", "eGon2037", "eGon2045"]:
+        # 28 connection points present in BOTH NEP and FEP sheets
         id_bus = {
             "Büttel": "136034396",
-            "Suchraum Gemeinden Ibbenbüren/Mettingen/Westerkappeln": "114319248",
-            "Rommerskirchen": "24839976",
-            "Oberzier": "26593929",
             "Diele": "177829920",
-            "Dörpen/West": "142487746",
+            "Dörpen West": "142487746",
             "Hagermarsch": "79316833",
-            "Hanekenfähr": "61918154",
+            "Hanekenfähr (Amprion)": "61918154",
             "Inhausen": "29420322",
-            "Unterweser": "32076853",
-            "Wehrendorf": "33411203",
-            "Bentwisch": "32063539",
-            "Lubmin": "460134233",
-            "Suchraum Gemeinde Papendorf": "32063539",
-            "Suchraum Brünzow (Gemeinden Brünzow /Kemnitz)": "460134233",
-            "inhausen": "29420322",
+            "Unterweser (TenneT)": "32076853",
+            "Wehrendorf (Amprion)": "33411203",
+            "Bentwisch (50Hertz)": "32063539",
+            "Lubmin (50Hertz)": "460134233",
+            "Gemeinde Papendorf": "32063539",
+            "Rommerskirchen (Amprion)": "24839976",
         }
         if "200101" in osm_year:
             id_bus2 = {
-                "Heide/West": "289836713",
-                "Emden/Ost": "177829920",
-                "Wilhelmshaven 2": "23837631",
-                "Suchraum Zensenbusch": "76185022",
-                "Garrel/Ost": "23837631",
-                "Rastede": "23837631",
-                "Emden/Borßum": "34835258",
-                "Cloppenburg": "50643382",
+                "Heide West": "289836713",
+                "Emden Ost": "177829920",
+                "Garrel / Ost (TenneT)": "23837631",
+                "Emden-Borssum": "34835258",
             }
         elif ("250101" in osm_year) | ("240101" in osm_year):
-            id_bus2 = { 
-                "Heide/West": "603661085",
-                "Emden/Ost": "1280178909",
-                "Wilhelmshaven 2": "637595524",
-                "Suchraum Zensenbusch": "24479003",
-                "Garrel/Ost": "24493551",
-                "Rastede": "1128250707",
-                "Emden/Borßum": "34835258",
-                "Cloppenburg": "24493551",
+            id_bus2 = {
+                "Heide West": "603661085",
+                "Emden Ost": "1280178909",
+                "Garrel / Ost (TenneT)": "24493551",
+                "Emden-Borssum": "34835258",
                 "Westerkappeln": "954305865",
-                "Niederrhein": "24462426",
+                "Niederrhein (Amprion)": "24462426",
                 "Kusenhorst": "29077623",
-                "Lippe": "957746797",
-                "Sechtem": "22766160",
-                "Suchraum Heide (Gemeinden Hemmings-tedt / Lieth / Lohe-Rickels-hof / Wöhrden)": "603661085",
-                "Suchraum Pöschendorf (Gemeinden Pöschen-dorf / Hadenfeld /Kaisborstel / Looft)": "258275257",
-                "Wiemersdorf / Hardebek": "1107961833",
-                "Suchraum BBS (Ämter Büchen / Breitenfelde / Schwarzenbek-Land)": "1089911133",
-                "Suchraum Brunsbüttel (Gemeinden Brunsbüttel / Büttel / St. Margarethen / Brokdorf)": "30622610",
-                "Suchraum Nüttermoor (Stadt Leer / Gemeinden Moormerland und Jemgum)": "1280178911",
-                "Samtgemeinde Sottrum": "955268864",
-                "Suchraum Rastede (Ovelgönne / Rastede / Wiefelstede / Westerstede)": "1128250704",
+                "Sechtem (Amprion)": "22766160",
+                "Hardebek": "1107961833",
+                "Suchraum BBS (50Hertz)": "1089911133",
+                "Suchraum Nüttermoor (TenneT)": "1280178911",
+                "Suchraum Gnewitz (50Hertz)": "940919943",
+                "Suchraum Kemnitz (50Hertz)": "460134233",
+                "Suchraum Ried (Amprion)": "1223405794",
+                "Kriftel (Amprion)": "38661452",
+                
+                "Suchraum Pöschendorf (50Hertz)": "258275257",
+                "Suchraum Pöschendorf (TenneT)": "258275257",
+ 
+
                 "Wilhelmshaven / Landkreis Friesland": "1134105414",
-                "Suchraum Gnewitz (Gemeinden Dettmanns-dorf / Gnewitz / Sanitz / Stadt Marlow)": "940919943",
-                "Suchraum Broderstorf (Gemeinden Broder-storf / Dummerstorf / Papendorf / Roggentin / Stadt Rostock)": "320635393",
-                "Suchraum Ried (Stadt Bürstadt / Gemeinden Biblis und Groß-Rohrheim)": "1223405794",
-                "Kriftel": "38661452",
+                "Suchraum Brunsbüttel (Gemeinden Brunsbüttel / Büttel / St. Margarethen / Brokdorf)": "30622610",
+                "Suchraum Rastede (Ovelgönne / Rastede / Wiefelstede / Westerstede)": "1128250704",
                 "Blockland / Neu": "44717036",
+                "Samtgemeinde Sottrum": "955268864",
+ 
+
+                "Sengwarden": "1134105414",                  # Wilhelmshaven / Landkreis Friesland
+                "Sengwarden (TenneT)": "1134105414",         #  Wilhelmshaven / Landkreis Friesland
+                "Hochwöhrden (50Hertz)": "30622610",         # Suchraum Brunsbüttel
+                "Hochwörden (TenneT)": "603661085",          #  Heide West
+                "Großenmeer": "1128250704",                  #  Suchraum Rastede
+                "Großenmeer (TenneT)": "44717036",           #  Blockland / Neu
+                "Stilow (50Hertz)": "460134233",             #  Suchraum Kemnitz (50Hertz)
+                "Suchraum Werderland (TenneT)": "955268864", #  Samtgemeinde Sottrum
+                "Suchraum Esens (TenneT)": "1280178911",     #  Suchraum Nüttermoor (TenneT)
+                "Suchraum Esens (Amprion)": "1280178911",    #  Suchraum Nüttermoor (TenneT)
             }
+
         else:
             raise Exception("""The OSM year used is not yet compatible with
                             this function""")
         id_bus = {**id_bus, **id_bus2}
-
+ 
     elif "status" in scenario:
         year = int(scenario[-4:])
-
+ 
         id_bus = {
             "UW Inhausen": "29420322",
             "UW Bentwisch": "32063539",
@@ -96,82 +100,144 @@ def map_id_bus(scenario, sources):
             "UW Diele": "177829920",
             "UW Lubmin": "460134233",
         }
-
+ 
         if year >= 2023:
             # No update needed as no new stations used for offshore wind
             # between 2019 and 2023
             pass
-
+ 
         # TODO: If necessary add new stations when generating status quo > 2023
-
+ 
     else:
         id_bus = {}
-
+ 
     return id_bus
 
 
+
 def assign_ONEP_areas():
+    # Maps connection point name → representative wind park ID
+    # (Each NVP gets ONE representative wind park location since NEP aggregates
+    #  capacity per connection point. All wind parks at the same NVP share the
+    #  same geom in the database.)
     return {
-        "Büttel": "NOR-4-1",
-        "Heide/West": "NOR-10-2",
-        "Suchraum Gemeinden Ibbenbüren/Mettingen/Westerkappeln": "NOR-9-2",
-        "Suchraum Zensenbusch": "NOR-7-1",
-        "Rommerskirchen": "NOR-7-1",
-        "Oberzier": "NOR-7-1",
-        "Garrel/Ost": "NOR-7-1",
-        "Diele": "NOR-6-1",
-        "Dörpen/West": "NOR-2-2",
-        "Emden/Borßum": "NOR-0-1",
-        "Emden/Ost": "NOR-3-3",
-        "Hagermarsch": "NOR-2-1",
-        "Hanekenfähr": "NOR-6-3",
-        "Inhausen": "NOR-0-2",
-        "Unterweser": "NOR-9-1",
-        "Wehrendorf": "NOR-7-1",
-        "Wilhelmshaven 2": "NOR-11-1",
-        "Rastede": "NOR-7-1",
-        "Bentwisch": "OST-3-1",
-        "Lubmin": "OST-1-1",
-        "Suchraum Gemeinde Papendorf": "OST-7-1",
-        "Suchraum Gemeinden Brünzow/Kemnitz": "OST-1-4",
-        "inhausen": "NOR-0-2",
-        "Cloppenburg": "NOR-4-1",
+        # 28 connection points present in BOTH NEP and FEP sheets
+        "Büttel": "NOR-4-1 (HelWin1)",
+        "Diele": "NOR-6-1 (BorWin1)",
+        "Dörpen West": "NOR-2-2 (DolWin1)",
+        "Hagermarsch": "NOR-2-1 (Alpha Ventus)",
+        "Hanekenfähr (Amprion)": "NOR-6-3 (BorWin4)",
+        "Inhausen": "NOR-0-2 (Nordergründe)",
+        "Unterweser (TenneT)": "NOR-9-1 (BalWin1)",
+        "Wehrendorf (Amprion)": "NOR-9-1 (BalWin1)",
+        "Bentwisch (50Hertz)": "OST-3-1 (Baltic1)",
+        "Lubmin (50Hertz)": "OST-1-1 (Ostwind 1)",
+        "Gemeinde Papendorf": "OST-7-1 (nördlich Warnemünde)",
+        "Rommerskirchen (Amprion)": "NOR-14-2",
+        "Heide West": "NOR-10-2",
+        "Emden Ost": "NOR-3-3 (DolWin6)",
+        "Garrel / Ost (TenneT)": "NOR-7-1 (BorWin5)",
+        "Emden-Borssum": "NOR-0-1 (Riffgat)",
+        "Westerkappeln": "NOR-10-1 (BalWin2)",
+        "Niederrhein (Amprion)": "NOR-5-2",
+        "Kusenhorst": "NOR-6-4",
+        "Sechtem (Amprion)": "NOR-5-3",
+        "Hardebek": "NOR-16-1",
+        "Suchraum BBS (50Hertz)": "NOR-16-2",
+        "Suchraum Nüttermoor (TenneT)": "NOR-17-2",
+        "Suchraum Ried (Amprion)": "NOR-17-1",
+        "Kriftel (Amprion)": "NOR-16-4",
+        "Suchraum Pöschendorf (50Hertz)": "NOR-12-3",
+        "Suchraum Pöschendorf (TenneT)": "NOR-12-4",
+ 
+
+        "Wilhelmshaven / Landkreis Friesland": "NOR-9-2",
+        "Suchraum Brunsbüttel (Gemeinden Brunsbüttel / Büttel / St. Margarethen / Brokdorf)": "NOR-11-1",
+        "Suchraum Rastede (Ovelgönne / Rastede / Wiefelstede / Westerstede)": "NOR-13-1",
+        "Blockland / Neu": "NOR-14-1",
+        "Samtgemeinde Sottrum": "NOR-9-4 (BalWin5)",
+ 
+
+        "Sengwarden": "NOR-9-2",                      #  Wilhelmshaven / Landkreis Friesland
+        "Sengwarden (TenneT)": "NOR-11-2",            #  Wilhelmshaven / Landkreis Friesland
+        "Hochwöhrden (50Hertz)": "NOR-11-1",          #  Suchraum Brunsbüttel
+        "Hochwörden (TenneT)": "NOR-12-2 (LanWin2)",  #  Heide West
+        "Großenmeer": "NOR-13-1",                     #  Suchraum Rastede
+        "Großenmeer (TenneT)": "NOR-14-1",            #  Blockland / Neu
+        "Stilow (50Hertz)": "OST-2-4 (Ostwind4)",     #  Suchraum Kemnitz (50Hertz)
+        "Suchraum Werderland (TenneT)": "NOR-9-4 (BalWin5)",  # → Samtgemeinde Sottrum
+        "Suchraum Esens (TenneT)": "NOR-19-1",        #  Suchraum Nüttermoor (TenneT)
+        "Suchraum Esens (Amprion)": "NOR-19-2",       #  Suchraum Nüttermoor (TenneT)
     }
+
+
+
 
 
 def map_ONEP_areas():
     return {
-        "NOR-0-1": Point(6.5, 53.6),
-        "NOR-0-2": Point(8.07, 53.76),
-        "NOR-1": Point(6.21, 54.06),
-        "NOR-1-1": Point(6.21, 54.06),
-        "NOR-2-1": Point(6.54, 53.99),
-        "NOR-2-2": Point(6.54, 53.99),
-        "NOR-2-3": Point(6.54, 53.99),
-        "NOR-3-1": Point(6.95, 54.02),
-        "NOR-3-3": Point(6.95, 54.02),
-        "NOR-4-1": Point(7.70, 54.44),
-        "NOR-4-2": Point(7.70, 54.44),
-        "NOR-5-1": Point(7.21, 55.14),
-        "NOR-6-1": Point(5.92, 54.30),
-        "NOR-6-2": Point(5.92, 54.30),
-        "NOR-6-3": Point(5.92, 54.30),
-        "NOR-7": Point(6.22, 54.32),
-        "NOR-7-1": Point(6.22, 54.32),
-        "NOR-8-1": Point(6.35, 54.48),
-        "NOR-9-1": Point(5.75, 54.5),
-        "NOR-9-2": Point(5.75, 54.5),
+        # Existing wind park coordinates (from original code)
+        "NOR-0-1 (Riffgat)": Point(6.5, 53.6),
+        "NOR-0-2 (Nordergründe)": Point(8.07, 53.76),
+        "NOR-1-1 (DolWin5)": Point(6.21, 54.06),
+        "NOR-2-1 (Alpha Ventus)": Point(6.54, 53.99),
+        "NOR-2-2 (DolWin1)": Point(6.54, 53.99),
+        "NOR-2-3 (DolWin3)": Point(6.54, 53.99),
+        "NOR-3-1 (DolWin2)": Point(6.95, 54.02),
+        "NOR-3-2 (DolWin4)": Point(6.95, 54.02),
+        "NOR-3-3 (DolWin6)": Point(6.95, 54.02),
+        "NOR-4-1 (HelWin1)": Point(7.70, 54.44),
+        "NOR-4-2 (HelWin2)": Point(7.70, 54.44),
+        "NOR-5-1 (SylWin1)": Point(7.21, 55.14),
+        "NOR-6-1 (BorWin1)": Point(5.92, 54.30),
+        "NOR-6-2 (BorWin2)": Point(5.92, 54.30),
+        "NOR-6-3 (BorWin4)": Point(5.92, 54.30),
+        "NOR-7-1 (BorWin5)": Point(6.22, 54.32),
+        "NOR-7-2 (BorWin6)": Point(6.22, 54.32),
+        "NOR-8-1 (BorWin3)": Point(6.35, 54.48),
+        "NOR-9-1 (BalWin1)": Point(5.79475, 54.43928),
         "NOR-10-2": Point(6, 54.75),
+        "OST-1-1 (Ostwind 1)": Point(14.09, 54.82),
+        "OST-1-2 (Ostwind 1)": Point(14.09, 54.82),
+        "OST-1-3 (Ostwind 1)": Point(14.09, 54.82),
+        "OST-2-1 (Ostwind 2)": Point(14.09, 54.82),
+        "OST-2-2 (Ostwind 2)": Point(14.09, 54.82),
+        "OST-2-3 (Ostwind 2)": Point(14.09, 54.82),
+        "OST-3-1 (Baltic1)": Point(13.16, 54.98),
+        "OST-3-2 (Baltic2)": Point(13.16, 54.98),
+        "OST-7-1 (nördlich Warnemünde)": Point(12.25, 54.5),
+ 
+        # NEW wind park coordinates from Coordinates2.xlsx (NEP V2025)
+        "NOR-5-2": Point(7.01693, 55.0888),
+        "NOR-5-3": Point(6.86276, 55.23015),
+        "NOR-6-4": Point(6.02912, 54.42698),
+        "NOR-9-2": Point(5.6928, 54.49997),
+        "NOR-9-3 (BalWin4)": Point(5.59334, 54.53894),
+        "NOR-9-4 (BalWin5)": Point(5.90591, 54.53444),
+        "NOR-10-1 (BalWin2)": Point(6.0569, 54.61459),
         "NOR-11-1": Point(6.5, 54.75),
-        "OST-1-1": Point(14.09, 54.82),
-        "OST-1-2": Point(14.09, 54.82),
-        "OST-1-3": Point(14.09, 54.82),
-        "OST-1-4": Point(14.09, 54.82),
-        "OST-2": Point(13.86, 54.83),
-        "OST-3-1": Point(13.16, 54.98),
-        "OST-3-2": Point(13.16, 54.98),
-        "OST-7-1": Point(12.25, 54.5),
+        "NOR-11-2": Point(6.28843, 54.90406),
+        "NOR-12-1": Point(6.17076, 54.74855),
+        "NOR-12-2 (LanWin2)": Point(6.08071, 54.84685),
+        "NOR-12-3": Point(5.9539, 54.87547),
+        "NOR-12-4": Point(6.14039, 55.06388),
+        "NOR-13-2": Point(6.38346, 55.18865),
+        "NOR-14-1": Point(5.11342, 54.72641),
+        "NOR-14-2": Point(5.14326, 54.89121),
+        "NOR-16-1": Point(5.77932, 55.39819),
+        "NOR-16-2": Point(5.59797, 55.24033),
+        "NOR-16-3": Point(5.32235, 55.1404),
+        "NOR-16-4": Point(5.04044, 55.19885),
+        "NOR-17-1": Point(5.0382, 55.08595),
+        "NOR-17-2": Point(4.52533, 55.32292),
+        "NOR-19-1": Point(3.73521, 55.72058),
+        "NOR-19-2": Point(3.5456, 55.79089),
+        "OST-2-4 (Ostwind4)": Point(14.01305, 54.8837),
+        "NOR-13-1": Point(6.25970, 55.04013),
     }
+
+
+
 
 
 def insert():
@@ -195,9 +261,16 @@ def insert():
             """)
 
         # load file
-        if scenario == "eGon2035":
-            filename = "NEP2035_V2021_scnC2035.xlsx"
-
+        if scenario in ["eGon2035", "eGon2037", "eGon2045"]:
+            filename = "NEP2035_2037_2045_V2025_2023_scnC2037.xlsx"
+ 
+            # Map scenario to its capacity column
+            capacity_col = {
+                "eGon2035": "C 2035",
+                "eGon2037": "C 2037",
+                "eGon2045": "C 2045",
+            }[scenario]
+ 
             offshore_path = (
                 Path(".")
                 / "data_bundle_egon_data"
@@ -210,12 +283,13 @@ def insert():
                 usecols=[
                     "Netzverknuepfungspunkt",
                     "Spannungsebene in kV",
-                    "C 2035",
+                    capacity_col,
                 ],
             )
             offshore.dropna(subset=["Netzverknuepfungspunkt"], inplace=True)
-            offshore.rename(columns={"C 2035": "el_capacity"}, inplace=True)
+            offshore.rename(columns={capacity_col: "el_capacity"}, inplace=True)
             offshore = offshore[offshore["el_capacity"] > 0]
+
 
         elif scenario == "eGon100RE":
             offshore_path = (
@@ -309,18 +383,32 @@ def insert():
         offshore.dropna(subset=["bus_id"], inplace=True)
 
         # Overwrite geom for status2019 parks
-        if scenario in ["eGon2035", "eGon100RE"]:
+        if scenario in ["eGon2035", "eGon100RE", "eGon2037", "eGon2045"]:
             offshore["Name ONEP/NEP"] = offshore["Netzverknuepfungspunkt"].map(
-                assign_ONEP_areas()
-            )
+            assign_ONEP_areas()
+        )
+
 
         offshore["geom"] = offshore["Name ONEP/NEP"].map(map_ONEP_areas())
         offshore["weather_cell_id"] = pd.NA
-
+ 
         offshore.drop(["Name ONEP/NEP"], axis=1, inplace=True)
-
+ 
+        # Drop rows with placeholder coordinates (Point(0, 0)) — these are
+        # TBD entries where the wind park coordinates are not yet assigned.
+        # This prevents fake data from being inserted into the database.
+        placeholder = Point(0, 0)
+        tbd_mask = offshore["geom"].apply(
+            lambda g: g is not None and g.equals(placeholder)
+        )
+        if tbd_mask.any():
+            skipped = offshore.loc[tbd_mask, "Netzverknuepfungspunkt"].tolist()
+            print(f"Skipping {len(skipped)} TBD entries (placeholder coords): {skipped}")
+            offshore = offshore.loc[~tbd_mask].copy()
+ 
         if "status" in scenario:
             offshore.drop(["Inbetriebnahme"], axis=1, inplace=True)
+
 
         # Scale capacities for eGon100RE
         if scenario == "eGon100RE":
