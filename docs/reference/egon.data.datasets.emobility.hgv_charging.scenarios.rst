117d0edb919868a20ca5895a3045bfaada0651c4
Moritz.Schloesser <moritz.schloesser@rl-institut.de>
2026-08-06 14:59:36 +0000

    Add hgv_charging API docs, changelog and authors entry
    The hgv_charging package had no docs/reference/ stubs and was not listed in
    the emobility toctree, so none of its API documentation was built. Add one
    file per module plus the package file, following the existing
    heavy_duty_transport layout.
    
    Bump HeavyDutyTransport 0.0.5 -> 0.0.6: its active scenario list changed
    from ["eGon2035", "eGon100RE"] to ["eGon2035"], since eGon100RE is not
    registered as an EgonScenario row on this branch and would hit a foreign
    key error.
    
    Add the changelog entry for the electric HGV demand model (#1436) and add
    myself to AUTHORS.rst.

---
+++ docs/reference/egon.data.datasets.emobility.hgv_charging.scenarios.rst
@@ -0,0 +1,7 @@
+scenarios
+=========
+
+.. automodule:: egon.data.datasets.emobility.hgv_charging.scenarios
+   :members:
+   :undoc-members:
+   :show-inheritance:
--- src/egon/data/datasets/emobility/heavy_duty_transport/__init__.py
+++ src/egon/data/datasets/emobility/heavy_duty_transport/__init__.py
@@ -165,7 +165,7 @@ class HeavyDutyTransport(Dataset):
     #:
     name: str = "HeavyDutyTransport"
     #:
-    version: str = "0.0.5"
+    version: str = "0.0.6"
 
     def __init__(self, dependencies):
         super().__init__(
