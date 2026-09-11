79d503fdcb167ca3fc2dfd337cd0be89c6c0a0d7
Christopher Varela <christopher.varelabarreno@dlr.de>
2026-09-11 06:58:54 +0200

    adapt gas_supply scripts

--- src/egon/data/airflow/dags/pipeline.py
+++ src/egon/data/airflow/dags/pipeline.py
@@ -71,8 +71,7 @@ from egon.data.datasets.hydrogen_etrago import (
 )
 from egon.data.datasets.industrial_gas_demand import (
     IndustrialGasDemand,
-    IndustrialGasDemandeGon100RE,
-    IndustrialGasDemandeGon2035,
+    IndustrialGasDemandScenarios,
 )
 from egon.data.datasets.industrial_sites import MergeIndustrialSites
 from egon.data.datasets.industry import IndustrialDemandCurves
@@ -496,18 +495,9 @@ with airflow.DAG(
         )
 
         # Assign industrial gas demand eGon2035
-        IndustrialGasDemandeGon2035(
+        IndustrialGasDemandScenarios(
             dependencies=[create_gas_polygons, industrial_gas_demand]
-        )
-
-        # Assign industrial gas demand eGon100RE
-        IndustrialGasDemandeGon100RE(
-            dependencies=[
-                create_gas_polygons,
-                industrial_gas_demand,
-                run_pypsaeur,
-            ]
-        )
+        ) # TO DO: decide on using pypsa-eur results as in Egon100RE
 
     with TaskGroup(
         group_id="combined_heat_and_power"
